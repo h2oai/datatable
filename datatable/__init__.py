ba1d97b596b1b65d9c0dad53544ffc34f77bb796
Pasha Stetsenko <pasha@h2o.ai>
2017-03-03 13:49:17 -0800

    Initial documentation for DataTable's desired interface

--- datatable/__init__.py
+++ datatable/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Copyright 2017 H2O.ai; Apache License Version 2.0;  -*- encoding: utf-8 -*-
-from __future__ import division, print_function
+from __future__ import division, print_function, unicode_literals
 
 from .__version__ import version as __version__
 
---
+++ datatable/dt.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# Copyright 2017 H2O.ai; Apache License Version 2.0;  -*- encoding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+
+__all__ = ("DataTable", )
+
+
+class DataTable(object):
+
+    def __init__(self):
+        self._ncols = None  # type: int
+        self._nrows = None  # type: int
+        self._names = None  # type: List[str]
+        self._types = None  # type: List[str]
+        self._cols = None   # type: List[Col]
+
+    def __call__(self, rows=None, select=None, update=None, groupby=None,
+                 join=None, sort=None, limit=None):
+        """
+        Perform computation on a datatable, and return the result.
+
+        :param rows:
+            Which rows to operate upon. Could be one of the following:
+
+                - ... (or omitted), representing all rows of the datatable
+                - an integer, representing a single row
+                - an list or tuple of integers, representing several rows
+                - a slice, representing some range of rows
+                - a list or tuple of slices
+                - a ``DataTable`` with a single boolean column and having same
+                  number of rows as the current datatable, this will select
+                  only those rows in the current datatable where the ``rows``
+                  datatable has truthful value
+                - a function that takes a single parameter -- the current
+                  datatable -- and returns any of the selectors mentioned
+                  above. Within this function, the frame behaves lazily.
+
+        :param select:
+            When this parameter is specified, a new datatable will be computed
+            and returned from this call. This parameter cannot be combined with
+            ``update``. Possible values:
+
+                - ..., to select all columns in the current frame
+                - dt.GROUP symbol, which can be used only when a ``groupby`` is
+                  requested. This symbol selects all columns that were grouped
+                  upon.
+                - an integer, selecting a single column at the given index
+                - a string, selecting a single column by name
+                - a slice, selecting a range of columns
+                - a Mapper object, bound to one (or more) columns of the current
+                  datatable. This object is callable, taking the per-row value
+                  of the bound column, and producing a single result or a list
+                  of results. When a list is produced, it will be used to create
+                  as many columns in the resulting datatable as there are
+                  elements in the list. The Mapper may also explicitly specify
+                  the name/type of the column(s) it produces. If any of the
+                  names already exist in the datatable, an exception will be
+                  raised.
+                - a Reducer object, bound to one (or more) columns of the
+                  current datatable. This object is a callable, taking a list
+                  (or list of lists) of values for each row of the current
+                  datatable, and returning a single output (or a list of
+                  outputs). The Reducer may also explicitly specify the name/
+                  type of the column(s) it produces.
+                - a list or tuple or dictionary of any of the above. A list or
+                  a tuple will create multiple columns in the resulting
+                  datatable having same names as in the current datatable. When
+                  a dict is used, the columns will be renamed according to the
+                  keys of the dictionary. Reducers cannot be combined with any
+                  other selectors.
+                - a function that takes a single argument -- the current
+                  datatable -- and returns any of the selectors above. Within
+                  the function any operations on the frame will be lazy.
+
+        :param update:
+            When this parameter is specified, it causes an in-place
+            modification of the current datatable. This parameter is exclusive
+            with ``select``. Possible values:
+
+                - a dictionary ``{str: Mapper}``, where each ``Mapper`` is
+                  bound to one or more columns of the current datatable. The
+                  mapper must return a single value (list of values is not
+                  allowed), and it will be stored in the column given by the
+                  corresponding key in the dictionary. If a column with same
+                  name already exists, it will be replaced; otherwise a new
+                  column will be added.
+                - a list of ``Mapper``s each bound to one or more columns of
+                  the current datatable. These mappers will operate on the
+                  datatable row-by-row, producing one or more outputs (in case
+                  a list of outputs is returned, multiple columns will be
+                  created by each mapper). The results will be appended to the
+                  current datatable with automatically generated column names.
+                  The mappers may also explicitly specify the name(s)/type(s)
+                  of columns produce; if any of these names already exist in
+                  the datatble, these columns will be replaced.
+                - a list of ``Reducer``s (or single reducer), which will
+                  produce a constant column having the value produced by the
+                  reducer after running on all rows of the current datatable.
+                - a function that takes a single argument -- the current
+                  datatable -- and returns any of the selectors above. Within
+                  the function any operations on the frame will be lazy.
+
+        :param groupby:
+            When this parameter is specified, it will perform a "group-by"
+            operation on the datatable. The ``select``/``update`` clauses in
+            this case may contain only ``Reducer``s, or the columns specified
+            in the groupby, or mappers bound to the columns specified in the
+            groupby. Then each reducer will be executed within the subset of
+            rows for each group. When used with a select clause, the produced
+            datatable will contain as many rows as there are distinct groups
+            in the current datatable. When used with an update clause, the
+            new columns will have constant reduced value within each group.
+            Possible values for the parameter:
+
+                - an integer, specifying column's index
+                - a string, selecting a single column by name
+                - a Mapper object bound to one or more columns of the current
+                  datatable -- the mapped values will be used to produce the
+                  groupby values.
+                - a list or a tuple or a dict of the above. If a dictionary is
+                  given, then it specifies how to rename the columns within
+                  the groupby.
+                - a function taking the current datatable as an argument, and
+                  producing any of the groupby selectors listed above. Within
+                  this function all datatable operations are lazy.
+
+        :param join:
+            Specifies another datatable to join with. If this parameter is
+            given, then the "function" argument within ``rows``, ``select``
+            and ``update`` will be passed two parameters instead of one: the
+            current datatable, and the ``join`` datatable. The join condition
+            should be expressed in the ``rows`` parameter.
+
+        :param sort:
+            When specified, the datatable will be sorted. If used with
+            ``select``, it will sort the resulting datatable. If there is no
+            ``select`` or ``update``, it will sort the current datatable
+            in-place. Cannot be used together with ``update``.
+
+            Possible values are same as for the ``groupby`` parameter. The
+            ``sort`` argument may refer to the names of the columns being
+            produced by the select/update clauses. Additionally, every column
+            specified may be wrapped in a ``dt.reverse()`` call, reversing the
+            sorting direction for that column.
+
+        :param limit:
+            If an integer, then no more than that many rows will be returned by
+            the ``select`` clause. This can also be a slice, which effectively
+            applies that slice to the resulting datatable.
+        """
+        pass
