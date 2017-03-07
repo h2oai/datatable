af2a9028b47f41997032d2c8bd348cf3157745a5
Pasha Stetsenko <pasha@h2o.ai>
2017-03-07 13:10:27 -0800

    Forgot to include dtutils.c into prev. commit

---
+++ c/dtutils.c
@@ -0,0 +1,19 @@
+#include "dtutils.h"
+
+
+/**
+ * Create and return a new instance of python's None object
+ * (actually, this reuses Py_None singleton, increasing its REFCNT)
+ */
+PyObject* none(void) {
+    Py_RETURN_NONE;
+}
+
+
+/**
+ * Make and return a new reference for the provided PyObject
+ */
+PyObject* makeref(PyObject* x) {
+    Py_INCREF(x);
+    return x;
+}
