b4ccbefc01dedab380031d4ce64d032790857cd9
Pasha Stetsenko <pasha@h2o.ai>
2017-05-24 14:10:23 -0700

    Make all ISNA_* functions inline (both in C code and in auto-generated LLVM)

--- datatable/graph/context.py
+++ datatable/graph/context.py
@@ -80,7 +80,7 @@ class CModuleNode(Node):
     def add_extern(self, name):
         if name not in self._global_names:
             self._global_names.add(name)
-            self._extern_declarations += "extern %s;\n" % _externs[name]
+            self._extern_declarations += _externs[name] + "\n"
 
 
     def add_initializer(self, expr):
@@ -161,12 +161,12 @@ typedef struct DataTable {
   struct Column **columns;
 } DataTable;
 
+#define BIN_NAF4 0x7F8007A2u
+#define BIN_NAF8 0x7FF00000000007A2ull
 typedef union { uint64_t i; double d; } double_repr;
 typedef union { uint32_t i; float f; } float_repr;
-static inline int ISNA_F4(float x) { float_repr xx; xx.f = x; return xx.i == 0x7F8007A2u; }
-static inline int ISNA_F8(double x) { double_repr xx; xx.d = x; return xx.i == 0x7FF00000000007A2ull; }
-static inline float __nanf__(void) { const float_repr x = { 0x7F8007A2ul }; return x.f; }
-static inline double __nand__(void) { const double_repr x = { 0x7FF00000000007A2ull }; return x.d; }
+static inline float _nanf_(void) { float_repr x = { BIN_NAF4 }; return x.f; }
+static inline double _nand_(void) { double_repr x = { BIN_NAF8 }; return x.d; }
 
 #define NA_I1  (-128)
 #define NA_I2  (-32768)
@@ -176,42 +176,22 @@ static inline double __nand__(void) { const double_repr x = { 0x7FF00000000007A2
 #define NA_U2  65535u
 #define NA_U4  4294967295u
 #define NA_U8  18446744073709551615u
-#define NA_F4  __nanf__()
-#define NA_F8  __nand__()
+#define NA_F4  _nanf_()
+#define NA_F8  _nand_()
 
 """
 
 
 _externs = {
-    "ISNA_F4": "int ISNA_F4(float)",
-    "ISNA_F8": "int ISNA_F8(double)",
-    "ISNA_I1": "int ISNA_I1(int8_t)",
-    "ISNA_I2": "int ISNA_I2(int16_t)",
-    "ISNA_I4": "int ISNA_I4(int32_t)",
-    "ISNA_I8": "int ISNA_I8(int64_t)",
-    "ISNA_U1": "int ISNA_U1(uint8_t)",
-    "ISNA_U2": "int ISNA_U2(uint16_t)",
-    "ISNA_U4": "int ISNA_U4(uint32_t)",
-    "columns_from_slice":
-        "Column** columns_from_slice"
-        "(DataTable *dt, int64_t start, int64_t count, int64_t step)",
-    "columns_from_pymixed":
-        "Column** columns_from_pymixed(PyObject *elems, DataTable *dt, "
-        "RowMapping *rowmapping, "
-        "int (*mapfn)(int64_t row0, int64_t row1, void** out))",
-    "pydatatable_assemble":
-        "DataTable_PyObject* pydatatable_assemble"
-        "(int64_t nrows, Column **cols)",
-    "pydatatable_assemble_view":
-        "DataTable_PyObject* pydatatable_assemble_view"
-        "(DataTable_PyObject *src, RowMapping *rm, Column **cols)",
-    "pydt_from_dt":
-        "DataTable_PyObject* pydt_from_dt(DataTable*, DataTable_PyObject*)",
-    "rowmapping_from_datacolumn":
-        "RowMapping* rowmapping_from_datacolumn(Column *col, int64_t nrows)",
-    "rowmapping_from_filterfn32":
-        "RowMapping* rowmapping_from_filterfn32"
-        "(int (*filter)(int64_t, int64_t, int32_t*, int32_t*), int64_t nrows)",
-    "rowmapping_from_slice":
-        "RowMapping* rowmapping_from_slice(int64_t, int64_t, int64_t)",
+    "ISNA_F4": "inline int ISNA_F4(float x) "
+               "{ float_repr xx; xx.f = x; return xx.i == BIN_NAF4; }",
+    "ISNA_F8": "inline int ISNA_F8(double x) "
+               "{ double_repr xx; xx.d = x; return xx.i == BIN_NAF8; }",
+    "ISNA_I1": "inline int ISNA_I1(int8_t x) { return x == NA_I1; }",
+    "ISNA_I2": "inline int ISNA_I2(int16_t x) { return x == NA_I2; }",
+    "ISNA_I4": "inline int ISNA_I4(int32_t x) { return x == NA_I4; }",
+    "ISNA_I8": "inline int ISNA_I8(int64_t x) { return x == NA_I8; }",
+    "ISNA_U1": "inline int ISNA_U1(uint8_t x) { return x == NA_U1; }",
+    "ISNA_U2": "inline int ISNA_U2(uint16_t x) { return x == NA_U2; }",
+    "ISNA_U4": "inline int ISNA_U4(uint32_t x) { return x == NA_U4; }",
 }
