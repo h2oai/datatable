061f4620d26b1bdd61c23659ea270d3d25c6274e
Pasha Stetsenko <pasha@h2o.ai>
2017-05-24 15:32:38 -0700

    Review test_dt_rows.py; ensure 100% coverage of rows_node.py

--- tests/munging/test_dt_rows.py
+++ tests/munging/test_dt_rows.py
@@ -38,6 +38,21 @@ def as_list(datatable):
     nrows, ncols = datatable.shape
     return datatable.internal.window(0, nrows, 0, ncols).data
 
+def is_slice(dt, cols=range(3)):
+    return (dt.internal.isview and
+            dt.internal.rowmapping_type == "slice" and
+            dt.internal.view_colnumbers == tuple(cols))
+
+def is_arr(dt, cols=range(3)):
+    return (dt.internal.isview and
+            dt.internal.rowmapping_type == "arr32" and
+            dt.internal.view_colnumbers == tuple(cols))
+
+def is_slice_or_arr(dt, nrows, cols=range(3)):
+    rtype = "slice" if nrows > 0 else "arr32"
+    return (dt.internal.isview and
+            dt.internal.rowmapping_type == rtype and
+            dt.internal.view_colnumbers == tuple(cols))
 
 
 #-------------------------------------------------------------------------------
@@ -45,6 +60,7 @@ def as_list(datatable):
 #-------------------------------------------------------------------------------
 
 def test_dt0_properties(dt0):
+    """Test basic properties of the DataTable object."""
     assert isinstance(dt0, dt.DataTable)
     assert dt0.nrows == 10
     assert dt0.ncols == 3
@@ -52,25 +68,35 @@ def test_dt0_properties(dt0):
     assert dt0.names == ("colA", "colB", "colC")
     assert dt0.types == ("bool", "int", "real")
     assert dt0.stypes == ("i1b", "i2i", "f8r")
+    assert str(dt0.internal.__class__) == "<class '_datatable.DataTable'>"
+    assert dt0.internal.isview is False
+    assert dt0.internal.rowmapping_type is None
+    assert dt0.internal.view_colnumbers is None
 
 
 def test_rows_ellipsis(dt0):
+    """Both dt(...) and dt() should select all rows and all columns."""
+    dt1 = dt0()
+    assert dt1.shape == (10, 3)
+    assert is_slice(dt1)
     dt1 = dt0(...)
     assert dt1.shape == (10, 3)
-    assert dt1.internal.isview
-    assert dt1.internal.rowmapping_type == "slice"
-    assert dt1.internal.view_colnumbers == (0, 1, 2)
+    assert is_slice(dt1)
 
 
 def test_rows_integer(dt0):
+    """
+    Test row selectors which are simple integers, such as:
+        dt(5)
+    should select the 6-th row of the datatable. Negative integers are also
+    allowed, and should count from the end of the datatable.
+    """
     for i in range(-10, 10):
         dt1 = dt0(i)
         assert dt1.shape == (1, 3)
         assert dt1.names == ("colA", "colB", "colC")
         assert dt1.types == ("bool", "int", "real")
-        assert dt1.internal.isview
-        assert dt1.internal.rowmapping_type == "slice"
-        assert dt1.internal.view_colnumbers == (0, 1, 2)
+        assert is_slice(dt1)
     assert as_list(dt0(0)) == [[0], [7], [5]]
     assert as_list(dt0(-2))[:2] == [[1], [1]]
     assert isnan(as_list(dt0(-2))[2][0])
@@ -80,9 +106,17 @@ def test_rows_integer(dt0):
     assert_valueerror(dt0, 10, "Row `10` is invalid for datatable with 10 rows")
     assert_valueerror(dt0, -11, "Row `-11` is invalid for datatable with 10")
     assert_valueerror(dt0, -20, "Row `-20` is invalid for datatable with 10")
+    assert_valueerror(dt0, 10**19, "Row `10000000000000000000` is invalid")
 
 
 def test_rows_slice(dt0):
+    """
+    Test row selectors which are slices, such as:
+        dt[:5, :]
+        dt[3:-2, :]
+        dt[::-1, :]
+        dt(rows=slice(None, None, -1))
+    """
     for sliceobj, nrows in [(slice(None, -2), 8),
                             (slice(0, 7), 7),
                             (slice(5, 9), 4),
@@ -93,9 +127,7 @@ def test_rows_slice(dt0):
         assert dt1.shape == (nrows, 3)
         assert dt1.names == ("colA", "colB", "colC")
         assert dt1.types == ("bool", "int", "real")
-        assert dt1.internal.isview
-        assert dt1.internal.view_colnumbers == (0, 1, 2)
-        assert dt1.internal.rowmapping_type == "slice" if nrows > 1 else "arr32"
+        assert is_slice_or_arr(dt1, nrows)
     assert as_list(dt0[:5, :])[0] == [0, 1, 1, None, 0]
     assert as_list(dt0[::-1, :])[1] == [None, 1, -1, 0, 0, None, 1e4, 9, -11, 7]
     assert as_list(dt0[::3, :])[1] == [7, 10000, 0, None]
@@ -112,6 +144,11 @@ def test_rows_slice(dt0):
 
 
 def test_rows_range(dt0):
+    """
+    Test that a range object can be used as a row selector, for example:
+        dt(range(5))
+        dt(range(3, -1, -1))
+    """
     for rangeobj in [range(5),
                      range(2, 3),
                      range(-5, -2),
@@ -123,9 +160,7 @@ def test_rows_range(dt0):
         assert dt1.shape == (nrows, 3)
         assert dt1.names == ("colA", "colB", "colC")
         assert dt1.types == ("bool", "int", "real")
-        assert dt1.internal.isview
-        assert dt1.internal.view_colnumbers == (0, 1, 2)
-        assert dt1.internal.rowmapping_type == "slice" if nrows > 1 else "arr32"
+        assert is_slice_or_arr(dt1, nrows)
     assert_valueerror(dt0, range(15),
                       "Invalid range(0, 15) for a datatable with 10 rows")
     assert_valueerror(dt0, range(-5, 5),
@@ -133,17 +168,26 @@ def test_rows_range(dt0):
 
 
 def test_rows_generator(dt0):
+    """
+    Test the use of a generator object as a row selector:
+        dt(i for i in range(dt.nrows) if i % 3 <= 1)
+    """
     g = (i * 2 for i in range(4))
     assert type(g).__name__ == "generator"
     dt1 = dt0(g)
     assert dt1.shape == (4, 3)
-    assert dt1.internal.isview
-    assert dt1.internal.rowmapping_type == "arr32"
+    assert is_arr(dt1)
     assert_valueerror(dt0, (i if i % 3 < 2 else str(-i) for i in range(10)),
                       "Invalid row selector '-2' generated at position 2")
 
 
 def test_rows_multislice(dt0):
+    """
+    Test that it is possible to combine multiple row-selectors in an array:
+        dt([0, 5, 2, 1])
+        dt([slice(None, None, 2), slice(1, None, 2)])
+        dt([0, 1] * 100)
+    """
     for selector, nrows in [([2, 7, 0, 9], 4),
                             ({1, -1, 0}, 3),
                             ((-1, -1, -1, -1), 4),
@@ -154,9 +198,7 @@ def test_rows_multislice(dt0):
         assert dt1.shape == (nrows, 3)
         assert dt1.names == ("colA", "colB", "colC")
         assert dt1.types == ("bool", "int", "real")
-        assert dt1.internal.isview
-        assert dt1.internal.view_colnumbers == (0, 1, 2)
-        assert dt1.internal.rowmapping_type == "arr32"
+        assert is_arr(dt1)
     assert as_list(dt0([3, 9, 1, 0]))[0] == [None, 1, 1, 0]
     assert as_list(dt0((2, 5, 5, -1)))[1] == [9, 0, 0, None]
     assert (as_list(dt0([slice(5, None), slice(None, 5)]))[1] ==
@@ -170,14 +212,16 @@ def test_rows_multislice(dt0):
 
 
 def test_rows_column(dt0):
+    """
+    Test the use of a boolean column as a filter:
+        dt(rows=dt2[0])
+    """
     col = dt.DataTable([1, 0, 1, 1, None, 0, None, 1, 1, 0])
     dt1 = dt0(col)
     assert dt1.shape == (5, 3)
     assert dt1.names == ("colA", "colB", "colC")
     assert dt1.types == ("bool", "int", "real")
-    assert dt1.internal.isview
-    assert dt1.internal.view_colnumbers == (0, 1, 2)
-    assert dt1.internal.rowmapping_type == "arr32"
+    assert is_arr(dt1)
     assert as_list(dt1)[1] == [7, 9, 10000, -1, 1]
     assert_valueerror(dt0, dt0,
                       "`rows` argument should be a single-column datatable")
@@ -189,10 +233,13 @@ def test_rows_column(dt0):
 
 
 def test_rows_function(dt0):
+    """
+    Test that it is possible to use a lambda-expression as a filter:
+        dt(lambda f: f.colA < f.colB)
+    """
     dt1 = dt0(lambda f: f.colA)
     assert dt1.shape == (5, 3)
-    assert dt1.internal.isview
-    assert dt1.internal.rowmapping_type == "arr32"
+    assert is_arr(dt1)
     assert as_list(dt1)[1] == [-11, 9, 0, 1, None]
     dt2 = dt0(lambda f: [5, 7, 9])
     assert dt2.shape == (3, 3)
@@ -244,5 +291,8 @@ def test_chained_array(dt0):
 
 
 def test_rows_bad_arguments(dt0):
+    """
+    Test row selectors that are invalid (i.e. not of the types listed above).
+    """
     assert_typeerror(dt0, 0.5, "Unexpected `rows` argument: 0.5")
     assert_typeerror(dt0, "5", "Unexpected `rows` argument: '5'")
