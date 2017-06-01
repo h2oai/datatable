9e3567cb7d877a7844e1e4ad08c4f9873f852716
Pasha Stetsenko <pasha@h2o.ai>
2017-06-01 10:59:22 -0700

    Merge macro MALLOC into dtmalloc. Closes #33

--- c/utils.h
+++ c/utils.h
@@ -3,6 +3,7 @@
 //==============================================================================
 #ifndef dt_UTILS_H
 #define dt_UTILS_H
+#include <stddef.h>
 
 int64_t min(int64_t a, int64_t b);
 int64_t max(int64_t a, int64_t b);
@@ -20,58 +21,66 @@ float max_f4(float a, float b);
 })
 
 
+
+//==============================================================================
+// Pointer macros
+//==============================================================================
+
 /**
  * Use this macro to malloc a memory region and assign it to a variable. For
- * example, to malloc an array of 1000 integers:
+ * example, to malloc an array of 1000 integers / a void* memory region of 1024
+ * bytes:
  *
  *     int32_t *arr;
  *     dtmalloc(arr, int32_t, 1000);
+ *     void *buf;
+ *     dtmalloc_v(buf, 1024);
  *
  * The macro checks that the allocation was successful, and if not it executes
- * `return NULL`.
+ * `return NULL`. Thus, it can only be used within a function that returns a
+ * pointer.
  **/
-#ifdef __cplusplus
-    #define dtmalloc(ptr, T, n) do {                                           \
-        ptr = (T*) malloc((size_t)(n) * sizeof(T));                            \
-        if (ptr == nullptr) return nullptr;                                    \
-    } while(0)
-    #define dtmalloc_v(ptr, n) do {                                            \
-        ptr = malloc((size_t)(n));                                             \
-        if (ptr == nullptr) return nullptr;                                    \
-    } while(0)
-    #define dtrealloc(ptr, T, n) do {                                          \
-        ptr = (T*) realloc(ptr, (size_t)(n) * sizeof(T));                      \
-        if (ptr == nullptr) return nullptr;                                    \
-    } while(0)
-    #define dtrealloc_v(ptr, n) do {                                           \
-        ptr = realloc(ptr, (size_t)(n));                                       \
-        if (ptr == nullptr) return nullptr;                                    \
-    } while(0)
-#else
-    #define dtmalloc(ptr, T, n) do {                                           \
-        ptr = malloc((size_t)(n) * sizeof(T));                                 \
-        if (ptr == NULL) return NULL;                                          \
-    } while(0)
-    #define dtmalloc_v(ptr, n) do {                                            \
-        ptr = malloc((size_t)(n));                                             \
-        if (ptr == NULL) return NULL;                                          \
-    } while(0)
-    #define dtrealloc(ptr, T, n) do {                                          \
-        ptr = realloc(ptr, (size_t)(n) * sizeof(T));                           \
-        if (ptr == NULL) return NULL;                                          \
-    } while(0)
-    #define dtrealloc_v(ptr, n) do {                                           \
-        ptr = realloc(ptr, (size_t)(n));                                       \
-        if (ptr == NULL) return NULL;                                          \
-    } while(0)
-#endif
+#define dtmalloc(ptr, T, n) do {                                               \
+    ptr = (T*) _dt_malloc((size_t)(n) * sizeof(T));                            \
+    if (ptr == _NULL) return _NULL;                                            \
+} while(0)
+
+#define dtmalloc_v(ptr, n) do {                                                \
+    ptr = _dt_malloc((size_t)(n));                                             \
+    if (ptr == _NULL) return _NULL;                                            \
+} while(0)
 
 
 /**
- * Macro to free the memory previously allocated with `dtmalloc`.
- * (Currently it's same as system `free`, however that may change in the future).
+ * This macro is a replacement for standard `realloc` in much the same way as
+ * `dtmalloc` is a replacement for system `malloc`:
+ *
+ *     float *data = NULL;
+ *     dtrealloc(data, float, 100);
+ *
+ * The macro checks that the allocation was successful, and if not it executes
+ * `return NULL`. Thus, it can only be used within a function that returns a
+ * pointer.
+ */
+#define dtrealloc(ptr, T, n) do {                                              \
+    ptr = (T*) _dt_realloc(ptr, (size_t)(n) * sizeof(T));                      \
+    if (ptr == _NULL) return _NULL;                                            \
+} while(0)
+
+#define dtrealloc_v(ptr, n) do {                                               \
+    ptr = _dt_realloc(ptr, (size_t)(n));                                       \
+    if (ptr == _NULL) return _NULL;                                            \
+} while(0)
+
+
+/**
+ * Macro to free the memory previously allocated with `dtmalloc` / `dtrealloc`.
+ * This frees the memory and sets the pointer to NULL, for safety.
  **/
-#define dtfree  free
+#define dtfree(ptr) do {                                                       \
+    _dt_free(ptr);                                                             \
+    ptr = _NULL;                                                               \
+} while(0)
 
 
 /**
@@ -83,11 +92,27 @@ float max_f4(float a, float b);
 
 
 
+//---- Private -------------------------
+// The _dt_xxx functions are external and should be defined in py_utils.c
+void* _dt_malloc(size_t n);
+void* _dt_realloc(void *ptr, size_t n);
+void  _dt_free(void *ptr);
+#ifdef __cplusplus
+    #define _NULL nullptr
+#else
+    #define _NULL NULL
+#endif
+
+
+
+//==============================================================================
+// Other
+//==============================================================================
+
 /**
  * Mark a function parameter as deliberately unused.
  **/
 #define UNUSED(x)  __attribute__((unused)) x
-
 
 
 #endif
