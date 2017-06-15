f11629dc81006884b1c2d4ef451866cb5bb0d7e2
Pasha Stetsenko <pasha@h2o.ai>
2017-06-14 22:09:03 -0700

    Add type DataTable_t to typechecks (allows to check for a DataTable without having to load the class)

--- datatable/utils/typechecks.py
+++ datatable/utils/typechecks.py
@@ -3,6 +3,7 @@
 """
 Utility for checking types at runtime.
 """
+import importlib
 import typesentry
 from typesentry import U
 
@@ -14,4 +15,33 @@ ValueError = _tc.ValueError
 TypeError.__module__ = "dt"
 ValueError.__module__ = "dt"
 
-__all__ = ("typed", "is_type", "U", "TypeError", "ValueError")
+
+class _LazyClass(typesentry.MagicType):
+    def __init__(self, module, symbol):
+        self._module = module
+        self._symbol = symbol
+        self._checker = None
+
+    def name(self):
+        if self._module == "datatable":
+            return self._symbol
+        else:
+            return self._module + "." + self._symbol
+
+    def check(self, var):
+        if not self._checker:
+            self._load_symbol()
+        return self._checker(var)
+
+    def _load_symbol(self):
+        try:
+            mod = importlib.import_module(self._module)
+            cls = getattr(mod, self._symbol, False)
+            self._checker = lambda x: isinstance(x, cls)
+        except ImportError:
+            self._checker = lambda: False
+
+
+DataTable_t = _LazyClass("datatable", "DataTable")
+
+__all__ = ("typed", "is_type", "U", "TypeError", "ValueError", "DataTable_t")
