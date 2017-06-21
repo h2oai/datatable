h2oai/datatable

commit ea7f6d8afadea8eced266060b703ba51909c8771

Make fread auto-uncompress .zip and .gz2 archives before parsing

c/py_fread.c
@@ -87,9 +87,11 @@ PyObject* freadPy(UU, PyObject *args)
 
     freadMainArgs frargs;
 
-    filename = TOSTRING(ATTR(freader, "filename"), NULL);
-    input = TOSTRING(ATTR(freader, "text"), NULL);
+    // filename & input are borrowed references
+    filename = TOSTRING(ATTR(freader, "filename"));
+    input = TOSTRING(ATTR(freader, "text"));
     na_strings = TOSTRINGLIST(ATTR(freader, "na_strings"), NULL);
+
     frargs.filename = filename;
     frargs.input = input;
     frargs.sep = TOCHAR(ATTR(freader, "sep"), 0);
@@ -135,8 +137,6 @@ void cleanup_fread_session(freadMainArgs *frargs) {
     nstrcols = 0;
     types = NULL;
     sizes = NULL;
-    dtfree(filename);
-    dtfree(input);
     if (frargs) {
         if (na_strings) {
             char **ptr = na_strings;

c/py_utils.c
@@ -86,3 +86,36 @@ void _dt_free(void *ptr)
 {
     free(ptr);
 }
+
+
+char* _to_string(PyObject *x, _Bool copy)
+{
+    if (x == NULL) return (char*)-1;
+    char *res = NULL;
+    if (x != Py_None) {
+        char *buf = NULL;
+        size_t len = 0;
+        if (PyUnicode_Check(x)) {
+            PyObject *y = PyUnicode_AsEncodedString(x, "utf-8", "strict");
+            if (y == NULL) return (char*)-1;
+            buf = PyBytes_AsString(y);
+            len = (size_t) PyBytes_Size(y);
+            dtmalloc(res, char, len + 1);
+            memcpy(res, buf, len + 1);
+            copy = 0;
+            Py_DECREF(y);
+        }
+        if (PyBytes_Check(x)) {
+            buf = PyBytes_AsString(x);
+            len = (size_t) PyBytes_Size(x);
+        }
+        if (copy) {
+            dtmalloc(res, char, len + 1);
+            memcpy(res, buf, len + 1);
+        } else {
+            res = buf;
+        }
+    }
+    Py_DECREF(x);
+    return res;
+}

c/py_utils.h
@@ -43,22 +43,12 @@ void* clone(void *src, size_t n_bytes);
  * The returned result is a newly allocated memory buffer, and it is the
  * responsibility of the caller to free it.
  */
-#define TOSTRING(pyobj, dflt) ({                                               \
-    char *res = dflt;                                                          \
-    PyObject *x = pyobj;                                                       \
-    if (x == NULL) goto fail;                                                  \
-    if (x != Py_None) {                                                        \
-        PyObject *y = PyUnicode_AsEncodedString(x, "utf-8", "strict");         \
-        if (y == NULL) goto fail;                                              \
-        char *buf = PyBytes_AsString(y);                                       \
-        Py_DECREF(y);                                                          \
-        size_t len = (size_t) PyBytes_Size(y);                                 \
-        dtmalloc(res, char, len + 1);                                          \
-        memcpy(res, buf, len + 1);                                             \
-    }                                                                          \
-    Py_DECREF(x);                                                              \
+#define TOSTRING(pyobj) ({                                                     \
+    char *res = _to_string(pyobj, 0);                                          \
+    if ((int64_t)res == -1) goto fail;                                         \
     res;                                                                       \
 })
+
 
 
 #define TOCHAR(pyobj, dflt) ({                                                 \
@@ -120,5 +110,7 @@ void* clone(void *src, size_t n_bytes);
     res;                                                                       \
 })
 
+
+char* _to_string(PyObject *x, _Bool copy);
 
 #endif

datatable/fread.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2017 H2O.ai; Apache License Version 2.0;  -*- encoding: utf-8 -*-
 import os
+import tempfile
 
 # noinspection PyUnresolvedReferences
 import _datatable as c
@@ -26,6 +27,8 @@ class FReader(object):
                  header=None, na_strings=None, verbose=False, fill=False,
                  show_progress=term.is_a_tty, encoding=None, **args):
         self._filename = None   # type: str
+        self._tempfile = None   # type: str
+        self._tempdir = None    # type: str
         self._text = None       # type: str
         self._sep = None        # type: str
         self._maxnrows = None   # type: int
@@ -41,13 +44,13 @@ class FReader(object):
         self._bar_ends = None
         self._bar_symbols = None
 
-        self.filename = filename
+        self.verbose = verbose
         self.text = text
+        self.filename = filename
         self.sep = sep
         self.max_nrows = max_nrows
         self.header = header
         self.na_strings = na_strings
-        self.verbose = verbose
         self.fill = fill
         self.show_progress = show_progress
 
@@ -65,7 +68,9 @@ class FReader(object):
 
     @property
     def filename(self):
-        return self._filename
+        if self._text:
+            return None
+        return self._tempfile or self._filename
 
     @filename.setter
     @typed(filename=U(str, None))
@@ -75,10 +80,8 @@ class FReader(object):
         else:
             if filename.startswith("~"):
                 filename = os.path.expanduser(filename)
-            if "\x00" in filename:
-                raise ValueError("Path %s contains NULs" % filename)
+            self._check_file(filename)
             self._filename = filename
-            self._text = None
 
 
     @property
@@ -94,7 +97,6 @@ class FReader(object):
             if "\x00" in text:
                 raise ValueError("Text contains NUL characters")
             self._text = text
-            self._filename = None
 
 
     @property
@@ -185,15 +187,24 @@ class FReader(object):
 
 
     def read(self):
-        dt = c.fread(self)
-        return DataTable(dt, colnames=self._colnames)
+        _dt = c.fread(self)
+        dt = DataTable(_dt, colnames=self._colnames)
+        if self._tempfile:
+            if self._verbose:
+                self._vlog("Removing temporary file %s\n" % self._tempfile)
+            os.remove(self._tempfile)
+            os.rmdir(self._tempdir)
+        return dt
 
+
+    #---------------------------------------------------------------------------
 
     def _vlog(self, message):
         if self._log_newline:
             print("  ", end="")
         self._log_newline = message.endswith("\n")
         print(_log_color(message), end="", flush=True)
+
 
     def _progress(self, percent):
         bs = self._bar_symbols
@@ -231,3 +242,40 @@ class FReader(object):
                 pass
             except LookupError:
                 print("Warning: unknown encoding %s" % tty_encoding)
+
+
+    def _check_file(self, filename):
+        if "\x00" in filename:
+            raise ValueError("Path %s contains NUL characters" % filename)
+        if not os.path.exists(filename):
+            xpath = os.path.abspath(filename)
+            ypath = xpath
+            while not os.path.exists(xpath):
+                xpath = os.path.abspath(os.path.join(xpath, ".."))
+            ypath = ypath[len(xpath):]
+            raise ValueError("File %s`%s` does not exist" % (xpath, ypath))
+        if not os.path.isfile(filename):
+            raise ValueError("Path `%s` is not a file" % filename)
+
+        ext = os.path.splitext(filename)[1]
+        if ext == ".zip":
+            import zipfile
+            zf = zipfile.ZipFile(filename)
+            zff = zf.namelist()
+            if len(zff) > 1:
+                raise ValueError("Zip file %s contains multiple compressed "
+                                 "files: %r" % (filename, zff))
+            if len(zff) == 0:
+                raise ValueError("Zip file %s is empty" % filename)
+            self._tempdir = tempfile.mkdtemp()
+            if self._verbose:
+                self._vlog("Extracting %s to temporary directory %s\n"
+                           % (filename, self._tempdir))
+            self._tempfile = zf.extract(zff[0], path=self._tempdir)
+
+        elif ext == ".gz":
+            import gzip
+            zf = gzip.GzipFile(filename)
+            if self._verbose:
+                self._vlog("Extracting %s into memory\n" % filename)
+            self._text = zf.read()

commit ff8b100268aa50cebe2a397d543d7fd8f41dadcd

Fix a bug introduced in ea7f6d8

c/py_fread.c
@@ -75,6 +75,7 @@ static StrBuf *strbufs = NULL;
  */
 PyObject* freadPy(UU, PyObject *args)
 {
+    PyObject *tmp1 = NULL, *tmp2 = NULL;
     if (freader != NULL) {
         PyErr_SetString(PyExc_RuntimeError,
             "Cannot run multiple instances of fread() in-parallel.");
@@ -88,8 +89,8 @@ PyObject* freadPy(UU, PyObject *args)
     freadMainArgs frargs;
 
     // filename & input are borrowed references
-    filename = TOSTRING(ATTR(freader, "filename"));
-    input = TOSTRING(ATTR(freader, "text"));
+    filename = TOSTRING(ATTR(freader, "filename"), &tmp1);
+    input = TOSTRING(ATTR(freader, "text"), &tmp2);
     na_strings = TOSTRINGLIST(ATTR(freader, "na_strings"), NULL);
 
     frargs.filename = filename;
@@ -120,6 +121,8 @@ PyObject* freadPy(UU, PyObject *args)
 
     PyObject *pydt = pydt_from_dt(dt);
     if (pydt == NULL) goto fail;
+    Py_XDECREF(tmp1);
+    Py_XDECREF(tmp2);
     cleanup_fread_session(&frargs);
     return pydt;
 

c/py_utils.c
@@ -88,34 +88,22 @@ void _dt_free(void *ptr)
 }
 
 
-char* _to_string(PyObject *x, _Bool copy)
+char* _to_string(PyObject *x, PyObject **tmp)
 {
     if (x == NULL) return (char*)-1;
-    char *res = NULL;
     if (x != Py_None) {
-        char *buf = NULL;
-        size_t len = 0;
         if (PyUnicode_Check(x)) {
             PyObject *y = PyUnicode_AsEncodedString(x, "utf-8", "strict");
             if (y == NULL) return (char*)-1;
-            buf = PyBytes_AsString(y);
-            len = (size_t) PyBytes_Size(y);
-            dtmalloc(res, char, len + 1);
-            memcpy(res, buf, len + 1);
-            copy = 0;
-            Py_DECREF(y);
+            Py_DECREF(x);
+            *tmp = y;
+            return PyBytes_AsString(y);
         }
         if (PyBytes_Check(x)) {
-            buf = PyBytes_AsString(x);
-            len = (size_t) PyBytes_Size(x);
-        }
-        if (copy) {
-            dtmalloc(res, char, len + 1);
-            memcpy(res, buf, len + 1);
-        } else {
-            res = buf;
+            *tmp = x;
+            return PyBytes_AsString(x);
         }
     }
     Py_DECREF(x);
-    return res;
+    return NULL;
 }

c/py_utils.h
@@ -43,8 +43,8 @@ void* clone(void *src, size_t n_bytes);
  * The returned result is a newly allocated memory buffer, and it is the
  * responsibility of the caller to free it.
  */
-#define TOSTRING(pyobj) ({                                                     \
-    char *res = _to_string(pyobj, 0);                                          \
+#define TOSTRING(pyobj, tmp) ({                                                \
+    char *res = _to_string(pyobj, tmp);                                        \
     if ((int64_t)res == -1) goto fail;                                         \
     res;                                                                       \
 })
@@ -111,6 +111,6 @@ void* clone(void *src, size_t n_bytes);
 })
 
 
-char* _to_string(PyObject *x, _Bool copy);
+char* _to_string(PyObject *x, PyObject **tmp);
 
 #endif