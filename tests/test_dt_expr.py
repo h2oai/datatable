9ceb2d0c6536ee7e1bfcebec676955c8aea2fec0
Pasha Stetsenko <pasha@h2o.ai>
2017-08-08 23:28:21 -0700

    Update test_dt_expr.py

--- tests/test_dt_expr.py
+++ tests/test_dt_expr.py
@@ -8,7 +8,7 @@ def test_dt_isna_single_col():
     res = dt.isna(dt0)
     assert res.internal.check()
     assert res.types == ("bool", )
-    assert_equals(res, dt.DataTable([True, False, True, False, False]))
+    assert_equals(res, dt.DataTable({"V0": [True, False, True, False, False]}))
 
     
 def test_dt_isna_multi_col():
@@ -25,7 +25,6 @@ def test_dt_isna_expr():
                        colnames=list("ABC"))
     res = dt0[lambda f: dt.isna(f.C), :]
     assert res.internal.check()
-    assert res.types == ("bool", )
     assert_equals(res[:-1], dt.DataTable([[6, None],
                                           [2, 2]],
                                           colnames=list("AB")))
@@ -39,7 +38,6 @@ def test_dt_isna_expr_str():
                        colnames=list("ABC"))
     res = dt0[lambda f: dt.isna(f.C), :]
     assert res.internal.check()
-    assert res.types == ("bool", )
     assert_equals(res, dt.DataTable([[6],
                                      [2],
                                      [None]],
