2ca232c4fea3bbca2afff4751e5f8cd2e559a140
Pasha Stetsenko <pasha@h2o.ai>
2017-08-17 16:25:43 -0700

    Implement benchmark comparison with R data.table (#260)

--- tests/benchmarks/bench_sort_bool.py
+++ tests/benchmarks/bench_sort_bool.py
@@ -5,6 +5,10 @@
 #-------------------------------------------------------------------------------
 import datatable
 import pytest
+import os
+import tempfile
+import subprocess
+import re
 
 
 
@@ -16,6 +20,29 @@ def src(numpy, benchmark, request):
     assert a.dtype == numpy.dtype("bool")
     return a
 
+@pytest.fixture()
+def srctxt(numpy, src):
+    fd, name = tempfile.mkstemp(suffix="csv",
+                                prefix="dtbench%d_" % src.shape[0])
+    f = os.fdopen(fd, "wb")
+    numpy.savetxt(f, src, fmt="%r")
+    f.close()
+    return name
+
+class ManualTimer(object):
+
+    def __init__(self):
+        self._elapsed_time = 0
+
+    def __call__(self):
+        t = self._elapsed_time
+        self._elapsed_time = 0
+        return t
+
+    def set(self, t):
+        self._elapsed_time += t
+
+
 
 def test_datatable(benchmark, src):
     benchmark.name = "datatable"
@@ -24,10 +51,10 @@ def test_datatable(benchmark, src):
     benchmark(lambda: d(sort=0))
 
 
-def test_numpy(benchmark, src):
+def test_numpy(benchmark, src, numpy):
     benchmark.name = "numpy"
     a = src
-    benchmark(lambda: a.sort())
+    benchmark(lambda: numpy.sort(a))
 
 
 def test_pandas(benchmark, src, pandas):
@@ -40,6 +67,32 @@ def test_python(benchmark, src):
     benchmark.name = "python"
     p = src.tolist()
     benchmark(lambda: sorted(p))
+
+
+def test_rdatatable(benchmark, srctxt):
+    mm = re.search(r"dtbench(\d+)_", srctxt)
+    n = int(mm.group(1))
+    iters = max(int(1000000 / n), 1)
+    timer = ManualTimer()
+    script = ("require(data.table);"
+              "DT=fread('%s',header=F);"
+              "bb=%d;"
+              "tt=system.time(for(b in 1:bb){DC=copy(DT);setkey(DC,V1)});"
+              "te=as.numeric(tt['elapsed'])/bb;"
+              "cat(paste('Time taken:', te, '\\\\n'));"
+              % (srctxt, iters))
+
+    def run():
+        out = subprocess.check_output(["R", "-e", script]).decode()
+        mm = re.search(r"Time taken: (\d*(?:\.\d*)?(?:[eE][-+]?\d+)?)", out)
+        if mm:
+            timer.set(float(mm.group(1)))
+        else:
+            raise RuntimeError(out)
+
+    benchmark.name = "R data.table"
+    benchmark._timer = timer
+    benchmark.pedantic(run, rounds=5, iterations=1)
 
 
 # def test_h2o(benchmark, src, h2o):
