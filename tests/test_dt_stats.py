61f5ec962e5ef321de0c70ca363c8281667edd93
Pasha Stetsenko <pasha@h2o.ai>
2017-09-06 15:08:57 -0700

    Produce correct results for mean/sd when columns contain infinite values; template-ize functions for computing stats

--- tests/test_dt_stats.py
+++ tests/test_dt_stats.py
@@ -3,6 +3,8 @@
 import pytest
 import datatable as dt
 import statistics
+from math import inf, nan
+from tests import list_equals
 
 
 # Sets of tuples containing test columns of each type
@@ -20,11 +22,12 @@ dt_int = {(5, -3, 6, 3, 0),
           }
 
 dt_float = {(9.5, 0.2, 5.4857301, -3.14159265358979),
-            (1.1, 2.3e12, -.5, None, float("inf"), 0.0)}
+            (1.1, 2.3e12, -.5, None, inf, 0.0)}
 
 dt_all = dt_bool | dt_int | dt_float
 
-# Helper function that provides the resulting stype after `min()` or `max()` is called
+# Helper function that provides the resulting stype after `min()` or `max()` is
+# called
 def min_max_stype(stype):
     if stype in ["i1b"]:
         return "i1b"
@@ -34,7 +37,8 @@ def min_max_stype(stype):
         return "f8r"
     return stype
 
-# Helper function that provides the resulting stype after `mean()` or `sd()` is called
+# Helper function that provides the resulting stype after `mean()` or `sd()` is
+# called
 def mean_sd_stype(stype):
     if stype in ["i1i", "i2i", "i4i", "i8i", "f4r", "f8r"]:
         return "f8r"
@@ -87,7 +91,9 @@ def test_dt_max(src):
     for i in range(dt0.ncols):
         assert dt0.names[i] == dtr.names[i]
     assert dtr.topython() == [t_max(src)]
-    
+
+
+
 #-------------------------------------------------------------------------------
 # Mean function dt.mean()
 #-------------------------------------------------------------------------------
@@ -99,7 +105,6 @@ def t_mean(t):
     else:
         return [statistics.mean(t)]
 
-@pytest.mark.skip
 @pytest.mark.parametrize("src", dt_all)
 def test_dt_mean(src):
     dt0 = dt.DataTable(src)
@@ -108,7 +113,29 @@ def test_dt_mean(src):
     assert list(dtr.stypes) == [mean_sd_stype(s) for s in dt0.stypes]
     assert dtr.shape == (1, dt0.shape[1])
     assert dt0.names == dtr.names
-    assert dtr.topython() == [t_mean(src)]
+    assert list_equals(dtr.topython(), [t_mean(src)])
+
+
+@pytest.mark.parametrize("src, res", [([1, 3, 5, None], 3),
+                                      ([], None),
+                                      ([10], 10),
+                                      ([1.5], 1.5),
+                                      ([2, 4], 3),
+                                      ([None, 1.3], 1.3),
+                                      ([None, None, None], None),
+                                      ([inf, inf, inf], inf),
+                                      ([5, 1, inf], inf),
+                                      ([inf, 4, 2], inf),
+                                      ([-inf, -inf, -inf, -inf], -inf),
+                                      ([-inf, 0, inf], nan),
+                                      ])
+def test_dt_mean_special_cases(src, res):
+    dt0 = dt.DataTable(src)
+    dtr = dt0.mean()
+    assert dtr.internal.check()
+    assert list_equals(dtr.topython(), [[res]])
+
+
 
 #-------------------------------------------------------------------------------
 # Standard Deviation function dt.sd()
@@ -116,12 +143,13 @@ def test_dt_mean(src):
 
 def t_sd(t):
     t = [i for i in t if i is not None and not isinstance(i, bool)]
-    if len(t) < 2:
+    if len(t) == 0:
         return [None]
+    elif len(t) == 1:
+        return [0.0]
     else:
         return [statistics.stdev(t)]
 
-@pytest.mark.skip
 @pytest.mark.parametrize("src", dt_all)
 def test_dt_sd(src):
     dt0 = dt.DataTable(src)
@@ -130,5 +158,18 @@ def test_dt_sd(src):
     assert list(dtr.stypes) == [mean_sd_stype(s) for s in dt0.stypes]
     assert dtr.shape == (1, dt0.shape[1])
     assert dt0.names == dtr.names
-    assert dtr.topython() == [t_sd(src)]
+    assert list_equals(dtr.topython(), [t_sd(src)])
 
+
+@pytest.mark.parametrize("src, res", [([1, 3, 5, None], 2.0),
+                                      ([], None),
+                                      ([3.0], 0),
+                                      ([None, None, None], None),
+                                      ([inf, inf, inf], nan),
+                                      ([-inf, 0, inf], nan),
+                                      ])
+def test_dt_sd_special_cases(src, res):
+    dt0 = dt.DataTable(src)
+    dtr = dt0.sd()
+    assert dtr.internal.check()
+    assert list_equals(dtr.topython(), [[res]])
