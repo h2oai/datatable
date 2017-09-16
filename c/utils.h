f991df69d99f4d53d22a549e4e8ff8aec12c9c28
Pasha Stetsenko <pasha@h2o.ai>
2017-09-15 18:28:29 -0700

    Updating fread.c

--- c/utils.h
+++ c/utils.h
@@ -5,6 +5,7 @@
 #define dt_UTILS_H
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <errno.h>   // errno
 #include <string.h>  // strerr
 
@@ -157,11 +158,11 @@ void* _dt_calloc(size_t n, size_t size);
 void* _dt_malloc(size_t n);
 void* _dt_realloc(void *ptr, size_t n);
 void  _dt_free(void *ptr);
+
 #ifdef __cplusplus
     #define null NULL
 #else
     #define null NULL
-    #define bool _Bool
 #endif
 
 
