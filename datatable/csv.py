40854b390059be1c20bbfa5a51b4fca5799da09e
Pasha Stetsenko <pasha@h2o.ai>
2017-09-22 18:19:13 -0700

    Add parameter nthreads to the Python write_csv function; and add documentation

--- datatable/csv.py
+++ datatable/csv.py
@@ -4,23 +4,35 @@ import _datatable
 import os
 
 
-def write_csv(dt, path=""):
+def write_csv(dt, path="", nthreads=0):
     """
     Write the DataTable into the provided file in CSV format.
+
+    Parameters
+    ----------
+    path: str
+        Path to the output CSV file that will be created. If the file already
+        exists, it will be overwritten. If path is not given, then the DataTable
+        will be serialized into a string, and that string will be returned.
+
+    nthreads: int
+        How many threads to use for writing. The value of 0 means to use all
+        available threads. Negative values mean to use that many threads less
+        than the maximum available.
     """
     if path.startswith("~"):
         path = os.path.expanduser(path)
-    writer = CsvWriter(dt, path)
+    writer = CsvWriter(dt, path, nthreads)
     return writer.write()
 
 
 
 class CsvWriter(object):
 
-    def __init__(self, datatable, path):
+    def __init__(self, datatable, path, nthreads=0):
         self.datatable = datatable.internal
         self.column_names = datatable.names
-        self.nthreads = 0
+        self.nthreads = nthreads
         self.path = path
 
     def write(self):
