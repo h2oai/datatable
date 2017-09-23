26390a7100a8e9003110ec54d77fe05204d1b6b2
Pasha Stetsenko <pasha@h2o.ai>
2017-09-22 20:30:14 -0700

    Add a test

---
+++ tests/test_tocsv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Copyright 2017 H2O.ai; Apache License Version 2.0;  -*- encoding: utf-8 -*-
+import datatable as dt
+
+
+def test_issue365():
+    """
+    This is to verify that all data written to CSV can be safely read back,
+    and that no rows were accidentally omitted.
+    """
+    # 999983 is prime; we want to have the number of rows not evenly divisible
+    # by the number of threads.
+    d0 = dt.DataTable({"A": list(range(999983)), "B": [5] * 999983})
+    d1 = dt.fread(text=d0.to_csv())
+    assert d1.shape == d0.shape
+    assert d1.names == d0.names
+    assert d1.types == d0.types
+    assert d1.topython() == d0.topython()
