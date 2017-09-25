b0c610d567d6da7f123b0a54c8acb5b1f0b08575
Pasha Stetsenko <pasha@h2o.ai>
2017-09-24 23:09:12 -0700

    Add memory-map writer

--- microbench/writecsv/writers.cpp
+++ microbench/writecsv/writers.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>   // srand, rand
 #include <string.h>   // strerror, strlen
 #include <fcntl.h>    // O_WRONLY, O_CREAT
+#include <sys/mman.h> // mmap
 #include <time.h>     // time
 #include <unistd.h>   // write, close, lseek
 #include <omp.h>
@@ -49,6 +50,8 @@ static void write_int64(char **pch, int64_t value) {
   *ch = value + '0';
   *pch = ch + 1;
 }
+
+
 
 
 // Plain write into the file from within the #ordered section.
@@ -159,9 +162,75 @@ void kernel_seek(const char *filename, int64_t *data, int64_t nrows)
 }
 
 
+void kernel_mmap(const char *filename, int64_t *data, int64_t nrows)
+{
+  int64_t bytes_total = nrows * 5 * 20;
+  int64_t rows_per_chunk = 20000;
+  int64_t nchunks = nrows / rows_per_chunk;
+  int64_t bytes_per_chunk = bytes_total / nchunks;
+
+  int fd = open(filename, O_RDWR|O_CREAT, 0666);
+  if (fd == -1) {
+    printf("Unable to create file %s: %s", filename, strerror(errno));
+    return;
+  }
+  size_t allocsize = static_cast<size_t>(bytes_total * 1.25);
+  lseek(fd, allocsize, SEEK_SET);
+  write(fd, filename, 1);  // doesn't matter what to write
+
+  char *buf = static_cast<char*>(mmap(NULL, allocsize, PROT_WRITE|PROT_READ, MAP_SHARED, fd, 0));
+  close(fd);
+  if (buf == MAP_FAILED) {
+    printf("Memory map failed!\n");
+    return;
+  }
+  size_t bytes_written = 0;
+
+  int nth = omp_get_num_threads();
+  #pragma omp parallel num_threads(nth)
+  {
+    char *mybuff = new char[bytes_per_chunk];
+    int64_t write_size = 0;
+    int64_t write_at = 0;
+
+    #pragma omp for ordered schedule(dynamic)
+    for (int64_t start = 0; start < nrows; start += rows_per_chunk) {
+      if (write_size) {
+        memcpy(buf + write_at, mybuff, write_size);
+        write_size = 0;
+      }
+
+      int64_t end = start + rows_per_chunk;
+      if (end > nrows) end = nrows;
+      char *mych = mybuff;
+      for (int64_t i = start; i < end; i++) {
+        for (int64_t j = 0; j < 5; j++) {
+          write_int64(&mych, data[i] + j);
+          *mych++ = ',';
+        }
+        mych[-1] = '\n';
+      }
+      #pragma omp ordered
+      {
+        write_size = mych - mybuff;
+        write_at = bytes_written;
+        bytes_written += write_size;
+      }
+    }
+    if (write_size) {
+      memcpy(buf + write_at, mybuff, write_size);
+    }
+
+    delete[] mybuff;
+  }
+  munmap(buf, allocsize);
+}
+
+
 static WKernel kernels[] = {
-  {&kernel_fwrite, "fwrite"},
+  {&kernel_write,  "write"},
   {&kernel_seek,   "seek&write"},
+  {&kernel_mmap,   "memory-map"},
   {NULL, NULL}
 };
 
