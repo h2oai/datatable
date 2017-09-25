44b6427072a29da430c326538cb2328ccdc1a5c6
Pasha Stetsenko <pasha@h2o.ai>
2017-09-25 09:02:45 -0700

    Properly use multithreading in writers.cpp; drop caches between the runs

--- microbench/writecsv/writers.cpp
+++ microbench/writecsv/writers.cpp
@@ -68,7 +68,7 @@ void kernel_write(const char *filename, int64_t *data, int64_t nrows)
   int64_t rows_per_chunk = 20000;
   int64_t nchunks = nrows / rows_per_chunk;
   int64_t bytes_per_chunk = rows_per_chunk * 5 * 20;
-  int nth = omp_get_num_threads();
+  int nth = omp_get_max_threads();
 
   bool stopTeam = false;
   #pragma omp parallel num_threads(nth)
@@ -118,7 +118,7 @@ void kernel_seek(const char *filename, int64_t *data, int64_t nrows)
   int64_t nchunks = nrows / rows_per_chunk;
   int64_t bytes_per_chunk = rows_per_chunk * 5 * 20;
   int64_t bytes_written = 0;
-  int nth = omp_get_num_threads();
+  int nth = omp_get_max_threads();
 
   bool stopTeam = false;
   #pragma omp parallel num_threads(nth)
@@ -189,7 +189,7 @@ void kernel_mmap(const char *filename, int64_t *data, int64_t nrows)
   }
   size_t bytes_written = 0;
 
-  int nth = omp_get_num_threads();
+  int nth = omp_get_max_threads();
   #pragma omp parallel num_threads(nth)
   {
     char *mybuff = new char[bytes_per_chunk];
@@ -239,8 +239,9 @@ void kernel_mmap2(const char *filename, int64_t *data, int64_t nrows)
   int64_t bytes_per_chunk = bytes_total / nchunks;
   size_t allocsize = static_cast<size_t>(bytes_total * 1.25);
 
+  double t0 = now();
   FILE *fp = fopen(filename, "w");
-  fseek(fp, (long)(allocsize - 1), SEEK_SET);
+  fseek(fp, allocsize - 1, SEEK_SET);
   fputc('\0', fp);
   fclose(fp);
 
@@ -256,53 +257,64 @@ void kernel_mmap2(const char *filename, int64_t *data, int64_t nrows)
   if (buf == MAP_FAILED) throw std::runtime_error("Memory map failed!\n");
   size_t bytes_written = 0;
 
-  int nth = omp_get_num_threads();
+  double t1 = now();
+  int nth = omp_get_max_threads();
+  printf("      Using n threads = %d\n", nth);
   #pragma omp parallel num_threads(nth)
   {
-    char *mybuff = new char[bytes_per_chunk];
-    int64_t write_size = 0;
-    int64_t write_at = 0;
+    char *thbuf = new char[bytes_per_chunk];
+    char *tch = thbuf;
+    int64_t th_write_size = 0;
+    int64_t th_write_at = 0;
 
     #pragma omp for ordered schedule(dynamic)
-    for (int64_t start = 0; start < nrows; start += rows_per_chunk) {
-      if (write_size) {
-        memcpy(buf + write_at, mybuff, write_size);
-        write_size = 0;
+    for (int64_t i = 0; i < nchunks; i++) {
+      int64_t row0 = static_cast<int64_t>(i * rows_per_chunk);
+      int64_t row1 = static_cast<int64_t>((i + 1) * rows_per_chunk);
+      if (i == nchunks-1) row1 = nrows;  // always go to the last row for last chunk
+
+      if (th_write_size) {
+        memcpy(buf + th_write_at, thbuf, th_write_size);
+        tch = thbuf;
+        th_write_size = 0;
       }
 
-      int64_t end = start + rows_per_chunk;
-      if (end > nrows) end = nrows;
-      char *mych = mybuff;
-      for (int64_t i = start; i < end; i++) {
-        for (int64_t j = 0; j < 5; j++) {
-          write_int64(&mych, data[i] + j);
-          *mych++ = ',';
+      for (int64_t row = row0; row < row1; row++) {
+        for (int64_t col = 0; col < 5; col++) {
+          write_int64(&tch, data[row] + col);
+          *tch++ = ',';
         }
-        mych[-1] = '\n';
+        tch[-1] = '\n';
       }
       #pragma omp ordered
       {
-        write_size = mych - mybuff;
-        write_at = bytes_written;
-        bytes_written += write_size;
+        th_write_at = bytes_written;
+        th_write_size = static_cast<size_t>(tch - thbuf);
+        bytes_written += th_write_size;
       }
     }
-    if (write_size) {
-      memcpy(buf + write_at, mybuff, write_size);
+    if (th_write_size) {
+      memcpy(buf + th_write_at, thbuf, th_write_size);
     }
 
-    delete[] mybuff;
+    delete[] thbuf;
   }
+  double t2 = now();
   munmap(buf, allocsize);
   truncate(filename, bytes_written);
+  double t3 = now();
+  printf("        %6.3fs Creating file\n", t1-t0);
+  printf("      + %6.3fs Writing data\n", t2-t1);
+  printf("      + %6.3fs Finalizing\n", t3-t2);
+  printf("      = %6.3fs Total\n", t3-t0);
 }
 
 
 static WKernel kernels[] = {
-  {&kernel_write,  "write"},
-  {&kernel_seek,   "seek&write"},
   {&kernel_mmap,   "memorymap1"},
   {&kernel_mmap2,  "memorymap2"},
+  {&kernel_write,  "write"},
+  {&kernel_seek,   "seek&write"},
   {NULL, NULL}
 };
 
@@ -334,18 +346,28 @@ void test_write_methods(int B, int64_t N)
   }
 
   for (int k = 0; k < nkernels; k++) {
-    double total_time = 0;
+    printf("[%d] %s:\n", k, kernels[k].name);
     for (int b = 0; b < B; b++) {
       char filename[20];
       sprintf(filename, "out-%d-%d.csv", k, b);
       double t0 = now();
       kernels[k].kernel(filename, data, N);
       double t1 = now();
-      total_time += t1 - t0;
-      // remove(filename);
+
+      int fd = open(filename, O_RDONLY);
+      struct stat statbuf;
+      fstat(fd, &statbuf);
+      printf("  %7.3fs   %zu B\n", t1-t0, statbuf.st_size);
+      close(fd);
+      remove(filename);
+
+      // Drop caches
+      sync();
+      fd = open("/proc/sys/vm/drop_caches", O_WRONLY);
+      char data = '3';
+      write(fd, &data, 1);
+      close(fd);
     }
-    printf("[%d] %-*s: %7.3f s\n",
-           k, maxnamelen, kernels[k].name, total_time/B);
   }
 
   delete[] data;
