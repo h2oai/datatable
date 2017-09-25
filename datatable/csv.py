b89336d92819caf89b67bb6a013f0a2663e87bcf
Pasha Stetsenko <pasha@h2o.ai>
2017-09-25 13:32:48 -0700

    Add 'decimal' writer for doubles/floats

--- datatable/csv.py
+++ datatable/csv.py
@@ -9,7 +9,7 @@ __all__ = ("write_csv", "CsvWriter")
 
 
 
-def write_csv(dt, path="", nthreads=0, verbose=False):
+def write_csv(dt, path="", nthreads=0, hex=False, verbose=False):
     """
     Write the DataTable into the provided file in CSV format.
 
@@ -24,21 +24,32 @@ def write_csv(dt, path="", nthreads=0, verbose=False):
         How many threads to use for writing. The value of 0 means to use all
         available threads. Negative values mean to use that many threads less
         than the maximum available.
+
+    hex: bool
+        If True, then all floating-point values will be printed in hex format
+        (equivalent to %a format in C `printf`). This format is around 3 times
+        faster to write/read compared to usual decimal representation, so its
+        use is recommended if you need maximum speed.
+
+    verbose: bool
+        If True, some extra information will be printed to the console, which
+        may help to debug the inner workings of the algorithm.
     """
     if path.startswith("~"):
         path = os.path.expanduser(path)
-    writer = CsvWriter(dt, path, nthreads, verbose)
+    writer = CsvWriter(dt, path, nthreads, hex, verbose)
     return writer.write()
 
 
 
 class CsvWriter(object):
 
-    def __init__(self, datatable, path, nthreads=0, verbose=False):
+    def __init__(self, datatable, path, nthreads=0, hex=False, verbose=False):
         self.datatable = datatable.internal
         self.column_names = datatable.names
         self.nthreads = nthreads
         self.path = path
+        self.hex = hex
         self.verbose = verbose
         self._log_newline = False
 
