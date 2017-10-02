1bc98bf4ef0f5d12a0230d76bc6ba0ff037b0ab8
Pasha Stetsenko <pasha@h2o.ai>
2017-10-02 10:41:54 -0700

    wip on ftoa

--- c/csv/dtoa.h
+++ c/csv/dtoa.h
@@ -20,7 +20,7 @@
 // Grisu2.
 //
 //
-// ==== Dragonfly ====
+// ==== Dragonfly (float64) ====
 //
 // Let `x` be an IEEE-754 64-bit floating-point number (double), and `v` is the
 // same number reinterpreted as an uint64_t. In order to convert `x` into a
@@ -147,6 +147,31 @@
 //    In practice the range of ε is from 1.1 to 111.9, so we don't have to
 //    consider rounding to 10000 or more.
 //
+//
+// ==== Dragonfly (float32) ====
+//
+// The algorithm for 32-bit IEEE-754 floats is very similar to the algorithm for
+// doubles. It can be described as follows:
+//
+// The input value `x` can be interpreted as a `uint32_t` with 1 bit for sign,
+// 8 bits of exponent, and 23 bits of mantissa:
+//
+//     s = v >> 31               // sign
+//     eᵇ = (v >> 23) & 0xFF     // biased exponent
+//     m = v & ((1 << 23) - 1)   // mantissa
+//     x = (1 + m/2^23) * 2^(eᵇ - 127)
+//       = G * 2^(e + 1 - 32)
+// where
+//     e = eᵇ - 127 = (v >> 23) - 127
+//     G = (2^23 + m) * 2^8 = (v << 8) | (1 << 31)
+//
+// Then
+//     D = G * 2^(eᵇ - 127 + 1) * 10^(8 - E) / 2^32
+//     E = ((3153 + eᵇ*1233) >> 12) - 39
+//     A˜= Floor[2^(eᵇ - 127 + 1) * 10^(8 - E(eᵇ)) + 1/2]
+//
+// With this choice of E(e), the value of D ranges from 1.0e8 to 1.98e9 < 2^32.
+//
 //------------------------------------------------------------------------------
 #ifndef dt_csv_DTOA_H
 #define dt_csv_DTOA_H
@@ -156,12 +181,15 @@ typedef union { double d; uint64_t u; }  _dbl_u64;
 typedef unsigned __int128  uint128_t;
 
 #define F64_SIGN_MASK  0x8000000000000000u
-#define F64_MANT_MASK  0x000FFFFFFFFFFFFFu
-#define F64_1em5       0x3EE4F8B588E368F1u
-#define F64_1e00       0x3FF0000000000000u
-#define F64_1e15       0x430c6bf526340000u
+#define F64_INFINITY   0x7FF0000000000000u
 #define TENp17         100000000000000000
 #define TENp18         1000000000000000000
+
+#define F32_SIGN_MASK  0x80000000
+#define F32_INFINITY   0x7F800000
+#define TENp09         1000000000
+#define TENp10         10000000000
+
 
 static const int64_t DIVS64[19] = {
   1L,
@@ -183,6 +211,19 @@ static const int64_t DIVS64[19] = {
   10000000000000000L,
   100000000000000000L,
   1000000000000000000L,
+};
+
+static const int32_t DIVS32[11] = {
+  1,
+  10,
+  100,
+  1000,
+  10000,
+  100000,
+  1000000,
+  10000000,
+  100000000,
+  1000000000,
 };
 
 
@@ -715,6 +756,84 @@ static const uint64_t Atable64[2048] = {
   0x063cac186ba81c61, 0x0c795830d75038c2, 0x18f2b061aea07184, 0x04fd5679efb9b04e,
 };
 
+//
+//    from fractions import Fraction
+//    for eb in range(256):
+//       EE = 8 - ((3153 + eb*1233) >> 12) + 39
+//       p1 = 10**EE if EE >= 0 else Fraction(1, 10**(-EE))
+//       ee = eb - 127 + 1
+//       p2 = 2**ee if ee >= 0 else Fraction(1, 2**(-ee))
+//       p = p1 * p2
+//       assert 2**27 < p < 2**32, "p is out of range: %r" % p
+//       print("0x%08x, " % round(p), end=("\n" if eb % 4 == 3 else ""))
+//
+static const uint32_t Atable32[256] = {
+  0x46109ecf, 0x0e0352f6, 0x1c06a5ec, 0x380d4bd9,
+  0x701a97b1, 0x166bb7f0, 0x2cd76fe1, 0x59aedfc1,
+  0x11efc65a, 0x23df8cb4, 0x47bf1967, 0x0e596b7b,
+  0x1cb2d6f6, 0x3965adec, 0x72cb5bd8, 0x16f578c5,
+  0x2deaf18a, 0x5bd5e314, 0x125dfa37, 0x24bbf46e,
+  0x4977e8dc, 0x0eb194f9, 0x1d6329f2, 0x3ac653e4,
+  0x758ca7c7, 0x178287f5, 0x2f050fe9, 0x5e0a1fd2,
+  0x12ced32a, 0x259da654, 0x4b3b4ca8, 0x0f0bdc22,
+  0x1e17b843, 0x3c2f7087, 0x0c097ce8, 0x1812f9cf,
+  0x3025f39f, 0x604be73e, 0x13426173, 0x2684c2e6,
+  0x4d0985cb, 0x0f684df5, 0x1ed09beb, 0x3da137d6,
+  0x0c537191, 0x18a6e322, 0x314dc645, 0x629b8c89,
+  0x13b8b5b5, 0x27716b6a, 0x4ee2d6d4, 0x0fc6f7c4,
+  0x1f8def88, 0x3f1bdf10, 0x0c9f2c9d, 0x193e593a,
+  0x327cb273, 0x64f964e7, 0x1431e0fb, 0x2863c1f6,
+  0x50c783ec, 0x1027e72f, 0x204fce5e, 0x409f9cbc,
+  0x0cecb8f2, 0x19d971e5, 0x33b2e3ca, 0x6765c794,
+  0x14adf4b7, 0x295be96e, 0x52b7d2dd, 0x108b2a2c,
+  0x21165458, 0x422ca8b1, 0x0d3c21bd, 0x1a78437a,
+  0x34f086f4, 0x69e10de7, 0x152d02c8, 0x2a5a0590,
+  0x54b40b20, 0x10f0cf06, 0x21e19e0d, 0x43c33c19,
+  0x0d8d726b, 0x1b1ae4d7, 0x3635c9ae, 0x6c6b935c,
+  0x15af1d79, 0x2b5e3af1, 0x56bc75e3, 0x1158e461,
+  0x22b1c8c1, 0x45639182, 0x0de0b6b4, 0x1bc16d67,
+  0x3782dacf, 0x6f05b59d, 0x16345786, 0x2c68af0c,
+  0x58d15e17, 0x11c37938, 0x2386f270, 0x470de4e0,
+  0x0e35fa93, 0x1c6bf526, 0x38d7ea4c, 0x71afd499,
+  0x16bcc41f, 0x2d79883d, 0x5af3107a, 0x12309ce5,
+  0x246139ca, 0x48c27395, 0x0e8d4a51, 0x1d1a94a2,
+  0x3a352944, 0x746a5288, 0x174876e8, 0x2e90edd0,
+  0x5d21dba0, 0x12a05f20, 0x2540be40, 0x4a817c80,
+  0x0ee6b280, 0x1dcd6500, 0x3b9aca00, 0x0bebc200,
+  0x17d78400, 0x2faf0800, 0x5f5e1000, 0x1312d000,
+  0x2625a000, 0x4c4b4000, 0x0f424000, 0x1e848000,
+  0x3d090000, 0x0c350000, 0x186a0000, 0x30d40000,
+  0x61a80000, 0x13880000, 0x27100000, 0x4e200000,
+  0x0fa00000, 0x1f400000, 0x3e800000, 0x0c800000,
+  0x19000000, 0x32000000, 0x64000000, 0x14000000,
+  0x28000000, 0x50000000, 0x10000000, 0x20000000,
+  0x40000000, 0x0ccccccd, 0x1999999a, 0x33333333,
+  0x66666666, 0x147ae148, 0x28f5c28f, 0x51eb851f,
+  0x10624dd3, 0x20c49ba6, 0x4189374c, 0x0d1b7176,
+  0x1a36e2eb, 0x346dc5d6, 0x68db8bac, 0x14f8b589,
+  0x29f16b12, 0x53e2d624, 0x10c6f7a1, 0x218def41,
+  0x431bde83, 0x0d6bf94d, 0x1ad7f29b, 0x35afe535,
+  0x6b5fca6b, 0x15798ee2, 0x2af31dc4, 0x55e63b89,
+  0x112e0be8, 0x225c17d0, 0x44b82fa1, 0x0dbe6fed,
+  0x1b7cdfda, 0x36f9bfb4, 0x6df37f67, 0x15fd7fe1,
+  0x2bfaffc3, 0x57f5ff86, 0x11979981, 0x232f3302,
+  0x465e6605, 0x0e12e134, 0x1c25c268, 0x384b84d1,
+  0x709709a1, 0x16849b87, 0x2d09370d, 0x5a126e1b,
+  0x1203af9f, 0x24075f3e, 0x480ebe7c, 0x0e69594c,
+  0x1cd2b298, 0x39a56530, 0x734aca5f, 0x170ef546,
+  0x2e1dea8d, 0x5c3bd519, 0x12725dd2, 0x24e4bba4,
+  0x49c97747, 0x0ec1e4a8, 0x1d83c950, 0x3b07929f,
+  0x760f253f, 0x179ca10d, 0x2f394219, 0x5e728432,
+  0x12e3b40a, 0x25c76814, 0x4b8ed028, 0x0f1c9008,
+  0x1e392010, 0x3c724020, 0x0c16d9a0, 0x182db340,
+  0x305b6680, 0x60b6cd00, 0x1357c29a, 0x26af8533,
+  0x4d5f0a67, 0x0f79687b, 0x1ef2d0f6, 0x3de5a1ec,
+  0x0c612062, 0x18c240c5, 0x31848189, 0x63090313,
+  0x13ce9a37, 0x279d346e, 0x4f3a68dc, 0x0fd87b5f,
+  0x1fb0f6be, 0x3f61ed7d, 0x0cad2f7f, 0x195a5eff,
+  0x32b4bdfd, 0x65697bfb, 0x14484bff, 0x289097fe,
+};
+
 
 inline void dtoa(char **pch, double dvalue)
 {
@@ -727,7 +846,7 @@ inline void dtoa(char **pch, double dvalue)
   }
   int eb = static_cast<int>(value >> 52);  // biased exponent
   if (eb == 0x7FF) {
-    if ((value & F64_MANT_MASK) == 0) {  // don't print nans at all
+    if (value == F64_INFINITY) {  // don't print nans at all
       ch[0] = 'i'; ch[1] = 'n'; ch[2] = 'f';
       *pch = ch + 3;
     }
