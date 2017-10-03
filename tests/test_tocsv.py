c70172b844c81425f9a0f1c53f2e26f7cf7c9f29
Pasha Stetsenko <pasha@h2o.ai>
2017-10-03 09:18:22 -0700

    Add more tests for csv writer

--- tests/test_tocsv.py
+++ tests/test_tocsv.py
@@ -4,6 +4,13 @@ import datatable as dt
 from tests import list_equals
 
 
+def test_save_simple():
+    d = dt.DataTable([[1, 4, 5], [True, False, None], ["foo", None, "bar"]],
+                     colnames=["A", "B", "C"])
+    out = d.to_csv()
+    assert out == "A,B,C\n1,1,foo\n4,0,\n5,,bar\n"
+
+
 def test_issue365():
     """
     This is to verify that all data written to CSV can be safely read back,
@@ -17,13 +24,6 @@ def test_issue365():
     assert d1.names == d0.names
     assert d1.types == d0.types
     assert d1.topython() == d0.topython()
-
-
-def test_save_simple():
-    d = dt.DataTable([[1, 4, 5], [True, False, None], ["foo", None, "bar"]],
-                     colnames=["A", "B", "C"])
-    out = d.to_csv()
-    assert out == "A,B,C\n1,1,foo\n4,0,\n5,,bar\n"
 
 
 def test_write_spacestrs():
@@ -41,9 +41,55 @@ def test_write_spacenames():
     assert d.topython() == dd.topython()
 
 
-def test_save_floats():
-    src = [0.0, -0.0, 1.5, 0.0034876143, 10.3074, 83476101.13487,
-           34981703410983.12, -3.232e-8, -4.241e+67]
+def test_save_bool():
+    d = dt.DataTable([True, False, None, None, False, False, True, False])
+    assert d.types == ("bool", )
+    assert d.to_csv() == "C1\n1\n0\n\n\n0\n0\n1\n0\n"
+
+
+def test_save_int8():
+    d = dt.DataTable([1, 0, -5, None, 127, -127, 99, 10, 100, -100, -10])
+    assert d.stypes == ("i1i", )
+    assert d.to_csv() == "C1\n1\n0\n-5\n\n127\n-127\n99\n10\n100\n-100\n-10\n"
+
+
+def test_save_int16():
+    d = dt.DataTable([0, 10, 100, 1000, 10000, 32767, -32767, -10, -20,
+                      5, 5125, 3791, 21, -45, 499, 999, 1001, -8, None, -1])
+    assert d.stypes == ("i2i", )
+    assert d.to_csv() == "C1\n0\n10\n100\n1000\n10000\n32767\n-32767\n" \
+                         "-10\n-20\n5\n5125\n3791\n21\n-45\n499\n999\n" \
+                         "1001\n-8\n\n-1\n"
+
+
+def test_save_int32():
+    d = dt.DataTable([0, None, 1, 10, 100, 1000, 1000000, 1000000000,
+                      2147483647, -2147483647, -25, 111, 215932, -1,
+                      None, 34857304, 999999, -1048573, 123456789])
+    assert d.stypes == ("i4i", )
+    assert d.to_csv() == "C1\n0\n\n1\n10\n100\n1000\n1000000\n1000000000\n" \
+                         "2147483647\n-2147483647\n-25\n111\n215932\n-1\n" \
+                         "\n34857304\n999999\n-1048573\n123456789\n"
+
+
+def test_save_int64():
+    d = dt.DataTable([0, None, 1, 10, 100, 1000, 1000000, 1000000000,
+                      10000000000, 1000000000000000, 1000000000000000000,
+                      9223372036854775807, -9223372036854775807, None,
+                      123456789876543210, -245253356, 328365213640134,
+                      99999999999, 10000000000001, 33, -245, 3])
+    assert d.stypes == ("i8i", )
+    assert d.to_csv() == ("C1\n0\n\n1\n10\n100\n1000\n1000000\n1000000000\n"
+                          "10000000000\n1000000000000000\n1000000000000000000\n"
+                          "9223372036854775807\n-9223372036854775807\n\n"
+                          "123456789876543210\n-245253356\n328365213640134\n"
+                          "99999999999\n10000000000001\n33\n-245\n3\n")
+
+
+def test_save_double():
+    src = ([0.0, -0.0, 1.5, 0.0034876143, 10.3074, 83476101.13487,
+            34981703410983.12, -3.232e-8, -4.241e+67] +
+           [10**p for p in range(309)])
     d = dt.DataTable(src)
     dd = dt.fread(text=d.to_csv())
     assert list_equals(d.topython()[0], dd.topython()[0])
