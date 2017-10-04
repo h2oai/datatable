47ab31f2549f58dd285c929984ca83acb63c390c
mmalohlava <michal@h2o.ai>
2017-10-04 15:38:01 -0700

    Proper name of library

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ os.environ["ARCHFLAGS"] = "-m64"
 # If we need to install llvmlite, this would help
 os.environ["LLVM_CONFIG"] = llvm_config
 # Bundle LLVM libraries as extra files in resulting wheel
-extra_libs = ["libomp", "libc++"]
+extra_libs = ["libomp", "libc++.1"]
 extra_data_files = [ ('lib', ["%s/%s.%s" % (libs, name, lib_ext) for name in extra_libs]) ]
 print(extra_data_files)
 
