01fa70a44d06d201462c26a491c7572b2c765e31
Pasha Stetsenko <pasha@h2o.ai>
2017-11-27 09:19:50 -0800

    Fix bug where opening a DT with an empty string column would result in a crash

--- tests/test_nff.py
+++ tests/test_nff.py
@@ -2,7 +2,7 @@
 # Copyright 2017 H2O.ai; Apache License Version 2.0;  -*- encoding: utf-8 -*-
 import shutil
 import tempfile
-import datatable
+import datatable as dt
 from tests import assert_equals
 
 
@@ -13,10 +13,23 @@ from tests import assert_equals
 
 def test_save_and_load():
     dir1 = tempfile.mkdtemp()
-    dt0 = datatable.DataTable({"A": [1, 7, 100, 12],
-                               "B": [True, None, False, None],
-                               "C": ["alpha", "beta", None, "delta"]})
-    datatable.save(dt0, dir1)
-    dt1 = datatable.open(dir1)
+    dt0 = dt.DataTable({"A": [1, 7, 100, 12],
+                        "B": [True, None, False, None],
+                        "C": ["alpha", "beta", None, "delta"]})
+    dt.save(dt0, dir1)
+    dt1 = dt.open(dir1)
+    assert_equals(dt0, dt1)
+    shutil.rmtree(dir1)
+
+
+def test_empty_string_col():
+    """
+    Test that DataTable with an empty string column can be saved/opened.
+    See #604
+    """
+    dir1 = tempfile.mkdtemp()
+    dt0 = dt.DataTable([[1, 2, 3], ["", "", ""]])
+    dt.save(dt0, dir1)
+    dt1 = dt.open(dir1)
     assert_equals(dt0, dt1)
     shutil.rmtree(dir1)
