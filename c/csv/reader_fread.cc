3a04f9de0e28067102d07ba5f29b088ae1e346b4
Pasha Stetsenko <pasha@h2o.ai>
2018-02-21 13:49:13 -0800

    Add several more assertions

--- c/csv/reader_fread.cc
+++ c/csv/reader_fread.cc
@@ -468,6 +468,7 @@ void FreadLocalParseContext::orderBuffer() {
     WritableBuffer* wb = columns[i].strdata;
     size_t write_at = wb->prep_write(sz, strbufs[k].mbuf->get());
     strbufs[k].ptr = write_at;
+    strbufs[k].sz = sz;
   }
 }
 
@@ -488,6 +489,7 @@ void FreadLocalParseContext::push_buffers() {
       field64* lo = tbuf + strbufs[k].idx8;
       int32_t lastOffset = lo[(used_nrows - 1) * tbuf_ncols].str32.offset;
       size_t sz = static_cast<size_t>(abs(lastOffset)) - 1;
+      assert(sz == strbufs[k].sz);
 
       wb->write_at(ptr, sz, strbufs[k].mbuf->get());
 
