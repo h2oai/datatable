1f2a57eb7ae13993880679d5a47b571b1a7e78ea
mmalohlava <michal@h2o.ai>
2018-07-06 15:49:21 -0700

    Skip tests

--- ci/install-capnproto.sh
+++ ci/install-capnproto.sh
@@ -8,7 +8,7 @@ curl -O https://capnproto.org/capnproto-c++-${VERSION}.tar.gz
 tar zxf capnproto-c++-${VERSION}.tar.gz
 cd capnproto-c++-${VERSION}
 ./configure
-make -j6 check
+make -j32 all
 SUDO=
 if [ $(id -u) -ne 0 ]; then
     SUDO=sudo
