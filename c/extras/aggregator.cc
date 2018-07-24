2c3bb20086e3ad6d63bf56e665b21a1431dd3764
oleksiyskononenko <35204136+oleksiyskononenko@users.noreply.github.com>
2018-07-23 18:21:59 -0700

    Handling the constant columns properly (#1193)
    - all the constant continuous columns are now normalized to 0.5 preventing the distance from blowing up;
    - refactoring based on comments to #1186;

--- c/extras/aggregator.cc
+++ c/extras/aggregator.cc
@@ -93,7 +93,7 @@ void Aggregator::aggregate_exemplars(DataTable* dt_exemplars, DataTablePtr& dt_m
   cols_weights[0] = Column::new_data_column(SType::INT32, static_cast<int64_t>(gb_members.ngroups()));
   cols_weights[1] = nullptr;
   dt_weights = new DataTable(cols_weights);
-  int32_t* d_weights = static_cast<int32_t*>(dt_weights->columns[0]->data_w());
+  auto d_weights = static_cast<int32_t*>(dt_weights->columns[0]->data_w());
   std::memset(d_weights, 0, static_cast<size_t>(gb_members.ngroups()) * sizeof(int32_t));
 
   // Setting up exemplar indices and weights
@@ -160,34 +160,34 @@ void Aggregator::group_2d(DataTablePtr& dt_exemplars, DataTablePtr& dt_members) 
 
 
 void Aggregator::group_1d_continuous(DataTablePtr& dt_exemplars, DataTablePtr& dt_members) {
-  RealColumn<double>* c0 = static_cast<RealColumn<double>*>(dt_exemplars->columns[0]);
-  const double* d_c0 = static_cast<const double*>(dt_exemplars->columns[0]->data());
-  int32_t* d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
+  auto c0 = static_cast<RealColumn<double>*>(dt_exemplars->columns[0]);
+  const double* d_c0 = c0->elements_r();
+  auto d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
 
-  // TODO: handle the case when the column is constant, i.e. min = max
-  double norm_factor = n_bins * (1 - epsilon) / (c0->max() - c0->min());
+  double norm_factor, norm_shift;
+  set_norm_coeffs(norm_factor, norm_shift, c0->min(), c0->max(), n_bins);
 
   for (int32_t i = 0; i < dt_exemplars->nrows; ++i) {
-    d_cg[i] = static_cast<int32_t>(norm_factor * (d_c0[i] - c0->min()));
+    d_cg[i] = static_cast<int32_t>(norm_factor * d_c0[i] + norm_shift);
   }
 }
 
 
 void Aggregator::group_2d_continuous(DataTablePtr& dt_exemplars, DataTablePtr& dt_members) {
-  RealColumn<double>* c0 = static_cast<RealColumn<double>*>(dt_exemplars->columns[0]);
-  RealColumn<double>* c1 = static_cast<RealColumn<double>*>(dt_exemplars->columns[1]);
+  auto c0 = static_cast<RealColumn<double>*>(dt_exemplars->columns[0]);
+  auto c1 = static_cast<RealColumn<double>*>(dt_exemplars->columns[1]);
+  double* d_c0 = c0->elements_w();
+  double* d_c1 = c1->elements_w();
+  auto d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
 
-  double* d_c0 = static_cast<double*>(dt_exemplars->columns[0]->data_w());
-  double* d_c1 = static_cast<double*>(dt_exemplars->columns[1]->data_w());
-  int32_t* d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
-
-  // TODO: handle the case when one of the columns or the both are constant, i.e. min = max
-  double normx_factor = nx_bins * (1 - epsilon) / (c0->max() - c0->min());
-  double normy_factor = ny_bins * (1 - epsilon) / (c1->max() - c1->min());
+  double normx_factor, normx_shift;
+  double normy_factor, normy_shift;
+  set_norm_coeffs(normx_factor, normx_shift, c0->min(), c0->max(), nx_bins);
+  set_norm_coeffs(normy_factor, normy_shift, c1->min(), c1->max(), ny_bins);
 
   for (int32_t i = 0; i < dt_exemplars->nrows; ++i) {
-    d_cg[i] = static_cast<int32_t>(normy_factor * (d_c1[i] - c1->min())) * nx_bins +
-              static_cast<int32_t>(normx_factor * (d_c0[i] - c0->min()));
+    d_cg[i] = static_cast<int32_t>(normy_factor * d_c1[i] + normy_shift) * nx_bins +
+              static_cast<int32_t>(normx_factor * d_c0[i] + normx_shift);
   }
 }
 
@@ -200,12 +200,13 @@ void Aggregator::group_1d_categorical(DataTablePtr& dt_exemplars, DataTablePtr& 
   RowIndex ri0 = dt_exemplars->sortby(cols, &grpby0);
   const int32_t* group_indices_0 = ri0.indices32();
 
-  int32_t* d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
+  auto d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
   const int32_t* offsets0 = grpby0.offsets_r();
 
   for (size_t i = 0; i < grpby0.ngroups(); ++i) {
+    auto g_id = static_cast<int32_t>(i);
     for (int32_t j = offsets0[i]; j < offsets0[i+1]; ++j) {
-      d_cg[group_indices_0[j]] = static_cast<int32_t>(i);
+      d_cg[group_indices_0[j]] = g_id;
     }
   }
 }
@@ -224,19 +225,21 @@ void Aggregator::group_2d_categorical(DataTablePtr& dt_exemplars, DataTablePtr& 
   RowIndex ri1 = dt_exemplars->sortby(cols, &grpby1);
   const int32_t* group_indices_1 = ri1.indices32();
 
-  int32_t* d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
+  auto d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
   const int32_t* offsets0 = grpby0.offsets_r();
   const int32_t* offsets1 = grpby1.offsets_r();
 
   for (size_t i = 0; i < grpby0.ngroups(); ++i) {
+    auto g_id = static_cast<int32_t>(i);
     for (int32_t j = offsets0[i]; j < offsets0[i+1]; ++j) {
-      d_cg[group_indices_0[j]] = static_cast<int32_t>(i);
+      d_cg[group_indices_0[j]] = g_id;
     }
   }
 
   for (size_t i = 0; i < grpby1.ngroups(); ++i) {
+    auto g_id = static_cast<int32_t>(grpby0.ngroups() * i);
     for (int32_t j = offsets1[i]; j < offsets1[i+1]; ++j) {
-      d_cg[group_indices_1[j]] += static_cast<int32_t>(grpby0.ngroups() * i);
+      d_cg[group_indices_1[j]] += g_id;
     }
   }
 }
@@ -250,16 +253,19 @@ void Aggregator::group_2d_mixed(bool cont_index, DataTablePtr& dt_exemplars, Dat
   RowIndex ri0 = dt_exemplars->sortby(cols, &grpby0);
   const int32_t* group_indices_0 = ri0.indices32();
 
-  RealColumn<double>* c0 = static_cast<RealColumn<double>*>(dt_exemplars->columns[cont_index]);
-  const double* d_c0 = static_cast<const double*>(dt_exemplars->columns[cont_index]->data());
-  int32_t* d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
+  auto c0 = static_cast<RealColumn<double>*>(dt_exemplars->columns[cont_index]);
+  auto d_c0 = c0->elements_r();
+  auto d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
   const int32_t* offsets0 = grpby0.offsets_r();
-  double normx_factor = nx_bins * (1 - epsilon) / (c0->max() - c0->min());
+
+  double normx_factor, normx_shift;
+  set_norm_coeffs(normx_factor, normx_shift, c0->min(), c0->max(), nx_bins);
 
   for (size_t i = 0; i < grpby0.ngroups(); ++i) {
+    int32_t g0_id = nx_bins * static_cast<int32_t>(i);
     for (int32_t j = offsets0[i]; j < offsets0[i+1]; ++j) {
-      d_cg[group_indices_0[j]] = nx_bins * static_cast<int32_t>(i) +
-                                 static_cast<int32_t>(normx_factor * (d_c0[group_indices_0[j]] - c0->min()));
+      d_cg[group_indices_0[j]] = g0_id +
+                                 static_cast<int32_t>(normx_factor * d_c0[group_indices_0[j]] + normx_shift);
     }
   }
 }
@@ -276,7 +282,7 @@ void Aggregator::group_nd(DataTablePtr& dt_exemplars, DataTablePtr& dt_members) 
   double* pmatrix = nullptr;
   std::vector<double*> exemplars;
   double delta, radius = .025 * (dt_exemplars->ncols), distance = 0.0;
-  int32_t* d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
+  auto d_cg = static_cast<int32_t*>(dt_members->columns[0]->data_w());
 
   if (dt_exemplars->ncols > max_dimensions) {
     adjust_radius(dt_exemplars, radius);
@@ -297,7 +303,6 @@ void Aggregator::group_nd(DataTablePtr& dt_exemplars, DataTablePtr& dt_members) 
 
     for (size_t j = 0; j < exemplars.size(); ++j) {
       distance = calculate_distance(member, exemplars[j], ndims, delta);
-
       if (distance < min_distance) {
         min_distance = distance;
         exemplar_id = j;
@@ -326,7 +331,7 @@ void Aggregator::adjust_radius(DataTablePtr& dt_exemplars, double& radius) {
   double diff = 0;
   for (int i = 0; i < dt_exemplars->ncols; ++i) {
     if (dt_exemplars->columns[i]->stype() == SType::FLOAT64) {
-      RealColumn<double>* ci = static_cast<RealColumn<double>*>(dt_exemplars->columns[i]);
+      auto ci = static_cast<RealColumn<double>*>(dt_exemplars->columns[i]);
       diff += (ci->max() - ci->min()) * (ci->max() - ci->min());
     }
   }
@@ -354,10 +359,14 @@ double Aggregator::calculate_distance(double* e1, double* e2, int64_t ndims, dou
 
 void Aggregator::normalize_row(DataTablePtr& dt, double* r, int32_t row_id) {
   for (int32_t i = 0; i < dt->ncols; ++i) {
-    if (dt->columns[i]->stype() == SType::FLOAT64) {
-      RealColumn<double>* c = static_cast<RealColumn<double>*>(dt->columns[i]);
-      const double* d_c = static_cast<const double*>(dt->columns[i]->data());
-      r[i] =  (d_c[row_id] - c->min()) / (c->max() - c->min());
+    Column* c = dt->columns[i];
+    if (c->stype() == SType::FLOAT64) {
+      auto c_real = static_cast<RealColumn<double>*>(c);
+      const double* d_real = c_real->elements_r();
+      double norm_factor, norm_shift;
+
+      set_norm_coeffs(norm_factor, norm_shift, c_real->min(), c_real->max(), 1);
+      r[i] =  norm_factor * d_real[row_id] + norm_shift;
     } else {
       r[i] = 0;
     }
@@ -388,19 +397,52 @@ double* Aggregator::generate_pmatrix(DataTablePtr& dt_exemplars) {
 
 void Aggregator::project_row(DataTablePtr& dt_exemplars, double* r, int32_t row_id, double* pmatrix) {
   std::memset(r, 0, static_cast<size_t>(max_dimensions) * sizeof(double));
+  int32_t n = 0;
 
   for (int32_t i = 0; i < dt_exemplars->ncols; ++i) {
-    if (dt_exemplars->columns[i]->stype() == SType::FLOAT64) {
-      const double* d_c = static_cast<const double*>(dt_exemplars->columns[i]->data());
-      if (!ISNA<double>(d_c[row_id])) {
+    Column* c = dt_exemplars->columns[i];
+    if (c->stype() == SType::FLOAT64) {
+      auto c_real = static_cast<RealColumn<double>*> (c);
+      auto d_real = c_real->elements_r();
 
-        RealColumn<double>* c = static_cast<RealColumn<double>*> (dt_exemplars->columns[i]);
+      if (!ISNA<double>(d_real[row_id])) {
+
+        double norm_factor, norm_shift;
+        set_norm_coeffs(norm_factor, norm_shift, c_real->min(), c_real->max(), 1);
+        double norm_row = norm_factor * d_real[row_id] + norm_shift;
         for (int32_t j = 0; j < max_dimensions; ++j) {
-          //TODO: handle missing values and do r[j] /= n normalization at the end
-          r[j] +=  pmatrix[i * max_dimensions + j] * (d_c[row_id] - c->min()) / (c->max() - c->min());
+          r[j] +=  pmatrix[i * max_dimensions + j] * norm_row;
         }
-
+        ++n;
       }
+
     }
+  }
+
+  for (int32_t j = 0; j < max_dimensions; ++j) {
+    r[j] /= n;
+  }
+}
+
+
+/*
+*  To normalize a continuous column x to [0; 1] range we use
+*  the following formula: x_i_new = (x_i - min) / (max - min),
+*  where x_i is the value stored in the i-th row, max and min are the column
+*  maximum and minimum, respectively. To save on arithmetics, this can be
+*  easily converted to x_i_new = x_i * norm_factor + norm_shift,
+*  where norm_factor = 1 / (max - min) and norm_shift = - min / (max - min).
+*  When max = min, i.e. the column is constant, there is a singularity
+*  that may lead to wrong distance calculations done by the Aggregator.
+*  One of the approaches here is to handle the constant columns separately
+*  and set their values to 0.5, i.e. norm_factor = 0 and norm_shift = 0.5.
+*/
+void Aggregator::set_norm_coeffs(double& norm_factor, double& norm_shift, double c_min, double c_max, int32_t c_bins) {
+  if (fabs(c_max - c_min) > epsilon) {
+    norm_factor = c_bins * (1 - epsilon) / (c_max - c_min);
+    norm_shift = -norm_factor * c_min;
+  } else {
+    norm_factor = 0.0;
+    norm_shift =  0.5 * c_bins;
   }
 }
