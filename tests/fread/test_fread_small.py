99659c80ca075a4e02c077515b18340b76005962
Michal Raška <michalr@h2o.ai>
2018-07-31 19:31:49 +0200

    [BUILD] Set LD_LIBRARY_PATH for CentOS 7 image. (#1210)

--- tests/fread/test_fread_small.py
+++ tests/fread/test_fread_small.py
@@ -140,8 +140,8 @@ def test_float_hex_invalid():
 
 
 def test_float_decimal0():
-    assert dt.fread("1.3485701e-303\n").scalar() == 1.3485701e-303
     if "powerpc64" not in str(sys.implementation):
+        assert dt.fread("1.3485701e-303\n").scalar() == 1.3485701e-303
         assert dt.fread("1.46761e-313\n").scalar() == 1.46761e-313
     assert (dt.fread("A\n1.23456789123456789123456999\n").scalar() ==
             1.23456789123456789123456999)
