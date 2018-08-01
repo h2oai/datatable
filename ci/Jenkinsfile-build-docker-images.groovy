ffc92b010c6a59ac2d0090e2d4b144cff34141a9
Pasha Stetsenko <pasha@h2o.ai>
2018-08-01 11:21:01 -0700

    Add .groovy extension to Jenkins files (#1216)

--- ci/Jenkinsfile-build-docker-images.groovy
+++ ci/Jenkinsfile-build-docker-images.groovy
@@ -1,4 +1,9 @@
 #!/usr/bin/groovy
+//------------------------------------------------------------------------------
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//------------------------------------------------------------------------------
 
 @Library('test-shared-library@mr/ita/190-build-summary-emailer') _
 
