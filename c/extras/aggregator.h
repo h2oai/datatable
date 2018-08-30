63b5026cce70840f302649eb543149dfedcca35c
Oleksiy Kononenko <oleksiy@h2o.ai>
2018-08-30 04:24:17 +0200

    Aggregator openmp (#1247)
    * Added OpenMP support
    * Removed unnecessary shallow copies of the categorical columns in ND.
    * Updated formula for `radius`.
    * Changed `max_dimensions` from 50 to 25
    * Controlling number of exemplars in ND
    
        When number of exemplars found is more than `nd_bins` do the following:
        - find the closest exemplar to the first exemplar;
        - adjust `delta` as `delta += min_distance`;
        - if the distance between two exemplars is less than new `delta` merge
        them and store the mapping info;
        - adjust exemplar's info for all the relevant members at the end.
    
    * Updated help message for the `aggregate` function
    * Added progress reporting through a supplied Python function
    
        - if the function is supplied, it will be used to report the progress
        - if not, some progress information will be printed on the screen
    
    * Do no aggregation if `nrows` is less than or equal to `min_rows`
    * Prevent aggregator from returning too few exemplars
    * Adjust reporting frequency
    * Overall refactoring
    
    Closes #1206 and #1227

--- c/extras/aggregator.h
+++ c/extras/aggregator.h
@@ -13,21 +13,37 @@
 #include <random>
 #include "py_datatable.h"
 
+typedef std::unique_ptr<double[]> DoublePtr;
+struct ex {
+  int64_t id;
+  DoublePtr coords;
+};
+typedef std::unique_ptr<ex> ExPtr;
+
+#define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+#define PBWIDTH 60
+#define PBSTEPS 100
 
 class Aggregator {
   public:
-    Aggregator(int32_t, int32_t, int32_t, int32_t, unsigned int);
+    Aggregator(int32_t, int32_t, int32_t, int32_t, int32_t, int32_t,
+               unsigned int, PyObject*);
     DataTablePtr aggregate(DataTable*);
     static constexpr double epsilon = 1.0e-15;
     static void set_norm_coeffs(double&, double&, double, double, int32_t);
+    static void print_progress(double, int);
 
   private:
+    int32_t min_rows;
     int32_t n_bins;
     int32_t nx_bins;
     int32_t ny_bins;
+    int32_t nd_bins;
     int32_t max_dimensions;
     unsigned int seed;
+    PyObject* progress_fn;
 
+    // Grouping and aggregating functions
     void group_1d(DataTablePtr&, DataTablePtr&);
     void group_2d(DataTablePtr&, DataTablePtr&);
     void group_nd(DataTablePtr&, DataTablePtr&);
@@ -38,16 +54,20 @@ class Aggregator {
     void group_2d_mixed(bool, DataTablePtr&, DataTablePtr&);
     void aggregate_exemplars(DataTable*, DataTablePtr&);
 
-
-    void normalize_row(DataTablePtr&, double*, int32_t);
-    double calculate_distance(double*, double*, int64_t, double);
-    void adjust_radius(DataTablePtr&, double&);
-    double* generate_pmatrix(DataTablePtr&);
-    void project_row(DataTablePtr&, double*, int32_t, double*);
+    // Helper functions
+    DoublePtr generate_pmatrix(DataTablePtr&);
+    void normalize_row(DataTablePtr&, DoublePtr&, int32_t);
+    void project_row(DataTablePtr&, DoublePtr&, int32_t, DoublePtr&);
+    double calculate_distance(DoublePtr&, DoublePtr&, int64_t, double,
+                              bool early_exit=true);
+    void adjust_delta(double&, std::vector<ExPtr>&, std::vector<int64_t>&,
+                      int64_t);
+    void adjust_members(std::vector<int64_t>&, DataTablePtr&);
+    size_t calculate_map(std::vector<int64_t>&, size_t);
+    void progress(double, int status_code=0);
 };
-
 
 DECLARE_FUNCTION(
   aggregate,
-  "aggregate(self, n_bins=500, nx_bins=50, ny_bins=50, max_dimensions=50, seed=0)\n\n",
+  "aggregate(self, min_rows=500, n_bins=500, nx_bins=50, ny_bins=50, nd_bins = 500, max_dimensions=25, seed=0)\n\n",
   dt_EXTRAS_AGGREGATOR_cc)
