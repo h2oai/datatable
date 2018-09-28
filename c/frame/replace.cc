2b8c9119846f8cda152bbeb8a37dbe8d184fadf2
Pasha Stetsenko <pasha@h2o.ai>
2018-09-28 15:51:54 -0700

    Implement replacing for real columns

--- c/frame/replace.cc
+++ c/frame/replace.cc
@@ -14,10 +14,12 @@
 // limitations under the License.
 //------------------------------------------------------------------------------
 #include "frame/py_frame.h"
+#include <type_traits>
 #include <unordered_set>
 #include "python/dict.h"
 #include "python/list.h"
 #include "utils/assert.h"
+#include "utils/progress.h"
 
 namespace py {
 
@@ -88,7 +90,7 @@ class ReplaceAgent {
     std::vector<int8_t> x_bool, y_bool;
     std::vector<int64_t> x_int, y_int;
     std::vector<double> x_real, y_real;
-    std::vector<std::string> x_str, y_str;
+    std::vector<CString> x_str, y_str;
     int64_t xmin_int, xmax_int;
     double xmin_real, xmax_real;
 
@@ -102,8 +104,10 @@ class ReplaceAgent {
     template <typename T> void process_int_column(size_t i);
     template <typename T> void process_real_column(size_t i);
     template <typename T> void process_str_column(size_t i);
-    template <typename T>
-    void replace_in_data(size_t n, T* x, T* y, size_t nrows, T* data);
+    template <typename T> void replace_fw(T* x, T* y, size_t nrows, T* data, size_t n);
+    template <typename T> void replace_fw1(T* x, T* y, size_t nrows, T* data);
+    template <typename T> void replace_fw2(T* x, T* y, size_t nrows, T* data);
+    template <typename T> void replace_fwN(T* x, T* y, size_t nrows, T* data, size_t n);
     bool types_changed() const { return columns_cast; }
 
   private:
@@ -111,6 +115,7 @@ class ReplaceAgent {
     void split_x_y_int();
     void split_x_y_real();
     void split_x_y_str();
+    template <typename T> void check_uniqueness(std::vector<T>&);
 };
 
 
@@ -132,6 +137,8 @@ void Frame::replace(const PKArgs& args) {
       case SType::INT16: ra.process_int_column<int16_t>(i); break;
       case SType::INT32: ra.process_int_column<int32_t>(i); break;
       case SType::INT64: ra.process_int_column<int64_t>(i); break;
+      case SType::FLOAT32: ra.process_real_column<float>(i); break;
+      case SType::FLOAT64: ra.process_real_column<double>(i); break;
       default: break;
     }
   }
@@ -238,7 +245,7 @@ void ReplaceAgent::split_x_y_by_type() {
       case SType::STR32:
       case SType::STR64: {
         if (done_str) continue;
-        // split_x_y_str();
+        split_x_y_str();
         done_str = true;
         break;
       }
@@ -300,37 +307,82 @@ void ReplaceAgent::split_x_y_int() {
     x_int.push_back(GETNA<int64_t>());
     y_int.push_back(na_repl);
   }
-  // Check that the values are unique
-  std::unordered_set<int64_t> check;
-  for (int64_t x : x_int) {
-    if (check.count(x)) {
-      throw ValueError() << "Replacement target `" << x << "` was specified "
-        "more than once in Frame.replace()";
-    }
-    check.insert(x);
-  }
+  check_uniqueness<int64_t>(x_int);
 }
 
 
 void ReplaceAgent::split_x_y_real() {
+  double na_repl = GETNA<double>();
   size_t n = vx.size();
+  xmin_real = std::numeric_limits<double>::max();
+  xmax_real = -xmin_real;
   for (size_t i = 0; i < n; ++i) {
     py::obj xelem = vx[i];
     py::obj yelem = vy[i];
     if (xelem.is_none()) {
-      if (yelem.is_none()) continue;
-      if (!yelem.is_float()) continue;
-      x_real.push_back(GETNA<double>());
-      y_real.push_back(yelem.to_double());
+      if (yelem.is_none() || !yelem.is_float()) continue;
+      na_repl = yelem.to_double();
     }
     else if (xelem.is_float()) {
       if (!(yelem.is_none() || yelem.is_float())) {
         throw TypeError() << "Cannot replace float value `" << xelem
           << "` with a value of type " << yelem.typeobj();
       }
-      x_real.push_back(xelem.to_double());
-      y_real.push_back(yelem.to_double());
+      double xval = xelem.to_double();
+      double yval = yelem.to_double();
+      if (ISNA(xval)) {
+        na_repl = yval;
+      } else {
+        x_real.push_back(xval);
+        y_real.push_back(yval);
+        if (xval < xmin_real) xmin_real = xval;
+        if (xval > xmax_real) xmax_real = xval;
+      }
     }
+  }
+  if (!ISNA(na_repl)) {
+    x_real.push_back(GETNA<double>());
+    y_real.push_back(na_repl);
+  }
+  check_uniqueness<double>(x_real);
+}
+
+
+void ReplaceAgent::split_x_y_str() {
+  size_t n = vx.size();
+  CString na_repl;
+  for (size_t i = 0; i < n; ++i) {
+    py::obj xelem = vx[i];
+    py::obj yelem = vy[i];
+    if (xelem.is_none()) {
+      if (yelem.is_none() || !yelem.is_string()) continue;
+      na_repl = yelem.to_cstring();
+    }
+    else if (xelem.is_string()) {
+      if (!(yelem.is_none() || yelem.is_string())) {
+        throw TypeError() << "Cannot replace string value `" << xelem
+          << "` with a value of type " << yelem.typeobj();
+      }
+      x_str.push_back(xelem.to_cstring());
+      y_str.push_back(yelem.to_cstring());
+    }
+  }
+  if (na_repl) {
+    x_str.push_back(CString());
+    y_str.push_back(na_repl);
+  }
+}
+
+
+template <typename T>
+void ReplaceAgent::check_uniqueness(std::vector<T>& data) {
+  std::unordered_set<T> check;
+  for (const auto& x : data) {
+    if (check.count(x)) {
+      throw ValueError() << "Replacement target `" << x << "` was specified "
+        "more than once in Frame.replace()";
+    }
+    check.insert(x);
   }
 }
 
@@ -396,56 +448,180 @@ void ReplaceAgent::process_int_column(size_t colidx) {
     xassert(n == yfilt.size());
     size_t nrows = static_cast<size_t>(col->nrows);
     T* coldata = col->elements_w();
-    replace_in_data<T>(n, xfilt.data(), yfilt.data(),
-                       nrows, coldata);
+    replace_fw<T>(xfilt.data(), yfilt.data(), nrows, coldata, n);
+    col->get_stats()->reset();
+  }
+}
+
+
+template <typename T>
+void ReplaceAgent::process_real_column(size_t colidx) {
+  constexpr double MAX_FLOAT = double(std::numeric_limits<float>::max());
+  if (x_real.empty()) return;
+  auto col = static_cast<RealColumn<T>*>(dt->columns[colidx]);
+  double col_min = static_cast<double>(col->min());
+  double col_max = static_cast<double>(col->max());
+  bool col_has_nas = (col->countna() > 0);
+  // xmax_real will be NA iff the replacement list has just one element: NA
+  if (ISNA(xmax_real)) {
+    if (!col_has_nas) return;
+  } else {
+    if (col_min > xmax_real || col_max < xmin_real) return;
+  }
+  // Prepare filtered x and y vectors
+  std::vector<T> xfilt, yfilt;
+  double maxy = 0;
+  for (size_t i = 0; i < x_real.size(); ++i) {
+    double x = x_real[i];
+    if (ISNA(x)) {
+      if (!col_has_nas) continue;
+      xassert(i == x_real.size() - 1);
+      xfilt.push_back(GETNA<T>());
+    } else {
+      if (x < col_min || x > col_max) continue;
+      xfilt.push_back(static_cast<T>(x));
+    }
+    double y = y_real[i];
+    if (ISNA(y)) {
+      yfilt.push_back(GETNA<T>());
+    } else if (std::is_same<T, float>::value && std::abs(y) <= MAX_FLOAT) {
+      yfilt.push_back(static_cast<T>(y));
+    } else {
+      maxy = std::abs(y);
+    }
+  }
+  if (std::is_same<T, float>::value && maxy > 0) {
+    Column* newcol = col->cast(SType::FLOAT64);
+    dt->columns[colidx] = newcol;
+    delete col;
+    columns_cast = true;
+    process_real_column<double>(colidx);
+  } else {
+    size_t n = xfilt.size();
+    xassert(n == yfilt.size());
+    size_t nrows = static_cast<size_t>(col->nrows);
+    T* coldata = col->elements_w();
+    replace_fw<T>(xfilt.data(), yfilt.data(), nrows, coldata, n);
     col->get_stats()->reset();
   }
 }
 
 
 
-
 //------------------------------------------------------------------------------
 // Step 4: perform actual data replacement
 //------------------------------------------------------------------------------
+// Ignore warnings in auto-generated lambda code
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wpadded"
+
 
 template <typename T>
-void ReplaceAgent::replace_in_data(size_t n, T* x, T* y, size_t nrows, T* data)
+void ReplaceAgent::replace_fw(T* x, T* y, size_t nrows, T* data, size_t n)
 {
-  xassert(n > 0);
   if (n == 1) {
-    T x0 = x[0], y0 = y[0];
-    #pragma omp parallel for
-    for (size_t i = 0; i < nrows; ++i) {
-      if (data[i] == x0) data[i] = y0;
-    }
+    replace_fw1<T>(x, y, nrows, data);
   }
   else if (n == 2) {
-    T x0 = x[0], y0 = y[0];
-    T x1 = x[1], y1 = y[1];
-    #pragma omp parallel for
-    for (size_t i = 0; i < nrows; ++i) {
-      T v = data[i];
-      if (v == x0) data[i] = y0;
-      else if (v == x1) data[i] = y1;
-    }
+    replace_fw2<T>(x, y, nrows, data);
   }
   else {
-    #pragma omp parallel for
-    for (size_t i = 0; i < nrows; ++i) {
-      T v = data[i];
-      for (size_t j = 0; j < n; ++j) {
-        if (v == x[j]) {
-          data[i] = y[j];
-          break;
-        }
-      }
-    }
+    xassert(n > 0);
+    replace_fwN<T>(x, y, nrows, data, n);
   }
 }
 
 
+template <typename T>
+void ReplaceAgent::replace_fw1(T* x, T* y, size_t nrows, T* data) {
+  T x0 = x[0], y0 = y[0];
+  if (std::is_floating_point<T>::value && ISNA<T>(x0)) {
+    dt::run_interleaved(nrows,
+      [=](size_t& i, size_t iend, size_t di) {
+        for (; i < iend; i += di) {
+          if (ISNA<T>(data[i])) data[i] = y0;
+        }
+      }
+    );
+  } else {
+    dt::run_interleaved(nrows,
+      [=](size_t& i, size_t iend, size_t di) {
+        for (; i < iend; i += di) {
+          if (data[i] == x0) data[i] = y0;
+        }
+      }
+    );
+  }
+}
 
 
+template <typename T>
+void ReplaceAgent::replace_fw2(T* x, T* y, size_t nrows, T* data) {
+  T x0 = x[0], y0 = y[0];
+  T x1 = x[1], y1 = y[1];
+  xassert(!ISNA<T>(x0));
+  if (std::is_floating_point<T>::value && ISNA<T>(x1)) {
+    dt::run_interleaved(nrows,
+      [=](size_t& i, size_t iend, size_t di) {
+        for (; i < iend; i += di) {
+          T v = data[i];
+          if (v == x0) data[i] = y0;
+          else if (ISNA<T>(v)) data[i] = y1;
+        }
+      }
+    );  } else {
+    dt::run_interleaved(nrows,
+      [=](size_t& i, size_t iend, size_t di) {
+        for (; i < iend; i += di) {
+          T v = data[i];
+          if (v == x0) data[i] = y0;
+          else if (v == x1) data[i] = y1;
+        }
+      }
+    );
+  }
+}
+
+
+template <typename T>
+void ReplaceAgent::replace_fwN(T* x, T* y, size_t nrows, T* data, size_t n) {
+  if (std::is_floating_point<T>::value && ISNA<T>(x[n-1])) {
+    n--;
+    dt::run_interleaved(nrows,
+      [=](size_t& i, size_t iend, size_t di) {
+        for (; i < iend; i += di) {
+          T v = data[i];
+          if (ISNA<T>(v)) {
+            data[i] = y[n];
+            continue;
+          }
+          for (size_t j = 0; j < n; ++j) {
+            if (v == x[j]) {
+              data[i] = y[j];
+              break;
+            }
+          }
+        }
+      }
+    );
+  } else {
+    dt::run_interleaved(nrows,
+      [=](size_t& i, size_t iend, size_t di) {
+        for (; i < iend; i += di) {
+          T v = data[i];
+          for (size_t j = 0; j < n; ++j) {
+            if (v == x[j]) {
+              data[i] = y[j];
+              break;
+            }
+          }
+        }
+      }
+    );
+  }
+}
+
+
+#pragma clang diagnostic pop
 
 }  // namespace py
