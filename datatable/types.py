1332a829f93e7b1a2951c79db6488921420b14df
Pasha Stetsenko <pasha@h2o.ai>
2018-10-23 16:49:08 -0700

    Update datatable/types.py
    Co-Authored-By: nkalonia1 <nkalonia1@gmail.com>

--- datatable/types.py
+++ datatable/types.py
@@ -251,7 +251,7 @@ _stype_extrema = {
     stype.int8: (-127, 127),
     stype.int16: (-32767, 32767),
     stype.int32: (-2147483647, 2147483647),
-    stype.int64: (-18446744073709551616, 18446744073709551616),
+    stype.int64: (-9223372036854775807, 9223372036854775807),
     stype.float32: (unpack(">f", b'\xff\x7f\xff\xff')[0], unpack(">f", b'\x7f\x7f\xff\xff')[0]),
     stype.float64: (unpack(">d", b'\xff\xef\xff\xff\xff\xff\xff\xff')[0], unpack(">d", b'\x7f\xef\xff\xff\xff\xff\xff\xff')[0]),
 }
