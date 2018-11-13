5e8672e877c237b9ba43a22bd05aca469527a65c
Pasha Stetsenko <pasha@h2o.ai>
2018-11-13 15:45:40 -0800

    bug fixes

--- c/frame/join.cc
+++ c/frame/join.cc
@@ -45,31 +45,34 @@ static cmpptr make_comparator(const Column* col1, const Column* col2);
 //------------------------------------------------------------------------------
 
 /**
- * Abstract base class that facilitates comparison of rows between two Frames.
+ * Abstract base class that facilitates comparison of rows between two frames,
+ * called X and J. The frames have different roles: J is the "look up" frame,
+ * and values within this frame are assumed sorted; X is the "main" frame, and
+ * we will be looking up the values from this Frame in J.
  *
- * The implementation classes are expected to store references to both Frames,
+ * The implementation classes are expected to store references to both frames,
  * and then provide the following comparison functions:
  *
- *   set_row(size_t row) -> int
- *     This selects the row within the left Frame. All subsequent comparisons
+ *   set_xrow(size_t row) -> int
+ *     This selects a row within the X frame. All subsequent comparisons
  *     will be done against that row.
- *     The function returns 0 if it succeded, or -1 if the lhs value in the
- *     given row will not match any value in the rhs Frame.
+ *     The function returns 0 if it succeeds, or -1 if the requested row will
+ *     not be able to match any row in the J frame.
  *
- *   cmp(size_t row) -> int
- *     compare the `row`th value in the right Frame against the value from the
- *     left Frame stored during the previous `set_row()` call. Return +1, -1,
+ *   cmp_jrow(size_t row) -> int
+ *     compare the `row`th value in the J frame against the value from the
+ *     X frame stored during the previous `set_xrow()` call. Returns +1, -1,
  *     or 0 depending if the row-value is greater than, less than, or equal
  *     to the value stored.
  *
  * This comparison function is then used as the basis for the binary search
- * algorithm to performa a join between two tables.
+ * algorithm to perform a join between two tables.
  */
 class Cmp {
   public:
     virtual ~Cmp();
-    virtual int cmp(size_t row) const = 0;
-    virtual int set_row(size_t row) = 0;
+    virtual int cmp_jrow(size_t row) const = 0;
+    virtual int set_xrow(size_t row) = 0;
 };
 
 Cmp::~Cmp() {}
@@ -85,30 +88,30 @@ class MultiCmp : public Cmp {
     std::vector<cmpptr> col_cmps;
 
   public:
-    MultiCmp(const colvec& lhs, const colvec& rhs);
-    int set_row(size_t row) override;
-    int cmp(size_t row) const override;
+    MultiCmp(const colvec& Xcols, const colvec& Jcols);
+    int set_xrow(size_t row) override;
+    int cmp_jrow(size_t row) const override;
 };
 
 
-MultiCmp::MultiCmp(const colvec& lhs, const colvec& rhs) {
-  xassert(lhs.size() == rhs.size());
-  for (size_t i = 0; i < lhs.size(); ++i) {
-    col_cmps.push_back(make_comparator(lhs[i], rhs[i]));
+MultiCmp::MultiCmp(const colvec& Xcols, const colvec& Jcols) {
+  xassert(Xcols.size() == Jcols.size());
+  for (size_t i = 0; i < Xcols.size(); ++i) {
+    col_cmps.push_back(make_comparator(Xcols[i], Jcols[i]));
   }
 }
 
-int MultiCmp::set_row(size_t row) {
+int MultiCmp::set_xrow(size_t row) {
   int r = 0;
-  for (auto& ch : col_cmps) {
-    r |= ch->set_row(row);
+  for (cmpptr& ch : col_cmps) {
+    r |= ch->set_xrow(row);
   }
   return r;
 }
 
-int MultiCmp::cmp(size_t row) const {
-  for (const auto& ch : col_cmps) {
-    int r = ch->cmp(row);
+int MultiCmp::cmp_jrow(size_t row) const {
+  for (const cmpptr& ch : col_cmps) {
+    int r = ch->cmp_jrow(row);
     if (r) return r;
   }
   return 0;
@@ -120,63 +123,65 @@ int MultiCmp::cmp(size_t row) const {
 // Fixed-width Cmp
 //------------------------------------------------------------------------------
 
-template <typename T, typename U>
+template <typename TX, typename TJ>
 class FwCmp : public Cmp {
   private:
-    const T* data1;
-    const U* data2;
-    T value;
-    size_t : (64 - 8 * sizeof(T)) & 63;
+    const TX* dataX;
+    const TJ* dataJ;
+    TJ x_value;  // Current value from X frame, converted to TJ type
+    size_t : (64 - 8 * sizeof(TJ)) & 63;
 
   public:
     FwCmp(const Column*, const Column*);
     static cmpptr make(const Column*, const Column*);
 
-    int cmp(size_t row) const override;
-    int set_row(size_t row) override;
+    int cmp_jrow(size_t row) const override;
+    int set_xrow(size_t row) override;
 };
 
 
-template <typename T, typename U>
-FwCmp<T, U>::FwCmp(const Column* xcol, const Column* jcol) {
-  auto xcol_f = static_cast<const FwColumn<U>*>(xcol);
-  auto jcol_f = static_cast<const FwColumn<T>*>(jcol);
-  data1 = jcol_f->elements_r();
-  data2 = xcol_f->elements_r();
+template <typename TX, typename TJ>
+FwCmp<TX, TJ>::FwCmp(const Column* xcol, const Column* jcol) {
+  auto xcol_f = dynamic_cast<const FwColumn<TX>*>(xcol);
+  auto jcol_f = dynamic_cast<const FwColumn<TJ>*>(jcol);
+  xassert(xcol_f && jcol_f);
+  dataX = xcol_f->elements_r();
+  dataJ = jcol_f->elements_r();
 }
 
-template <typename T, typename U>
-cmpptr FwCmp<T, U>::make(const Column* col1, const Column* col2) {
-  return cmpptr(new FwCmp<T, U>(col1, col2));
-}
-
-
-template <typename T, typename U>
-int FwCmp<T, U>::cmp(size_t row) const {
-  T x = data1[row];
-  return (x > value) - (x < value);
+template <typename TX, typename TJ>
+cmpptr FwCmp<TX, TJ>::make(const Column* col1, const Column* col2) {
+  return cmpptr(new FwCmp<TX, TJ>(col1, col2));
 }
 
 
-template <typename T, typename U>
-int FwCmp<T, U>::set_row(size_t row) {
-  U newval = data2[row];
-  if (ISNA<U>(newval)) {
-    value = GETNA<T>();
-  } else if (std::is_integral<U>::value) {
-    if (std::is_integral<T>::value && sizeof(U) > sizeof(T)) {
-      if (newval > static_cast<U>(std::numeric_limits<T>::max()) ||
-          newval < static_cast<U>(std::numeric_limits<T>::min())) return -1;
-    }
-    value = static_cast<T>(newval);
+template <typename TX, typename TJ>
+int FwCmp<TX, TJ>::cmp_jrow(size_t row) const {
+  TJ jval = dataJ[row];
+  return (jval > x_value) - (jval < x_value);
+}
+
+
+template <typename TX, typename TJ>
+int FwCmp<TX, TJ>::set_xrow(size_t row) {
+  TX newval = dataX[row];
+  if (ISNA<TX>(newval)) {
+    x_value = GETNA<TJ>();
   } else {
-    value = static_cast<T>(newval);
-    if (std::is_integral<T>::value) {
+    x_value = static_cast<TJ>(newval);
+    if (std::is_integral<TJ>::value) {
+      if (std::is_integral<TX>::value &&
+          sizeof(TX) > sizeof(TJ) &&
+          (newval > static_cast<TX>(std::numeric_limits<TJ>::max()) ||
+           newval < static_cast<TX>(std::numeric_limits<TJ>::min())))
+        return -1;
       // If matching floating point value to an integer column, values that
       // are not round numbers should not match. We return `lt` comparator
       // in this case, which is not entirely accurate, but it will provide
       // the desired no-match semantics.
-      if (static_cast<U>(value) != newval) return -1;
+      if (!std::is_integral<TX>::value &&
+          static_cast<TX>(x_value) != newval)
+        return -1;
     }
   }
   return 0;
@@ -188,69 +193,70 @@ int FwCmp<T, U>::set_row(size_t row) {
 // String Cmp
 //------------------------------------------------------------------------------
 
-template <typename T, typename U>
+template <typename TX, typename TJ>
 class StringCmp : public Cmp {
   private:
-    const uint8_t* strdata1;
-    const uint8_t* strdata2;
-    const T* offsets1;
-    const U* offsets2;
-    U start2;
-    U end2;
-    size_t : (128 - 2 * 8 * sizeof(U)) & 63;
+    const uint8_t* strdataX;
+    const uint8_t* strdataJ;
+    const TX* offsetsX;
+    const TJ* offsetsJ;
+    TX xstart;
+    TX xend;
+    size_t : (128 - 2 * 8 * sizeof(TJ)) & 63;
 
   public:
     StringCmp(const Column*, const Column*);
     static cmpptr make(const Column*, const Column*);
 
-    int cmp(size_t row) const override;
-    int set_row(size_t row) override;
+    int cmp_jrow(size_t row) const override;
+    int set_xrow(size_t row) override;
 };
 
 
-template <typename T, typename U>
-StringCmp<T, U>::StringCmp(const Column* xcol, const Column* jcol) {
-  auto xcol_s = static_cast<const StringColumn<U>*>(xcol);
-  auto jcol_s = static_cast<const StringColumn<T>*>(jcol);
-  strdata1 = jcol_s->ustrdata();
-  strdata2 = xcol_s->ustrdata();
-  offsets1 = jcol_s->offsets();
-  offsets2 = xcol_s->offsets();
+template <typename TX, typename TJ>
+StringCmp<TX, TJ>::StringCmp(const Column* xcol, const Column* jcol) {
+  auto xcol_s = dynamic_cast<const StringColumn<TX>*>(xcol);
+  auto jcol_s = dynamic_cast<const StringColumn<TJ>*>(jcol);
+  xassert(xcol_s && jcol_s);
+  strdataX = xcol_s->ustrdata();
+  offsetsX = xcol_s->offsets();
+  strdataJ = jcol_s->ustrdata();
+  offsetsJ = jcol_s->offsets();
 }
 
-template <typename T, typename U>
-cmpptr StringCmp<T, U>::make(const Column* col1, const Column* col2) {
-  return cmpptr(new StringCmp<T, U>(col1, col2));
+template <typename TX, typename TJ>
+cmpptr StringCmp<TX, TJ>::make(const Column* col1, const Column* col2) {
+  return cmpptr(new StringCmp<TX, TJ>(col1, col2));
 }
 
 
-template <typename T, typename U>
-int StringCmp<T, U>::cmp(size_t row) const {
-  T end1 = offsets1[row];
-  if (ISNA<T>(end1)) return ISNA<U>(end2) - 1;
-  if (ISNA<U>(end2)) return 1;
+template <typename TX, typename TJ>
+int StringCmp<TX, TJ>::cmp_jrow(size_t row) const {
+  TJ jend = offsetsJ[row];
+  if (ISNA<TJ>(jend)) return ISNA<TX>(xend) - 1;
+  if (ISNA<TX>(xend)) return 1;
 
-  T start1 = offsets1[row - 1] & ~GETNA<T>();
-  T len1 = end1 - start1;
-  U len2 = end2 - start2;
-  const uint8_t* str1 = strdata1 + start1;
-  const uint8_t* str2 = strdata2 + start2;
-  for (T i = 0; i < len1; ++i) {
-    if (i == len2) return 1;  // str2 is shorter than str1
-    uint8_t ch1 = str1[i];
-    uint8_t ch2 = str2[i];
+  TJ jstart = offsetsJ[row - 1] & ~GETNA<TJ>();
+  TJ jlen = jend - jstart;
+  TX xlen = xend - xstart;
+  const uint8_t* xstr = strdataX + xstart;
+  const uint8_t* jstr = strdataJ + jstart;
+  for (TJ i = 0; i < jlen; ++i) {
+    if (i == xlen) return 1;  // jstr is shorter than xstr
+    uint8_t ch1 = xstr[i];
+    uint8_t ch2 = jstr[i];
     if (ch1 != ch2) {
       return 1 - 2*(ch1 < ch2);
     }
   }
-  return -(len1 != len2);
+  return -(jlen != xlen);
 }
 
 
-template <typename T, typename U>
-int StringCmp<T, U>::set_row(size_t row) {
-  end2 = offsets2[row];
-  start2 = offsets2[row - 1] & ~GETNA<U>();
+template <typename TX, typename TJ>
+int StringCmp<TX, TJ>::set_xrow(size_t row) {
+  xend   = offsetsX[row];
+  xstart = offsetsX[row - 1] & ~GETNA<TX>();
   return 0;
 }
 
@@ -343,12 +349,12 @@ static size_t binsearch(Cmp* cmp, size_t nrows) {
   while (start < end) {
     // We won't ever have as many rows for this to overflow
     size_t mid = (start + end) >> 1;
-    int r = cmp->cmp(mid);
+    int r = cmp->cmp_jrow(mid);
     if (r > 0) end = mid - 1;
     else if (r < 0) start = mid + 1;
     else return mid;
   }
-  return cmp->cmp(start) == 0? start : NO_MATCH;
+  return cmp->cmp_jrow(start) == 0? start : NO_MATCH;
 }
 
 
@@ -388,13 +394,13 @@ Join two Frames `xdt` and `jdt` on the keys of `jdt`.
   size_t nchunks = std::min(xdt->nrows / 2,
                             static_cast<size_t>(config::nthreads));
 
-  #pragma omp parallel num_threads(nchunks)
+  // #pragma omp parallel num_threads(nchunks)
   {
     MultiCmp comparator(xcols, jcols);
 
-    #pragma omp parallel for
+    // #pragma omp parallel for
     for (size_t i = 0; i < xdt->nrows; ++i) {
-      comparator.set_row(i);
+      comparator.set_xrow(i);
       size_t j = binsearch(&comparator, jdt->nrows);
       result_indices[i] = (j == NO_MATCH)? GETNA<int32_t>()
                                          : static_cast<int32_t>(j);
