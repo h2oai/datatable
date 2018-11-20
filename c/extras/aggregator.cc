95a2e9083f0cca36a80076ac979c86d3219543c9
oleksiyskononenko <35204136+oleksiyskononenko@users.noreply.github.com>
2018-11-20 13:35:10 -0800

    Sort exemplars even when aggregation is bypassed (#1435)
    Sort exemplars by the first column, when the aggregation is bypassed.

--- c/extras/aggregator.cc
+++ c/extras/aggregator.cc
@@ -254,12 +254,17 @@ void Aggregator::aggregate_exemplars(DataTable* dt,
 
 
 /*
-*  Do no groupping, i.e. all rows become exemplars.
+*  Do no grouping, i.e. all rows become exemplars sorted by the first column.
 */
 void Aggregator::group_0d(const DataTable* dt, dtptr& dt_members) {
-  auto d_members = static_cast<int32_t*>(dt_members->columns[0]->data_w());
-  for (size_t i = 0; i < dt->nrows; ++i) {
-    d_members[i] = static_cast<int32_t>(i);
+  if (dt->ncols > 0) {
+    RowIndex ri_exemplars = dt->sortby({0}, nullptr);
+    auto d_members = static_cast<int32_t*>(dt_members->columns[0]->data_w());
+
+    ri_exemplars.strided_loop2(0, static_cast<int64_t>(dt->nrows), 1,
+    [&](int64_t i, int64_t j) {
+      d_members[j] = static_cast<int32_t>(i);
+    });
   }
 }
 
