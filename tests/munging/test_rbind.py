d389dd5ade16d921f5d68cbc8b2be853e8ad3eaa
Pasha Stetsenko <pasha@h2o.ai>
2019-01-25 12:43:10 -0800

    Fix rbinding frames with negative step slice (#1595)
    - The negative step slice was incorrectly accounted for during frame materialization;
    - Improved error printout for data mismatch in random attack script.
    
    Closes #1594

--- tests/munging/test_rbind.py
+++ tests/munging/test_rbind.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
-# © H2O.ai 2018; -*- encoding: utf-8 -*-
-#   This Source Code Form is subject to the terms of the Mozilla Public
-#   License, v. 2.0. If a copy of the MPL was not distributed with this
-#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Copyright 2018 H2O.ai
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
 #-------------------------------------------------------------------------------
 import math
 import pytest
@@ -368,3 +385,11 @@ def test_issue1292():
     f0.internal.check()
     assert f0.to_list() == [[None] * 4]
     assert f0.stypes == (stype.str32,)
+
+
+def test_issue1594():
+    DT = dt.Frame([range(5), list("abcde")])
+    DT = DT[::-1, :]
+    DT.rbind(DT)
+    assert DT.to_list() == [[4, 3, 2, 1, 0, 4, 3, 2, 1, 0],
+                            list("edcbaedcba")]
