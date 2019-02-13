8cf1c0186f1934cda6f94f3af1c4b435f1dde956
Pasha Stetsenko <pasha@h2o.ai>
2019-02-12 16:10:03 -0800

    Create mechanism for defining python interface methods more robustly

--- c/python/arg.cc
+++ c/python/arg.cc
@@ -90,6 +90,10 @@ SType       Arg::to_stype()        const { return pyobj.to_stype(*this); }
 SType       Arg::to_stype(const error_manager& em) const { return pyobj.to_stype(em); }
 DataTable*  Arg::to_frame()        const { return pyobj.to_frame(*this); }
 
+Arg::operator int32_t() const { return to_int32_strict(); }
+Arg::operator int64_t() const { return to_int64_strict(); }
+Arg::operator size_t()  const { return to_size_t(); }
+
 
 
 //------------------------------------------------------------------------------
