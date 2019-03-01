bddc892f991e5012c373f91b400d7ef27595911f
Pasha Stetsenko <pasha@h2o.ai>
2019-02-28 23:45:19 -0800

    Rename fixed_height_string_col -> writable_string_col

--- c/wstringcol.cc
+++ c/wstringcol.cc
@@ -21,23 +21,23 @@ namespace dt {
 
 
 //------------------------------------------------------------------------------
-// fixed_height_string_col
+// writable_string_col
 //------------------------------------------------------------------------------
 
-fixed_height_string_col::fixed_height_string_col(size_t nrows)
+writable_string_col::writable_string_col(size_t nrows)
   : strdata(new MemoryWritableBuffer(nrows)),
     offdata(MemoryRange::mem((nrows + 1) * sizeof(uint32_t))),
     n(nrows) {}
 
 
-Column* fixed_height_string_col::to_column() && {
+Column* writable_string_col::to_column() && {
   strdata->finalize();
   offdata.set_element<uint32_t>(0, 0);
   return new StringColumn<uint32_t>(n, std::move(offdata), strdata->get_mbuf());
 }
 
 
-fixed_height_string_col::buffer::buffer(fixed_height_string_col& s)
+writable_string_col::buffer::buffer(writable_string_col& s)
   : col(s), strbuf(1024)
 {
   strbuf_used = 0;
@@ -47,36 +47,37 @@ fixed_height_string_col::buffer::buffer(fixed_height_string_col& s)
 }
 
 
-void fixed_height_string_col::buffer::write(const CString& str) {
+void writable_string_col::buffer::write(const CString& str) {
   write(str.ch, static_cast<size_t>(str.size));
 }
 
-void fixed_height_string_col::buffer::write(const std::string& str) {
+void writable_string_col::buffer::write(const std::string& str) {
   write(str.data(), str.size());
 }
 
-void fixed_height_string_col::buffer::write(const char* ch, size_t len) {
+void writable_string_col::buffer::write(const char* ch, size_t len) {
   if (ch) {
+    xassert(len < GETNA<uint32_t>());
     strbuf.ensuresize(strbuf_used + len);
     std::memcpy(strbuf.data() + strbuf_used, ch, len);
     strbuf_used += len;
     *offptr++ = static_cast<uint32_t>(strbuf_used);
   } else {
-    *offptr++ = static_cast<uint32_t>(strbuf_used) | GETNA<uint32_t>();
+    *offptr++ = static_cast<uint32_t>(strbuf_used) ^ GETNA<uint32_t>();
   }
 }
 
-void fixed_height_string_col::buffer::write_na() {
-  *offptr++ = static_cast<uint32_t>(strbuf_used) | GETNA<uint32_t>();
+void writable_string_col::buffer::write_na() {
+  *offptr++ = static_cast<uint32_t>(strbuf_used) ^ GETNA<uint32_t>();
 }
 
 
-void fixed_height_string_col::buffer::order() {
+void writable_string_col::buffer::order() {
   strbuf_write_pos = col.strdata->prep_write(strbuf_used, strbuf.data());
 }
 
 
-void fixed_height_string_col::buffer::commit_and_start_new_chunk(size_t i0) {
+void writable_string_col::buffer::commit_and_start_new_chunk(size_t i0) {
   col.strdata->write_at(strbuf_write_pos, strbuf_used, strbuf.data());
   for (uint32_t* p = offptr0; p < offptr; ++p) {
     *p += strbuf_write_pos;
