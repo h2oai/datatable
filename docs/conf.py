00250bab12570094b8d76337e5ab0f887b9b01c5
Viktor Demin <deminvictor@gmail.com>
2019-03-11 22:13:13 +0300

    added file for testing ipython directive

--- docs/conf.py
+++ docs/conf.py
@@ -60,6 +60,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'IPython.sphinxext.ipython_directive',
     'IPython.sphinxext.ipython_console_highlighting'
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
