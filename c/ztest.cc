80464fd99bf5573b94a9a4d45c8e8ad98b33d269
Pasha Stetsenko <pasha@h2o.ai>
2019-03-26 18:07:27 -0700

    Shared bmutex using atomics (#1743)
    Instead of OMP #pragma atomic structures, use real std::atomic<> variables and atomic operations to implement shared_bmutex. This moves us closer to the goal of eliminating the dependency on OpenMP, and also should achieve better performance, esp. on PowerPC architecture, because we do not use the default seq_cst memory order.
    
    Added tests for shated mutex correct operation.

--- c/ztest.cc
+++ c/ztest.cc
@@ -10,22 +10,45 @@
 #include "python/args.h"
 #include "utils/exceptions.h"
 #include "ztest.h"
-namespace dttest {
+namespace py {
 
 
-static py::PKArgs arg_tests(
-  0, 0, 0, false, false, {},
-  "test_internal",
-  "Run internal tests that check functionality of some internal structures");
+static PKArgs arg_test_coverage(0, 0, 0, false, false, {}, "test_coverage");
 
-static py::oobj tests(const py::PKArgs&) {
-  cover_init_FrameInitializationManager_em();
-  cover_names_FrameNameProviders();
-  cover_names_integrity_checks();
-  return py::None();
+static void test_coverage(const PKArgs&) {
+  dttest::cover_init_FrameInitializationManager_em();
+  dttest::cover_names_FrameNameProviders();
+  dttest::cover_names_integrity_checks();
+}
+
+static PKArgs arg_test_shmutex(3, 0, 0, false, false,
+  {"n_iters", "n_threads", "impl"},
+  "test_shmutex");
+
+static void test_shmutex(const PKArgs& args) {
+  size_t n_iters = args[0].to_size_t();
+  size_t n_threads = args[1].to_size_t();
+  int impl = args[2].to_int32_strict();
+  dttest::test_shmutex(n_iters, n_threads, impl);
 }
 
 
+
+void DatatableModule::init_tests() {
+  ADD_FN(&test_coverage, arg_test_coverage);
+  ADD_FN(&test_shmutex, arg_test_shmutex);
+}
+
+
+} // namespace py
+
+
+
+
+//------------------------------------------------------------------------------
+// Helper functions
+//------------------------------------------------------------------------------
+namespace dttest {
 
 /**
  * Check that calling `f()` causes an AssertionError with the specified
@@ -41,22 +64,15 @@ void test_assert(const std::function<void(void)>& f,
     if (!pye.is_assertion_error()) throw pye;
     std::string error_message = pye.message();
     if (error_message.find(expected_error) == std::string::npos) {
-      throw ValueError() << "Expected exception message <<" << expected_error
-          << ">>, got <<" << error_message << ">>";
+      throw ValueError() << "Expected exception message `" << expected_error
+          << "`, got `" << error_message << "`";
     }
     return;
   }
-  throw ValueError() << "Assertion error <<" << expected_error
-      << ">> was not raised";
+  throw ValueError() << "Assertion error `" << expected_error
+      << "` was not raised";
 }
 
 
 }  // namespace dttest
-
-
-
-void py::DatatableModule::init_tests() {
-  ADD_FN(&dttest::tests, dttest::arg_tests);
-}
-
 #endif
