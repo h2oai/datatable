e3f76277926949b3100406850c8a162e91ebad55
Oleksiy Kononenko <oleksiy@h2o.ai>
2019-04-01 20:00:44 -0700

    Minor text fix

--- c/models/dt_ftrl.h
+++ c/models/dt_ftrl.h
@@ -53,7 +53,7 @@ struct FtrlParams {
 /*
 *  When FTRL fitting is completed, this structure is returned
 *  containing epoch at which fitting stopped, and, in the case validation set
-*  was provided, non-zero final loss.
+*  was provided, the corresponding final loss.
 */
 struct FtrlFitOutput {
     double epoch;
