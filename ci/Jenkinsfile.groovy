f15a6b6bbd129d0be04e66774176e1cefeffb1a5
abal5 <anmol@h2o.ai>
2019-04-23 12:22:51 -0700

    sha updated for dockerfile-centos

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -42,7 +42,7 @@ RSA_CRED_ID = 'master-id-rsa'
 X86_64_CENTOS_DOCKER_IMAGE_NAME = "harbor.h2o.ai/opsh2oai/datatable-build-x86_64_centos7"
 EXPECTED_SHAS = [
     files: [
-        'ci/Dockerfile-centos7.in': 'ef6d029c7bae9786f89b9c9a5a9526dafdffc9ee',
+        'ci/Dockerfile-centos7.in': 'b81387b927706b7378f3921e67f53aff448c7495',
         'ci/Dockerfile-ubuntu.in': '8dbbd6afe03062befa391c20be95daf58caee4ac',
     ]
 ]
