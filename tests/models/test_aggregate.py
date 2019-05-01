91b7b718ac45fb0437db0542d33358770ca9f4e6
Oleksiy Kononenko <oleksiy@h2o.ai>
2019-04-30 17:40:19 -0700

    Adjust progress check

--- tests/models/test_aggregate.py
+++ tests/models/test_aggregate.py
@@ -518,7 +518,7 @@ def aggregate_nd(nd):
         assert messages[0].progress == 0
         assert messages[0].status == "running"
         assert messages[0].message == "Preparing"
-        assert messages[-2].progress < 1.0
+        assert messages[-2].progress <= 1.0
         assert messages[-2].status == "running"
         assert messages[-2].message == "Finalizing"
         assert messages[-1].progress == 1.0
