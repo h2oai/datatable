4e0d64b63fc1383d5fb2813e324f75d608a49e4a
Pasha Stetsenko <pasha@h2o.ai>
2019-05-09 18:42:57 -0700

    Add constants e, pi, tau, nan, inf to math module

--- datatable/math.py
+++ datatable/math.py
@@ -20,6 +20,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 #-------------------------------------------------------------------------------
+from math import e, inf, nan, pi, tau
 
 from .lib._datatable import (
 	arccos,
