h2oai/datatable

commit e9fb1b1357a1fb40ee6a40b073be851fe9557afe

Parallel for static improvements (#1899)
- `parallel_for_static()` can now be called outside of a parallel region only. Inside a parallel region use `nested_for_static()` instead.
- `parallel_for_static()` templates are now written in such a way as to allow more opportunities for the compiler to inline the calls.
- Arguments `chunk_size` and `nthreads` must now be explicitly named: `ChunkSize(k)`, `NThreads(nth)`. This prevents any possibility to confuse the order of the arguments.
- Fixed a performance bug where the iterations where sometimes not distributed equally between the executor threads.
- When the chunk_size parameter is not given, the iterations are now divided equally among all available thread workers (before we used the default chunk_size of 4096). This behavior better matches what OMP does in the same setting.
- Make sure that asserts are skipped when compiling in the `make fast` mode.
- Four tests in test_ftrl.py are now randomly failing because the results are returned in a more random order. They are marked as "skipped" for now (to be fixed later).

In sorting benchmarks I see about 10% improvement in speed for integers and strings, and no significant change in speed for floats.

c/models/dt_ftrl.cc
@@ -488,7 +488,7 @@ FtrlFitOutput Ftrl<T>::fit(T(*linkfn)(T), U(*targetfn)(U, size_t), T(*lossfn)(T,
         size_t iteration_size = iteration_end - iteration_start;
 
         // Training.
-        dt::parallel_for_static(iteration_size, [&](size_t i) {
+        dt::nested_for_static(iteration_size, [&](size_t i) {
           size_t ii = (iteration_start + i) % dt_X_train->nrows;
           const size_t j0 = ri[0][ii];
 
@@ -520,7 +520,7 @@ FtrlFitOutput Ftrl<T>::fit(T(*linkfn)(T), U(*targetfn)(U, size_t), T(*lossfn)(T,
           dt::atomic<T> loss_global {0.0};
           T loss_local = 0.0;
 
-          dt::parallel_for_static(dt_X_val->nrows, [&](size_t i) {
+          dt::nested_for_static(dt_X_val->nrows, [&](size_t i) {
             const size_t j0 = ri_val[0][i];
 
             if (j0 != RowIndex::NA && !ISNA<U>(data_y_val[0][j0])) {
@@ -687,7 +687,7 @@ dtptr Ftrl<T>::predict(const DataTable* dt_X) {
     uint64ptr x = uint64ptr(new uint64_t[nfeatures]);
     tptr<T> w = tptr<T>(new T[nfeatures]);
 
-    dt::parallel_for_static(dt_X->nrows, [&](size_t i){
+    dt::nested_for_static(dt_X->nrows, [&](size_t i){
       hash_row(x, hashers, i);
       for (size_t k = 0; k < nlabels; ++k) {
         size_t label_id = static_cast<size_t>(data_label_ids[k]);

tests/models/test_ftrl.py
@@ -773,6 +773,7 @@ def test_ftrl_fit_predict_binomial_online_1_2():
     assert max(delta_even) < epsilon
 
 
+@pytest.mark.skip(reason="Unstable since #1899")
 def test_ftrl_fit_predict_binomial_online_2_1():
     ft = Ftrl(alpha = 0.1, nepochs = 10000, model_type = "binomial")
     df_train_even_odd = dt.Frame([[2, 1, 8, 3]])
@@ -811,6 +812,7 @@ def test_ftrl_fit_predict_binomial_online_2_1():
     assert max(delta_even) < epsilon
 
 
+@pytest.mark.skip(reason="Unstable since #1899")
 def test_ftrl_fit_predict_binomial_online_2_2():
     ft = Ftrl(alpha = 0.1, nepochs = 10000, model_type = "binomial")
     df_train_even_odd = dt.Frame([[2, 1, 8, 3]])
@@ -855,6 +857,7 @@ def test_ftrl_fit_multinomial_none():
     assert res.loss is None
 
 
+@pytest.mark.skip(reason="Unstable since #1899")
 def test_ftrl_fit_predict_multinomial_vs_binomial():
     target_names = ["cat", "dog"]
     ft_binomial = Ftrl(nbins = 10, nepochs = 1)
@@ -912,6 +915,7 @@ def test_ftrl_fit_predict_multinomial():
     assert p.names == labels
 
 
+@pytest.mark.skip(reason="Unstable since #1899")
 def test_ftrl_fit_predict_multinomial_online():
     ft = Ftrl(alpha = 0.2, nepochs = 1000, double_precision = True)
     labels = ["green", "red", "blue"]

c/frame/join.cc
@@ -418,7 +418,7 @@ RowIndex natural_join(const DataTable* xdt, const DataTable* jdt) {
         // Creating the comparator may fail if xcols and jcols are incompatible
         MultiCmp comparator(xcols, jcols, xdt, jdt);
 
-        dt::parallel_for_static(xdt->nrows,
+        dt::nested_for_static(xdt->nrows,
           [&](size_t i) {
             int r = comparator.set_xrow(i);
             if (r == 0) {

c/parallel/api.h
@@ -22,9 +22,6 @@
 namespace dt {
 using std::size_t;
 
-// Private
-void _parallel_for_static(size_t, size_t, size_t,
-                          std::function<void(size_t, size_t)>);
 
 
 //------------------------------------------------------------------------------
@@ -99,36 +96,6 @@ void parallel_region(function<void()> f);
 void barrier();
 
 
-/**
- * Run parallel loop `for i in range(nrows): f(i)`, with static scheduling.
- */
-template <typename F>
-void parallel_for_static(size_t nrows, F f) {
-  _parallel_for_static(nrows, 4096, dt::num_threads_available(),
-    [&](size_t i0, size_t i1) {
-      for (size_t i = i0; i < i1; ++i) f(i);
-    });
-}
-
-template <typename F>
-void parallel_for_static(size_t nrows, size_t chunk_size, F f) {
-  _parallel_for_static(nrows, chunk_size, dt::num_threads_available(),
-    [&](size_t i0, size_t i1) {
-      for (size_t i = i0; i < i1; ++i) f(i);
-    });
-}
-
-
-template <typename F>
-void parallel_for_static(size_t nrows, size_t chunk_size, size_t nthreads,
-                         F f)
-{
-  _parallel_for_static(nrows, chunk_size, nthreads,
-    [&](size_t i0, size_t i1) {
-      for (size_t i = i0; i < i1; ++i) f(i);
-    });
-}
-
 
 /**
  * Run parallel loop `for i in range(nrows): f(i)`, with dynamic scheduling.
@@ -170,4 +137,13 @@ void parallel_for_ordered(size_t n_iterations, size_t n_threads,
 std::mutex& python_mutex();
 
 }  // namespace dt
+
+
+//------------------------------------------------------------------------------
+// parallel-for-static
+//------------------------------------------------------------------------------
+#include "parallel/parallel_for_static.h"
+
+
+
 #endif

c/parallel/api_primitives.h
@@ -1,0 +1,63 @@
+//------------------------------------------------------------------------------
+// Copyright 2019 H2O.ai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//------------------------------------------------------------------------------
+#ifndef dt_PARALLEL_API_PRIMITIVES_h
+#define dt_PARALLEL_API_PRIMITIVES_h
+#include <cstddef>
+#include "utils/assert.h"
+namespace dt {
+using std::size_t;
+
+size_t num_threads_in_pool();
+
+
+
+class ChunkSize {
+  size_t value;
+  public:
+    explicit ChunkSize(size_t sz) {
+      value = sz? sz : 1;
+    }
+    ChunkSize(const ChunkSize&) = default;
+
+    size_t get() const noexcept {
+      return value;
+    }
+};
+
+
+class NThreads {
+  private:
+    size_t value;
+
+  public:
+    explicit NThreads() : value(num_threads_in_pool()) {}
+    explicit NThreads(int nth) : NThreads(static_cast<size_t>(nth)) {}
+    explicit NThreads(size_t nth) {
+      size_t n0 = num_threads_in_pool();
+      value = (nth == 0 || nth >= n0) ? n0 : nth;
+    }
+    NThreads(const NThreads&) = default;
+
+    size_t get() const {
+      xassert(value > 0 && value <= num_threads_in_pool());
+      return value;
+    }
+};
+
+
+
+}  // namespace dt
+#endif

c/parallel/parallel_for_static.cc
@@ -1,81 +1,0 @@
-//------------------------------------------------------------------------------
-// Copyright 2019 H2O.ai
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//------------------------------------------------------------------------------
-#include "parallel/api.h"
-#include "parallel/thread_pool.h"
-#include "utils/assert.h"
-#include "utils/function.h"
-namespace dt {
-
-
-//------------------------------------------------------------------------------
-// parallel_for_static
-//------------------------------------------------------------------------------
-
-void _parallel_for_static(size_t nrows, size_t min_chunk_size,
-                          size_t nthreads_in,
-                          std::function<void(size_t, size_t)> fn)
-{
-  size_t nthreads = nthreads_in? nthreads_in : dt::num_threads_in_pool();
-  size_t k = std::min(nrows / min_chunk_size, nthreads);
-  size_t ith = dt::this_thread_index();
-
-  // Standard parallel loop
-  if (!thpool->in_parallel_region()) {
-    if (k <= 1) {
-      fn(0, nrows);
-    }
-    else {
-      size_t nth = std::min(k, num_threads_in_pool());
-      size_t chunksize = nrows / k;
-      size_t nchunks = nrows / chunksize;
-
-      dt::parallel_region(nth,
-        [=] {
-          size_t ithread = this_thread_index();
-          for (size_t j = ithread; j < nchunks; j += nth) {
-            size_t i0 = j * chunksize;
-            size_t i1 = i0 + chunksize;
-            if (j == nchunks - 1) i1 = nrows;
-            fn(i0, i1);
-          }
-        });
-    }
-  }
-  // Parallel loop within a parallel region
-  else {
-    if (k <= 1) {
-      if (ith == 0) fn(0, nrows);
-    }
-    else {
-      size_t nth = num_threads_in_team();
-      // Cannot change numnber of threads, if in a parallel region
-      xassert(nth == nthreads);
-      size_t chunksize = nrows / k;
-      size_t nchunks = nrows / chunksize;
-
-      for (size_t j = ith; j < nchunks; j += nth) {
-        size_t i0 = j * chunksize;
-        size_t i1 = i0 + chunksize;
-        if (j == nchunks - 1) i1 = nrows;
-        fn(i0, i1);
-      }
-    }
-  }
-}
-
-
-
-} // namespace dt

c/parallel/parallel_for_static.h
@@ -1,0 +1,210 @@
+//------------------------------------------------------------------------------
+// Copyright 2019 H2O.ai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//------------------------------------------------------------------------------
+#ifndef dt_PARALLEL_FOR_STATIC_h
+#define dt_PARALLEL_FOR_STATIC_h
+#include <algorithm>
+#include "parallel/api_primitives.h"
+#include "utils/assert.h"
+namespace dt {
+
+// Declared in api.h
+size_t this_thread_index();
+size_t num_threads_in_pool();
+size_t num_threads_in_team();
+
+
+
+/**
+ * Run a for-loop in parallel, using static scheduling. The iterations
+ * will be distributed uniformly across the available threads, each
+ * thread will be processing a contiguous sub-range of iterations. The
+ * `func` argument should be a function with the signature
+ * `void(size_t i)`, and it will be called for all values of `i` in
+ * the range [0 .. n_iterations-1]. For example:
+ *
+ *     dt::parallel_for_static(100, NThreads(4),
+ *       [&](size_t i) {
+ *         // do something for iteration i
+ *       });
+ *
+ * Each thread will be assigned 25 observations to process:
+ *
+ *     Thread 0: [ 0:25]
+ *     Thread 1: [25:50]
+ *     Thread 2: [50:75]
+ *     Thread 3: [75:100]
+ *
+ * The parameter `nthreads` is optional, if omitted it will default to
+ * using all available threads.
+ *
+ * This function must not be called within a parallel region. See
+ * `nested_for_static()` instead.
+ */
+template <typename F>
+void parallel_for_static(size_t n_iterations, NThreads nthreads, F func) {
+  xassert(num_threads_in_team() == 0);
+  xassert(n_iterations <= size_t(-1) / nthreads.get());
+
+  // Fast case: the number of iterations is either 0 or 1
+  if (n_iterations <= 1) {
+    if (n_iterations) {
+      func(0);
+    }
+    return;
+  }
+
+  parallel_region(
+    nthreads.get(),
+    [=] {
+      size_t ith = this_thread_index();
+      size_t nth = num_threads_in_team();
+      size_t i0 = n_iterations * ith / nth;
+      size_t i1 = n_iterations * (ith + 1) / nth;
+      for (size_t i = i0; i < i1; ++i) {
+        func(i);
+      }
+    });
+}
+
+template <typename F>
+void parallel_for_static(size_t n_iterations, F func) {
+  parallel_for_static(n_iterations, NThreads(), func);
+}
+
+
+
+/**
+ * Run a for-loop in parallel, using static scheduling with an
+ * explicit chunk size. The `func` argument should be a function with
+ * the signature `void(size_t i)`, which will be called for all values
+ * of `i` in the range [0 .. n_iterations-1]. For example:
+ *
+ *     dt::parallel_for_static(100, ChunkSize(13), NThreads(4),
+ *       [&](size_t i) {
+ *         // do something for iteration i
+ *       });
+ *
+ * Here the iterations will be assigned as follows:
+ *
+ *     Thread 0: [ 0:13] + [52:65]
+ *     Thread 1: [13:26] + [65:78]
+ *     Thread 2: [26:39] + [78:91]
+ *     Thread 3: [39:52] + [91:100]
+ *
+ * The `nthreads` argument is optional; if omitted all threads in the
+ * pool will be used.
+ *
+ * This function must not be called within another parallel region.
+ * See `nested_for_static()` instead.
+ */
+template <typename F>
+void parallel_for_static(size_t n_iterations,
+                         ChunkSize chunk_size,
+                         NThreads nthreads,
+                         F func)
+{
+  xassert(num_threads_in_team() == 0);
+  size_t chunk_size_ = chunk_size.get();
+  size_t num_threads = nthreads.get();
+
+  // Fast case: the number of rows is too small compared to the
+  // chunk size, no need to start a parallel region
+  if (n_iterations <= chunk_size_ || num_threads == 1) {
+    for (size_t i = 0; i < n_iterations; ++i) {
+      func(i);
+    }
+    return;
+  }
+
+  parallel_region(
+    num_threads,
+    [=] {
+      size_t i0 = chunk_size_ * this_thread_index();
+      size_t di = chunk_size_ * num_threads;
+      while (i0 < n_iterations) {
+        size_t i1 = std::min(i0 + chunk_size_, n_iterations);
+        for (size_t i = i0; i < i1; ++i) {
+          func(i);
+        }
+        i0 += di;
+      }
+    });
+}
+
+template <typename F>
+void parallel_for_static(size_t n_iterations, ChunkSize cs, F func) {
+  parallel_for_static(n_iterations, cs, NThreads(), func);
+}
+
+
+
+/**
+ * Similar to `parallel_for_static(n_iterations, func)`, however this
+ * function is expected to be called from within a parallel region.
+ *
+ * This function will effectively execute the loop
+ *
+ *     for (size_t i = 0; i < n_iterations; ++i) {
+ *       func(i);
+ *     }
+ *
+ * but the actual iterations will be distributed among all threads
+ * in the team.
+ *
+ * There is no implicit barrier at the end of this function, meaning
+ * that as each thread finishes its iterations, it will not wait for
+ * the other threads to finish theirs. If a barrier is desired, add
+ * an explicit call to `dt::barrier()` at the end.
+ */
+template <typename F>
+void nested_for_static(size_t n_iterations, F func) {
+  size_t ith = this_thread_index();
+  size_t nth = num_threads_in_team();
+  xassert(nth > 0);
+
+  size_t i0 = n_iterations * ith / nth;
+  size_t i1 = n_iterations * (ith + 1) / nth;
+  for (size_t i = i0; i < i1; ++i) {
+    func(i);
+  }
+}
+
+
+
+/**
+ * Similar to `parallel_for_static(n_iterations, chunk_size, func)`,
+ * except that this function can only be called within a parallel
+ * region.
+ */
+template <typename F>
+void nested_for_static(size_t n_iterations, ChunkSize chunk_size, F func)
+{
+  size_t chsize = chunk_size.get();
+  size_t i0 = chsize * this_thread_index();
+  size_t di = chsize * num_threads_in_team();
+  while (i0 < n_iterations) {
+    size_t i1 = std::min(i0 + chsize, n_iterations);
+    for (size_t i = i0; i < i1; ++i) {
+      func(i);
+    }
+    i0 += di;
+  }
+}
+
+
+
+} // namespace dt
+#endif

c/parallel/ztest_parallel_for.cc
@@ -23,10 +23,10 @@ namespace dttest {
 
 
 void test_parallel_for_static(size_t n) {
-  for (size_t nth = 0; nth <= dt::num_threads_in_pool(); ++nth) {
+  for (size_t nth = 0; nth <= dt::num_threads_in_pool() * 2; ++nth) {
     std::vector<size_t> data(n, 0);
 
-    dt::parallel_for_static(n, 1, nth,
+    dt::parallel_for_static(n, dt::ChunkSize(1), dt::NThreads(nth),
       [&](size_t i) {
         data[i] += 1 + 2 * i;
       });

commit be7646aeac48417bc18a96a9b03d254bd9acf31a

Fix failing FTRL tests (#1900)
Fix FTRL tests that were failing due to changes introduced in #1899

c/models/dt_ftrl.cc
@@ -472,7 +472,7 @@ FtrlFitOutput Ftrl<T>::fit(T(*linkfn)(T), U(*targetfn)(U, size_t), T(*lossfn)(T,
 
   // If we request more threads than is available, `dt::parallel_region()`
   // will fall back to the possible maximum.
-  size_t nthreads = std::max(iteration_nrows / dt::FtrlBase::MIN_ROWS_PER_THREAD, 1lu);
+  size_t nthreads = get_nthreads(iteration_nrows);
   dt::parallel_region(nthreads,
     [&]() {
       // Each thread gets a private storage for hashes,
@@ -678,8 +678,7 @@ dtptr Ftrl<T>::predict(const DataTable* dt_X) {
                                  << "the model was trained in an unknown mode";
   }
 
-
-  size_t nthreads = dt_X->nrows / dt::FtrlBase::MIN_ROWS_PER_THREAD;
+  size_t nthreads = get_nthreads(dt_X->nrows);
   nthreads = std::min(std::max(nthreads, 1lu), dt::num_threads_in_pool());
   bool k_binomial;
 

c/models/dt_ftrl_base.cc
@@ -20,6 +20,7 @@
 // IN THE SOFTWARE.
 //------------------------------------------------------------------------------
 #include "models/dt_ftrl_base.h"
+#include "parallel/api.h"
 
 
 namespace dt {
@@ -29,6 +30,17 @@ namespace dt {
  *  Destructor for the abstract `dt::FtrlBase` class.
  */
 FtrlBase::~FtrlBase() {}
+
+
+/**
+ *  Calculate how many threads we need to run FTRL on.
+ */
+size_t FtrlBase::get_nthreads(size_t nrows) {
+  size_t nth = (nrows > MIN_ROWS_PER_THREAD)?
+               std::min(dt::num_threads_in_pool(), nrows / MIN_ROWS_PER_THREAD) :
+               1;
+  return nth;
+}
 
 
 }

c/models/dt_ftrl_base.h
@@ -111,6 +111,7 @@ class FtrlBase {
     virtual bool get_negative_class() = 0;
     virtual FtrlParams get_params() = 0;
     virtual DataTable* get_labels() = 0;
+    static size_t get_nthreads(size_t);
 
     // Setters
     virtual void set_model(DataTable*) = 0;

c/models/label_encode.h
@@ -23,9 +23,11 @@
 #include "parallel/api.h"
 #include "parallel/shared_mutex.h"
 #include "utils/exceptions.h"
+#include "models/dt_ftrl_base.h"
 
 
 namespace dt {
+
 
 void label_encode(const Column*, dtptr&, dtptr&, bool is_binomial = false);
 void label_encode_bool(const Column*, dtptr&, dtptr&);
@@ -109,7 +111,8 @@ void label_encode_fw(const Column* col, dtptr& dt_labels, dtptr& dt_encoded) {
 
 
   auto data = static_cast<const T_from*>(col->data());
-  dt::parallel_for_static(nrows,
+
+  dt::parallel_for_static(nrows, NThreads(dt::FtrlBase::get_nthreads(nrows)),
     [&](size_t irow) {
       size_t jrow = ri[irow];
       T_from v = data[jrow];
@@ -154,7 +157,6 @@ void label_encode_fw(const Column* col, dtptr& dt_labels, dtptr& dt_encoded) {
  */
 template <typename U, SType stype_to>
 void label_encode_str(const Column* col, dtptr& dt_labels, dtptr& dt_encoded) {
-  // std::cout << "stype_to bool: " << (stype_to == SType::BOOL) << "\n";
   using T_to = element_t<stype_to>;
   const size_t nrows = col->nrows;
   const RowIndex& ri = col->rowindex();
@@ -167,7 +169,7 @@ void label_encode_str(const Column* col, dtptr& dt_labels, dtptr& dt_encoded) {
   const U* offsets = scol->offsets();
   const char* strdata = scol->strdata();
 
-  dt::parallel_for_static(nrows,
+  dt::parallel_for_static(nrows, NThreads(dt::FtrlBase::get_nthreads(nrows)),
     [&](size_t irow) {
       size_t jrow = ri[irow];
 

tests/models/test_ftrl.py
@@ -773,7 +773,6 @@ def test_ftrl_fit_predict_binomial_online_1_2():
     assert max(delta_even) < epsilon
 
 
-@pytest.mark.skip(reason="Unstable since #1899")
 def test_ftrl_fit_predict_binomial_online_2_1():
     ft = Ftrl(alpha = 0.1, nepochs = 10000, model_type = "binomial")
     df_train_even_odd = dt.Frame([[2, 1, 8, 3]])
@@ -812,7 +811,6 @@ def test_ftrl_fit_predict_binomial_online_2_1():
     assert max(delta_even) < epsilon
 
 
-@pytest.mark.skip(reason="Unstable since #1899")
 def test_ftrl_fit_predict_binomial_online_2_2():
     ft = Ftrl(alpha = 0.1, nepochs = 10000, model_type = "binomial")
     df_train_even_odd = dt.Frame([[2, 1, 8, 3]])
@@ -857,7 +855,6 @@ def test_ftrl_fit_multinomial_none():
     assert res.loss is None
 
 
-@pytest.mark.skip(reason="Unstable since #1899")
 def test_ftrl_fit_predict_multinomial_vs_binomial():
     target_names = ["cat", "dog"]
     ft_binomial = Ftrl(nbins = 10, nepochs = 1)
@@ -915,7 +912,6 @@ def test_ftrl_fit_predict_multinomial():
     assert p.names == labels
 
 
-@pytest.mark.skip(reason="Unstable since #1899")
 def test_ftrl_fit_predict_multinomial_online():
     ft = Ftrl(alpha = 0.2, nepochs = 1000, double_precision = True)
     labels = ["green", "red", "blue"]