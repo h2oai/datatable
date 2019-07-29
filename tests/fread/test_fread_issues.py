bd9a378bfbe22136fba6da1a102d9207c79e18dd
Pasha Stetsenko <pasha@h2o.ai>
2019-07-29 11:38:26 -0700

    Do not ignore errors in fread(cmd=)

--- tests/fread/test_fread_issues.py
+++ tests/fread/test_fread_issues.py
@@ -15,6 +15,17 @@ from datatable.internal import frame_integrity_check
 from tests import find_file, same_iterables
 
 
+def test_issue1935():
+    # Check that giving wrong command to `cmd=` parameter raises
+    # an error instead of returning an empty Frame silently.
+    with pytest.raises(ValueError) as e:
+        dt.fread(cmd="leeroy jenkins")
+    assert ("Shell command returned error code" in str(e.value))
+    # This may need adjustments for different OSes
+    assert ("leeroy: command not found" in str(e.value))
+
+
+
 #-------------------------------------------------------------------------------
 
 def test_issue_R1113():
