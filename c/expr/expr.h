d7b2b7604f8c732d710c162816255604651f5206
Pasha Stetsenko <pasha@h2o.ai>
2019-08-28 15:38:11 -0700

    Move definition of expr::Op into a separate header file

--- c/expr/expr.h
+++ c/expr/expr.h
@@ -21,148 +21,40 @@
 //------------------------------------------------------------------------------
 #ifndef dt_EXPR_EXPR_h
 #define dt_EXPR_EXPR_h
+#include "expr/op.h"
 #include "expr/workframe.h"
+#include "python/obj.h"
 #include "column.h"
 namespace dt {
 namespace expr {
 
+
+//------------------------------------------------------------------------------
+// Expr
+//------------------------------------------------------------------------------
+
+class Expr {
+  private:
+    std::vector<Expr> inputs;
+    std::vector<Column> outputs;
+    bool output_is_grouped;
+    size_t : 56;
+
+  public:
+    Expr(py::robj src, int flags);
+};
+
+
+
+
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+
 class base_expr;
 using pexpr = std::unique_ptr<base_expr>;
 
-static constexpr size_t UNOP_FIRST    = 101;
-static constexpr size_t UNOP_LAST     = 111;
-static constexpr size_t BINOP_FIRST   = 201;
-static constexpr size_t BINOP_LAST    = 218;
-static constexpr size_t STRING_FIRST  = 301;
-static constexpr size_t REDUCER_FIRST = 401;
-static constexpr size_t REDUCER_LAST  = 409;
-static constexpr size_t MATH_FIRST    = 501;
-static constexpr size_t MATH_LAST     = 542;
-static constexpr size_t UNOP_COUNT    = UNOP_LAST - UNOP_FIRST + 1;
-static constexpr size_t BINOP_COUNT   = BINOP_LAST - BINOP_FIRST + 1;
-static constexpr size_t REDUCER_COUNT = REDUCER_LAST - REDUCER_FIRST + 1;
-
-// The values in this enum must be kept in sync with Python enum OpCodes in
-// file "datatable/expr/expr.py"
-enum class Op : size_t {
-  // Misc
-  NOOP = 0,
-  COL = 1,
-  CAST = 2,
-  SETPLUS = 3,
-  SETMINUS = 4,
-
-  // Unary
-  UPLUS = UNOP_FIRST,
-  UMINUS,
-  UINVERT,
-  ISFINITE,
-  ISINF,
-  ISNA,
-  ABS,
-  CEIL,
-  FLOOR,
-  TRUNC,
-  LEN = UNOP_LAST,
-
-  // Binary
-  PLUS = BINOP_FIRST,
-  MINUS,
-  MULTIPLY,
-  DIVIDE,
-  INTDIV,
-  MODULO,
-  POWER,
-  AND,
-  XOR,
-  OR,
-  LSHIFT,
-  RSHIFT,
-  EQ,
-  NE,
-  LT,
-  GT,
-  LE,
-  GE = BINOP_LAST,
-
-  // String
-  RE_MATCH = STRING_FIRST,
-
-  // Reducers
-  MEAN = REDUCER_FIRST,
-  MIN,
-  MAX,
-  STDEV,
-  FIRST,
-  SUM,
-  COUNT,
-  COUNT0,
-  MEDIAN = REDUCER_LAST,
-
-  // Math: trigonometric
-  SIN = MATH_FIRST,
-  COS,
-  TAN,
-  ARCSIN,
-  ARCCOS,
-  ARCTAN,
-  ARCTAN2,
-  HYPOT,
-  DEG2RAD,
-  RAD2DEG,
-
-  // Math: hyperbolic
-  SINH,
-  COSH,
-  TANH,
-  ARSINH,
-  ARCOSH,
-  ARTANH,
-
-  // Math: exponential/power
-  CBRT,
-  EXP,
-  EXP2,
-  EXPM1,
-  LOG,
-  LOG10,
-  LOG1P,
-  LOG2,
-  LOGADDEXP,
-  LOGADDEXP2,
-  // POWER,
-  SQRT,
-  SQUARE,
-
-  // Math: special
-  ERF,
-  ERFC,
-  GAMMA,
-  LGAMMA,
-
-  // Math: floating-point
-  // CEIL,
-  COPYSIGN,
-  FABS,
-  // FLOOR,
-  // FREXP: double->(double, int)
-  // ISCLOSE: non-trivial signature
-  LDEXP,
-  NEXTAFTER,
-  // RINT ?
-  SIGN,
-  SIGNBIT,
-  // SPACING ?
-  // TRUNC,
-
-  // Math: misc
-  CLIP,
-  // DIVMOD (double,double)->(double,double)
-  // MODF double->(double,double)
-  MAXIMUM,
-  MINIMUM,
-  FMOD = MATH_LAST,
-};
 
 
 
