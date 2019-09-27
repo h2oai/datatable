8fd284ffbbc29fa05f6da01ce4997ce89d16a131
Pasha Stetsenko <pasha@h2o.ai>
2019-09-27 15:46:22 -0700

    Remove buffer-reuse optimization in expr_unaryop: it is no longer needed with virtual columns

--- c/expr/expr_unaryop.cc
+++ c/expr/expr_unaryop.cc
@@ -285,29 +285,13 @@ GroupbyMode expr_unaryop::get_groupby_mode(const EvalContext& ctx) const {
 }
 
 
-// In this method we use the following shortcut:
-// if the `input_column` evaluated from `arg` is "writable" (meaning that
-// its reference-counter is 1, it is allocated in RAM and not marked readonly),
-// then this column's memory buffer can be reused to create the output column.
-// Indeed, if nobody else holds a reference to the same memory range, then it
-// will be deallocated at the end of this function anyways, together with the
-// `input_column`.
-//
-// This works only if each element in the output array corresponds to an
-// element in the input at exactly the same memory location. Thus, this
-// optimization is only applicable when both columns are "fixed-width" and
-// element sizes in the input and output columns are the same. Notably,
-// if the input is a string column, then it cannot be safely repurposed into
-// an integer output column because each "element" of the input actually
-// consists of 2 entries: the offsets of the start and the end of a string.
-//
 Column expr_unaryop::evaluate(EvalContext& ctx) {
   Column input_column = arg->evaluate(ctx);
 
   auto input_stype = input_column.stype();
   const auto& ui = unary_library.get_infox(opcode, input_stype);
   if (ui.cast_stype != SType::VOID) {
-    input_column = input_column.cast(ui.cast_stype);
+    input_column.cast_inplace(ui.cast_stype);
   }
   if (ui.vectorfn == nullptr) {
     return input_column;
@@ -315,26 +299,9 @@ Column expr_unaryop::evaluate(EvalContext& ctx) {
   input_column.materialize();
 
   size_t nrows = input_column.nrows();
-  const Buffer& input_mbuf = input_column->data_buf();
-  const void* inp = input_mbuf.rptr();
-
-  Buffer output_mbuf;
-  void* out = nullptr;
-  size_t out_elemsize = info(ui.output_stype).elemsize();
-  if (input_mbuf.is_writable() &&
-      input_column.is_fixedwidth() &&
-      input_column.elemsize() == out_elemsize) {
-    // The address `out` must be taken *before* `output_mbuf` is initialized,
-    // since the `.xptr()` method checks that the reference counter is 1.
-    out = input_mbuf.xptr();
-    output_mbuf = Buffer(input_mbuf);
-  }
-  else {
-    output_mbuf = Buffer::mem(out_elemsize * nrows);
-    out = output_mbuf.xptr();
-  }
-  auto output_column =
-      Column::new_mbuf_column(ui.output_stype, std::move(output_mbuf));
+  const void* inp =  input_column.get_data_readonly();
+  Column output_column = Column::new_data_column(ui.output_stype, nrows);
+  void* out = output_column.get_data_editable();
 
   ui.vectorfn(nrows, inp, out);
 
