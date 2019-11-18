0278c2eb4e366b0bc04beed90ceb5433d6297994
Pasha Stetsenko <pasha@h2o.ai>
2019-11-18 12:51:47 -0800

    Implement operators // and %

--- c/expr/fbinary/bimaker.cc
+++ c/expr/fbinary/bimaker.cc
@@ -49,8 +49,8 @@ bimaker_ptr resolve_op(Op opcode, SType stype1, SType stype2) {
     case Op::MINUS:    return resolve_op_minus(stype1, stype2);
     case Op::MULTIPLY: return resolve_op_multiply(stype1, stype2);
     case Op::DIVIDE:   return resolve_op_divide(stype1, stype2);
-    case Op::INTDIV:
-    case Op::MODULO:
+    case Op::INTDIV:   return resolve_op_intdiv(stype1, stype2);
+    case Op::MODULO:   return resolve_op_modulo(stype1, stype2);
     case Op::POWER:    return bimaker_ptr();
     case Op::AND:      return resolve_op_and(stype1, stype2);
     case Op::OR:       return resolve_op_or(stype1, stype2);
