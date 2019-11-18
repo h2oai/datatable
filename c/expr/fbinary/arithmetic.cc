57502b27a9298b2dfa26385a37526ba6f6bf6208
Pasha Stetsenko <pasha@h2o.ai>
2019-11-18 13:45:24 -0800

    Implement power (**) operator

--- c/expr/fbinary/arithmetic.cc
+++ c/expr/fbinary/arithmetic.cc
@@ -389,6 +389,71 @@ bimaker_ptr resolve_op_modulo(SType stype1, SType stype2)
 
 
 
+//------------------------------------------------------------------------------
+// Op::POWER (**)
+//------------------------------------------------------------------------------
+
+template <typename T>
+inline T op_power(T x, T y) {
+  return static_cast<T>(std::pow(static_cast<double>(x),
+                                 static_cast<double>(y)));
+}
+
+template <>
+inline float op_power(float x, float y) {
+  return std::powf(x, y);
+}
+
+
+template <typename T>
+static inline bimaker_ptr _power(SType uptype1, SType uptype2, SType outtype) {
+  assert_compatible_type<T>(outtype);
+  if (uptype1 != SType::VOID) assert_compatible_type<T>(uptype1);
+  if (uptype2 != SType::VOID) assert_compatible_type<T>(uptype2);
+  return bimaker1<T, T, T>::make(op_power<T>, uptype1, uptype2, outtype);
+}
+
+
+/**
+  * Operator `**` implements the following rules:
+  *
+  *   VOID ** {*} -> VOID
+  *   {BOOL, INT8, INT16, INT32} ** {same} -> INT32
+  *   INT64 ** {BOOL, INT*} -> INT64
+  *   FLOAT32 ** {BOOL, INT*, FLOAT32} -> FLOAT32
+  *   FLOAT64 ** {BOOL, INT*, FLOAT*} -> FLOAT64
+  *
+  * Note that these rules imply that `2 ** -2 == 0`, while
+  * `2 ** -2.0 == 0.25`. This may be somewhat counterintuitive if the
+  * user tries to compute `f.A ** -1` for example. In this case we
+  * recommend to write `f.A ** -1.0` instead. In the future we may be
+  * able to automatically apply such fix, in case when the power is
+  * a negative python integer.
+  *
+  */
+bimaker_ptr resolve_op_power(SType stype1, SType stype2)
+{
+  if (stype1 == SType::VOID || stype2 == SType::VOID) {
+    return bimaker_nacol::make();
+  }
+  SType stype0 = _find_common_stype(stype1, stype2);
+  if (stype0 == SType::BOOL || stype0 == SType::INT8 || stype0 == SType::INT16) {
+    stype0 = SType::INT32;
+  }
+  SType uptype1 = (stype1 == stype0)? SType::VOID : stype0;
+  SType uptype2 = (stype2 == stype0)? SType::VOID : stype0;
+  switch (stype0) {
+    case SType::INT32:   return _power<int32_t>(uptype1, uptype2, stype0);
+    case SType::INT64:   return _power<int64_t>(uptype1, uptype2, stype0);
+    case SType::FLOAT32: return _power<float>(uptype1, uptype2, stype0);
+    case SType::FLOAT64: return _power<double>(uptype1, uptype2, stype0);
+    default:
+      throw TypeError() << "Operator `**` cannot be applied to columns of "
+        "types `" << stype1 << "` and `" << stype2 << "`";
+  }
+}
+
+
 
 
 }}  // namespace dt::expr
