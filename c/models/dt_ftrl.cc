7b56d1b7a9735b1acaff1c93a1ace9497e74274e
oleksiyskononenko <35204136+oleksiyskononenko@users.noreply.github.com>
2019-11-24 09:31:02 -0800

    Add <algorithm> include (#2200)
    Standard header includes are slightly different on Windows, so in some files where we use std::min or std::max we need to explicitly include <algorithm>, otherwise compilation fails.
    
    WIP for #1369

--- c/models/dt_ftrl.cc
+++ c/models/dt_ftrl.cc
@@ -19,6 +19,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 //------------------------------------------------------------------------------
+#include <algorithm>            // std::max
 #include "frame/py_frame.h"
 #include "models/dt_ftrl.h"
 #include "parallel/api.h"
--- c/progress/progress_bar.cc
+++ c/progress/progress_bar.cc
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //------------------------------------------------------------------------------
+#include <algorithm>                // std::max
 #include "progress/_options.h"
 #include "progress/progress_bar.h"
 #include "python/string.h"          // py::ostring
