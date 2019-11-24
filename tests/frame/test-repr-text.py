f05652edab4ac2cb30cf1eca36b7a37f895c20bc
Pasha Stetsenko <pasha@h2o.ai>
2019-11-23 19:07:28 -0800

    Fix test_horizontal_elision on Travis (#2198)

--- tests/frame/test-repr-text.py
+++ tests/frame/test-repr-text.py
@@ -22,6 +22,7 @@
 # IN THE SOFTWARE.
 #-------------------------------------------------------------------------------
 import datatable as dt
+import os
 import pytest
 import re
 
@@ -318,12 +319,23 @@ def test_horizontal_elision(capsys):
         DT.view(interactive=False)
     out, err = capsys.readouterr()
     assert not err
-    # The output is truncated to 120 width (default terminal width for non-tty)
-    check_colored_output(out,
-        "   | C0                              C1                              C2                …  C19                           ",
-        "-- + ------------------------------  ------------------------------  ----------------     ------------------------------",
-        " 0 | 123456789012345678901234567890  123456789012345678901234567890  123456789012345…  …  123456789012345678901234567890",
-        "[1 row x 20 columns]")
+    if os.environ.get("TRAVIS"):
+        # On Travis the output is truncated to 80 width
+        check_colored_output(out,
+            "   | C0                              C1        …  C19                           ",
+            "-- + ------------------------------  --------     ------------------------------",
+            " 0 | 123456789012345678901234567890  1234567…  …  123456789012345678901234567890",
+            "[1 row x 20 columns]"
+            )
+    else:
+        # Normally the output is truncated to 120 width (default terminal width for non-tty)
+        check_colored_output(out,
+            "   | C0                              C1                              C2                …  C19                           ",
+            "-- + ------------------------------  ------------------------------  ----------------     ------------------------------",
+            " 0 | 123456789012345678901234567890  123456789012345678901234567890  123456789012345…  …  123456789012345678901234567890",
+            "[1 row x 20 columns]")
+
+
 
 
 
