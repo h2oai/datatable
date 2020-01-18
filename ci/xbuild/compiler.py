bb36553cca2a11c90e67770a1bd2cc7ccff15f10
Oleksiy Kononenko <oleksiy.s.kononenko@gmail.com>
2020-01-18 01:01:15 -0800

    Use most recent version of compiler and WinSDK

--- ci/xbuild/compiler.py
+++ ci/xbuild/compiler.py
@@ -152,7 +152,7 @@ class Compiler:
 
                 candidates = []
                 compiler_versions = next(os.walk(msvc_path))[1]
-                for compiler_version in compiler_versions:
+                for compiler_version in reversed(compiler_versions):
                     path =  os.path.join(msvc_path, compiler_version)
                     bin_path = os.path.join(path, "bin\\Hostx64\\x64")
                     candidates += [{
@@ -208,9 +208,10 @@ class Compiler:
         # Detect the latest available SDK version
         winsdk_version_dir = ""
         winsdk_versions = next(os.walk(winsdk_default_path + "\\include"))[1]
-        for version in winsdk_versions:
+        for version in reversed(winsdk_versions):
             if is_winsdk_version(version):
                 winsdk_version_dir = version
+                break
 
         if winsdk_version_dir is "":
             raise ValueError("A valid Windows SDK version directory %s not found.")
