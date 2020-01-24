f6b49037083db871edbb0fedf96ae211a4ed4b31
mmalohlava <michal@h2o.ai>
2020-01-24 10:39:38 -0800

    [CI] :fire: Missing escape for regex back-reference

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -508,7 +508,7 @@ ansiColor('xterm') {
                             //        same, and that if we are in release mode
                             //        the version contains only digits and dots.
                             //
-                            versionText = sh(script: """sed -ne "s/.*version='\\([^']*\\)',/\1/p" src/datatable/_build_info.py""", returnStdout: true).trim()
+                            versionText = sh(script: """sed -ne "s/.*version='\\([^']*\\)',/\\1/p" src/datatable/_build_info.py""", returnStdout: true).trim()
                             sh "echo versionText = ${versionText}"
                             def _versionText = versionText
                             s3upDocker {
