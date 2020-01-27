e5af8c633168dedad9d71e81a57432e63491204e
Oleksiy Kononenko <oleksiy@h2o.ai>
2020-01-27 12:35:34 -0800

    Remove xasserts, add LCOV_EXCL_LINE

--- src/core/expr/fnary/rowmean.cc
+++ src/core/expr/fnary/rowmean.cc
@@ -87,10 +87,9 @@ Column naryop_rowmean(colvec&& columns) {
   switch (res_stype) {
     case SType::FLOAT32: return _rowmean<float>(std::move(columns));
     case SType::FLOAT64: return _rowmean<double>(std::move(columns));
-    default: xassert(0);
-             throw RuntimeError()
+    default: throw RuntimeError()
                << "Wrong `res_stype` in `naryop_rowmean()`: "
-               << res_stype;
+               << res_stype;  // LCOV_EXCL_LINE
   }
 }
 
