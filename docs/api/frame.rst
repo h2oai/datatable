3d999d5267a677c23a25c343599f8fd057cb479f
Pasha Stetsenko <pasha@h2o.ai>
2020-02-18 12:51:53 -0800

    Add documentation for Frame.countna()

--- docs/api/frame.rst
+++ docs/api/frame.rst
@@ -6,7 +6,7 @@ Frame
     :members:
     :inherited-members:
     :undoc-members:
-    :exclude-members: cbind, colindex, copy, replace, to_csv
+    :exclude-members: cbind, colindex, copy, countna, replace, to_csv
 
 
 .. toctree::
@@ -15,5 +15,6 @@ Frame
 	.cbind()     <frame/cbind>
 	.colindex()  <frame/colindex>
 	.copy()      <frame/copy>
+	.countna()   <frame/countna>
 	.replace()   <frame/replace>
 	.to_csv()    <frame/to_csv>
--- src/core/frame/stats.cc
+++ src/core/frame/stats.cc
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// Copyright 2018 H2O.ai
+// Copyright 2018-2020 H2O.ai
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -54,13 +54,48 @@ static DataTable* _make_frame(DataTable* dt, Stat stat) {
 // Frame functions
 //------------------------------------------------------------------------------
 
+static const char* doc_countna =
+R"(countna(self)
+--
+
+Report the number of NA values in each column of the frame.
+
+Parameters
+----------
+(return): Frame
+    The frame will have 1 row and the same number and names of columns
+    as in the current frame. All columns will have stype ``int64``.
+
+Examples
+--------
+>>> DT = dt.Frame(A=[1, 5, None], B=[math.nan]*3, C=[None, None, 'bah!'])
+>>> DT.countna()
+   |  A   B   C
+-- + --  --  --
+ 0 |  1   3   2
+--
+[1 row x 3 columns]
+
+>>> DT.countna().to_tuples()[0]
+(1, 3, 2)
+
+
+See Also
+--------
+- :meth:`.countna1()` -- similar to this method, but operates on a
+  single-column frame only, and returns a number instead of a Frame.
+
+- :func:`dt.count()` -- function for counting non-NA ("valid") values
+  in a column; can also be applied per-group.
+)";
+
 static PKArgs args_min(0, 0, 0, false, false, {}, "min", nullptr);
 static PKArgs args_max(0, 0, 0, false, false, {}, "max", nullptr);
 static PKArgs args_mode(0, 0, 0, false, false, {}, "mode", nullptr);
 static PKArgs args_sum(0, 0, 0, false, false, {}, "sum", nullptr);
 static PKArgs args_mean(0, 0, 0, false, false, {}, "mean", nullptr);
 static PKArgs args_sd(0, 0, 0, false, false, {}, "sd", nullptr);
-static PKArgs args_countna(0, 0, 0, false, false, {}, "countna", nullptr);
+static PKArgs args_countna(0, 0, 0, false, false, {}, "countna", doc_countna);
 static PKArgs args_nunique(0, 0, 0, false, false, {}, "nunique", nullptr);
 static PKArgs args_nmodal(0, 0, 0, false, false, {}, "nmodal", nullptr);
 
