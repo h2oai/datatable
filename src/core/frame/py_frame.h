82f6206384fb133634052e2efe0fce4564517348
Pasha Stetsenko <pasha@h2o.ai>
2020-02-25 11:26:28 -0800

    Added method dt.Frame.__contains__()

--- src/core/frame/py_frame.h
+++ src/core/frame/py_frame.h
@@ -83,6 +83,7 @@ class Frame : public XObject<Frame> {
     oobj m__copy__();
     oobj m__deepcopy__(const PKArgs&);
     size_t m__len__() const;
+    oobj m__contains__(const PKArgs&);
 
     // Frame display
     oobj m__repr__() const;
