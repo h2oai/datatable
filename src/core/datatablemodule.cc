h2oai/datatable

commit 1bf92eb7348afc34299f7b4a7161a435ebfff8cb

Exceptions reorganized (#2361)
- The exception and warning classes used by datatable are now gathered in the `datatable.exceptions` module (they used to be all over the place);
- All exceptions/warnings are now derived from the common `DtException` class;
- The base `DtException` class, when stringified, removes all backticks from the error message. This ensures that the error message looks the same in the console and when logged into a file. Consequently, many tests had to be adjusted to remove the backticks from error messages.
- The custom exception rendering mechanism ported from typesentry module into datatable and improved;
- Terminal now looks both at stdout and stderr when autodetecting color support;
- Dependency `typesentry` removed;
- Module `datatable.utils.typechecks` removed;

Closes #1535

src/core/datatablemodule.cc
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// Copyright 2018-2019 H2O.ai
+// Copyright 2018-2020 H2O.ai
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -189,11 +189,7 @@ static void _register_function(const py::PKArgs& args) {
   switch (n) {
     case 2: init_py_stype_objs(fnref); break;
     case 3: init_py_ltype_objs(fnref); break;
-    case 4: replace_typeError(fnref); break;
-    case 5: replace_valueError(fnref); break;
-    case 6: replace_dtWarning(fnref); break;
     case 7: py::Frame_Type = fnref; break;
-    case 8: replace_invalidOpError(fnref); break;
     case 9: py::Expr_Type = fnref; break;
     default: throw ValueError() << "Unknown index: " << n;
   }
@@ -263,10 +259,13 @@ static py::oobj apply_color(const py::PKArgs& args) {
 
   dt::TerminalStream ts(true);
   if      (color == "bright_black") ts << dt::style::grey;
-  else if (color == "grey")         ts << dt::style::grey;
+  else if (color == "grey"        ) ts << dt::style::grey;
   else if (color == "bright_green") ts << dt::style::bgreen;
+  else if (color == "dim"         ) ts << dt::style::dim;
+  else if (color == "italic"      ) ts << dt::style::italic;
   else if (color == "yellow"      ) ts << dt::style::yellow;
   else if (color == "bold"        ) ts << dt::style::bold;
+  else if (color == "red"         ) ts << dt::style::red;
   else if (color == "bright_red"  ) ts << dt::style::bred;
   else if (color == "cyan"        ) ts << dt::style::cyan;
   else if (color == "bright_cyan" ) ts << dt::style::bcyan;
@@ -425,8 +424,6 @@ extern "C" {
     PyObject* m = nullptr;
 
     try {
-      init_exceptions();
-
       m = dtmod.init();
 
       // Initialize submodules

src/core/utils/terminal/terminal.cc
@@ -84,7 +84,10 @@ Terminal::~Terminal() = default;
 void Terminal::initialize() {
   py::robj rstdin = py::rstdin();
   py::robj rstdout = py::rstdout();
-  if (!rstdout || !rstdin || rstdout.is_none() || rstdin.is_none()) {
+  py::robj rstderr = py::rstderr();
+  if (!rstdout || !rstdin || !rstderr ||
+      rstdout.is_none() || rstdin.is_none() || rstderr.is_none())
+  {
     enable_keyboard_ = false;
     enable_colors_ = false;
     enable_ecma48_ = false;
@@ -92,6 +95,9 @@ void Terminal::initialize() {
   }
   else {
     allow_unicode_ = false;
+    enable_keyboard_ = false;
+    enable_colors_ = false;
+    enable_ecma48_ = false;
     try {
       std::string encoding = rstdout.get_attr("encoding").to_string();
       if (encoding == "UTF-8" || encoding == "UTF8" ||
@@ -99,9 +105,15 @@ void Terminal::initialize() {
         allow_unicode_ = true;
       }
     } catch (...) {}
-    enable_keyboard_ = true;
-    enable_colors_ = true;
-    enable_ecma48_ = true;
+    try {
+      bool istty = rstdout.get_attr("isatty").call().to_bool_strict() &&
+                   rstderr.get_attr("isatty").call().to_bool_strict();
+      if (istty) {
+        enable_colors_ = true;
+        enable_ecma48_ = true;
+        enable_keyboard_ = true;
+      }
+    } catch (...) {}
     _check_ipython();
   }
   // Set options

Makefile
@@ -83,7 +83,6 @@ geninfo:
 
 .PHONY: coverage
 coverage:
-	$(PYTHON) -m pip install 'typesentry>=0.2.6'
 	$(PYTHON) ci/ext.py coverage
 	$(MAKE) test_install
 	DTCOVERAGE=1 $(PYTHON) -m pytest -x \

appveyor.yml
@@ -12,8 +12,6 @@ build_script:
 - cmd: >-
     SET PATH=C:\Python37-x64;C:\Python37-x64\Scripts;%PATH%
 
-    pip install typesentry
-
     python ci/ext.py build
 
     cd src

ci/ext.py
@@ -364,7 +364,6 @@ def get_meta():
 
         # Runtime dependencies
         requirements=[
-            "typesentry (>=0.2.6)",
             "pytest (>=3.1); extra == 'tests'",
             "docutils (>=0.14); extra == 'tests'",
             "numpy; extra == 'optional'",

src/core/python/args.cc
@@ -150,11 +150,11 @@ void PKArgs::bind(PyObject* _args, PyObject* _kwds)
         n_varkwds++;
         if (has_varkwds) continue;
         throw TypeError() << get_long_name() << " got an unexpected keyword "
-          "argument `" << PyUnicode_AsUTF8(key) << '`';
+          "argument `" << PyUnicode_AsUTF8(key) << "`";
       }
       if (ikey < n_bound_args) {
         throw TypeError() << get_long_name() << " got multiple values for "
-          "argument `" << PyUnicode_AsUTF8(key) << '`';
+          "argument `" << PyUnicode_AsUTF8(key) << "`";
       }
       if (ikey < n_posonly_args) {
         throw TypeError() << get_long_name() << " got argument `"

src/core/python/obj.cc
@@ -57,6 +57,7 @@ PyObject* Expr_Type = nullptr;
 #define _Py_static_string_init(value) { NULL, value, NULL }
 _Py_IDENTIFIER(stdin);
 _Py_IDENTIFIER(stdout);
+_Py_IDENTIFIER(stderr);
 _Py_IDENTIFIER(write);
 
 
@@ -1078,6 +1079,16 @@ robj rstdout() {
       _PySys_GetObjectId(&PyId_stdout)  // borrowed ref
     #else
       PySys_GetObject("stdout")         // borrowed ref
+    #endif
+  );
+}
+
+robj rstderr() {
+  return robj(
+    #ifndef Py_LIMITED_API
+      _PySys_GetObjectId(&PyId_stderr)  // borrowed ref
+    #else
+      PySys_GetObject("stderr")         // borrowed ref
     #endif
   );
 }

src/core/python/obj.h
@@ -372,6 +372,7 @@ oobj False();
 oobj Ellipsis();
 robj rstdin();
 robj rstdout();
+robj rstderr();
 robj rnone();
 
 

commit 0668dc731b61435ca6275994dece2e5ff558703c

Fix memory mapping on WIndows (#2365)
Take into account the `MAP_PRIVATE` flag in the page and file protection routines of mman. This fixes memory mapping functionality when working with the `.jay` files on Windows.

Also fix color rendering in Windows terminal that was broken since #2361.

WIP for #2301

src/core/datatablemodule.cc
@@ -428,7 +428,6 @@ extern "C" {
 
       // Initialize submodules
       if (!init_py_encodings(m)) return nullptr;
-      dt::Terminal::standard_terminal().initialize();
 
       init_types();
       dt::expr::Head_Func::init();

src/core/lib/mman/mman.cc
@@ -43,30 +43,41 @@
       return err;
   }
 
-  static DWORD __map_mmap_prot_page(const int prot)
+  static DWORD __map_mmap_prot_page(const int prot, const int flags = 0)
   {
       DWORD protect = 0;
 
       if (prot == PROT_NONE)
           return protect;
 
+      bool is_private = (flags & MAP_PRIVATE) == MAP_PRIVATE;
       if ((prot & PROT_EXEC) != 0)
       {
-          protect = ((prot & PROT_WRITE) != 0) ?
-                      PAGE_EXECUTE_READWRITE : PAGE_EXECUTE_READ;
+          DWORD page_execute_write = is_private? PAGE_EXECUTE_WRITECOPY :
+                                                 PAGE_EXECUTE_READWRITE;
+
+          protect = ((prot & PROT_WRITE) != 0) ? page_execute_write :
+                                                 PAGE_EXECUTE_READ;
       }
       else
       {
-          protect = ((prot & PROT_WRITE) != 0) ?
-                      PAGE_READWRITE : PAGE_READONLY;
+          DWORD page_write = is_private? PAGE_WRITECOPY :
+                                         PAGE_READWRITE;
+
+          protect = ((prot & PROT_WRITE) != 0) ? page_write :
+                                                 PAGE_READONLY;
       }
 
       return protect;
   }
 
-  static DWORD __map_mmap_prot_file(const int prot)
+  static DWORD __map_mmap_prot_file(const int prot, const int flags = 0)
   {
       DWORD desiredAccess = 0;
+      bool is_private = (flags & MAP_PRIVATE) == MAP_PRIVATE;
+      DWORD file_map_write = is_private? FILE_MAP_COPY :
+                                         FILE_MAP_WRITE;
+
 
       if (prot == PROT_NONE)
           return desiredAccess;
@@ -74,7 +85,7 @@
       if ((prot & PROT_READ) != 0)
           desiredAccess |= FILE_MAP_READ;
       if ((prot & PROT_WRITE) != 0)
-          desiredAccess |= FILE_MAP_WRITE;
+          desiredAccess |= file_map_write;
       if ((prot & PROT_EXEC) != 0)
           desiredAccess |= FILE_MAP_EXECUTE;
 
@@ -96,8 +107,8 @@
                       (DWORD)off : (DWORD)(off & 0xFFFFFFFFL);
       const DWORD dwFileOffsetHigh = (sizeof(off_t) <= sizeof(DWORD)) ?
                       (DWORD)0 : (DWORD)((off >> 32) & 0xFFFFFFFFL);
-      const DWORD protect = __map_mmap_prot_page(prot);
-      const DWORD desiredAccess = __map_mmap_prot_file(prot);
+      const DWORD protect = __map_mmap_prot_page(prot, flags);
+      const DWORD desiredAccess = __map_mmap_prot_file(prot, flags);
 
       const off_t maxSize = off + (off_t)len;
 

src/core/lib/mman/mman.h
@@ -29,6 +29,7 @@
 // version of the original:
 // - added an operating system check, so that this code only builds on Windows;
 // - added MAP_NORESERVE;
+// - added MAP_PRIVATE handling to page and file protection routines;
 // - minor changes to code format.
 //
 //------------------------------------------------------------------------------

src/core/utils/terminal/terminal.cc
@@ -72,6 +72,7 @@ Terminal::Terminal(bool is_plain) : is_plain_(is_plain){
   is_jupyter_ = false;
   is_ipython_ = false;
   if (!enable_ecma48_) xassert(!enable_colors_);
+  if (!is_plain_) _initialize();
 }
 
 Terminal::~Terminal() = default;
@@ -81,7 +82,7 @@ Terminal::~Terminal() = default;
   * This is called for 'standard' terminal only from "datatablemodule.cc",
   * once during module initialization.
   */
-void Terminal::initialize() {
+void Terminal::_initialize() {
   py::robj rstdin = py::rstdin();
   py::robj rstdout = py::rstdout();
   py::robj rstderr = py::rstderr();
@@ -113,6 +114,16 @@ void Terminal::initialize() {
         enable_ecma48_ = true;
         enable_keyboard_ = true;
       }
+
+      // To render colors on Windows, we switch terminal to the ANSI mode
+      #if DT_OS_WINDOWS
+        DWORD dw_mode = ENABLE_PROCESSED_INPUT |
+                        ENABLE_ECHO_INPUT |
+                        ENABLE_LINE_INPUT;
+        HANDLE h_console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleMode(h_console_handle, dw_mode);
+      #endif
+
     } catch (...) {}
     _check_ipython();
   }

src/core/utils/terminal/terminal.h
@@ -58,7 +58,6 @@ class Terminal {
   public:
     static Terminal& standard_terminal();
     static Terminal& plain_terminal();
-    void initialize();
 
     bool is_jupyter() const noexcept;
     bool is_ipython() const noexcept;
@@ -77,6 +76,7 @@ class Terminal {
     Terminal(Terminal&&) = delete;
     ~Terminal();
 
+    void _initialize();
     void _check_ipython();
     void _detect_window_size();
 };