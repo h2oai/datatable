f8e4f33592974053ae592de16833aa36f9315b8f
Pasha Stetsenko <pasha@h2o.ai>
2020-03-19 18:15:00 -0700

    rename py_csv.cc -> py_fread.cc

--- src/core/read/py_fread.cc
+++ src/core/read/py_fread.cc
@@ -19,20 +19,17 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 //------------------------------------------------------------------------------
-#include <vector>
-#include <stdlib.h>
-#include "csv/reader.h"
-#include "frame/py_frame.h"
-#include "python/string.h"
-#include "read/read_source.h"
-#include "datatablemodule.h"
-#include "options.h"
-
-namespace py {
+#include "csv/reader.h"        // GenericReader
+#include "python/args.h"       // py::PKArgs
+#include "python/string.h"     // py::ostring
+#include "read/read_source.h"  // dt::read::resolve_sources
+#include "datatablemodule.h"   // DatatableModule
+namespace dt {
+namespace read {
 
 
 //------------------------------------------------------------------------------
-// fread()
+// fread() python function
 //------------------------------------------------------------------------------
 
 static const char* doc_fread =
@@ -44,11 +41,9 @@ R"(fread(anysource=None, *, file=None, text=None, cmd=None, url=None,
          tempdir=None, nthreads=None, logger=None)
 --
 
-Generic read function, similar to `fread` but supports other
-file types, not just csv.
 )";
 
-static PKArgs args_fread(
+static py::PKArgs args_fread(
   1, 0, 22, false, false,
   {"anysource", "file", "text", "cmd", "url",
    "columns", "sep", "dec", "max_nrows", "header", "na_strings",
@@ -58,32 +53,32 @@ static PKArgs args_fread(
    },
   "fread", doc_fread);
 
-static oobj fread(const PKArgs& args) {
+static py::oobj fread(const py::PKArgs& args) {
   size_t k = 0;
-  const Arg& arg_anysource  = args[k++];
-  const Arg& arg_file       = args[k++];
-  const Arg& arg_text       = args[k++];
-  const Arg& arg_cmd        = args[k++];
-  const Arg& arg_url        = args[k++];
+  const py::Arg& arg_anysource  = args[k++];
+  const py::Arg& arg_file       = args[k++];
+  const py::Arg& arg_text       = args[k++];
+  const py::Arg& arg_cmd        = args[k++];
+  const py::Arg& arg_url        = args[k++];
 
-  const Arg& arg_columns    = args[k++];
-  const Arg& arg_sep        = args[k++];
-  const Arg& arg_dec        = args[k++];
-  const Arg& arg_maxnrows   = args[k++];
-  const Arg& arg_header     = args[k++];
-  const Arg& arg_nastrings  = args[k++];
-  const Arg& arg_verbose    = args[k++];
-  const Arg& arg_fill       = args[k++];
-  const Arg& arg_encoding   = args[k++];
-  const Arg& arg_skiptostr  = args[k++];
-  const Arg& arg_skiptoline = args[k++];
-  const Arg& arg_skipblanks = args[k++];
-  const Arg& arg_stripwhite = args[k++];
-  const Arg& arg_quotechar  = args[k++];
-  const Arg& arg_saveto     = args[k++];
-  const Arg& arg_tempdir    = args[k++];
-  const Arg& arg_nthreads   = args[k++];
-  const Arg& arg_logger     = args[k++];
+  const py::Arg& arg_columns    = args[k++];
+  const py::Arg& arg_sep        = args[k++];
+  const py::Arg& arg_dec        = args[k++];
+  const py::Arg& arg_maxnrows   = args[k++];
+  const py::Arg& arg_header     = args[k++];
+  const py::Arg& arg_nastrings  = args[k++];
+  const py::Arg& arg_verbose    = args[k++];
+  const py::Arg& arg_fill       = args[k++];
+  const py::Arg& arg_encoding   = args[k++];
+  const py::Arg& arg_skiptostr  = args[k++];
+  const py::Arg& arg_skiptoline = args[k++];
+  const py::Arg& arg_skipblanks = args[k++];
+  const py::Arg& arg_stripwhite = args[k++];
+  const py::Arg& arg_quotechar  = args[k++];
+  const py::Arg& arg_saveto     = args[k++];
+  const py::Arg& arg_tempdir    = args[k++];
+  const py::Arg& arg_nthreads   = args[k++];
+  const py::Arg& arg_logger     = args[k++];
 
   GenericReader rdr;
   rdr.init_logger(     arg_logger, arg_verbose);
@@ -104,8 +99,8 @@ static oobj fread(const PKArgs& args) {
   (void) arg_saveto;
   (void) arg_encoding;
 
-  auto read_sources = dt::read::resolve_sources(arg_anysource, arg_file, arg_text,
-                                                arg_cmd, arg_url, rdr);
+  auto read_sources = resolve_sources(arg_anysource, arg_file, arg_text,
+                                      arg_cmd, arg_url, rdr);
   if (read_sources.size() == 1) {
     return read_sources[0].read(rdr);
   }
@@ -122,8 +117,9 @@ static oobj fread(const PKArgs& args) {
 
 
 
-void DatatableModule::init_methods_csv() {
-  ADD_FN(&fread, args_fread);
-}
+}}  // namespace dt::read
 
-} // namespace py
+
+void py::DatatableModule::init_methods_csv() {
+  ADD_FN(&dt::read::fread, dt::read::args_fread);
+}
