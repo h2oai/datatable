2fdf6910bc529a601186c802551856b2dff0a4a6
Pasha Stetsenko <pasha@h2o.ai>
2020-05-07 13:25:13 -0700

    fix S_ISDIR macro on windows

--- src/core/utils/file.cc
+++ src/core/utils/file.cc
@@ -1,9 +1,23 @@
 //------------------------------------------------------------------------------
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Copyright 2018-2020 H2O.ai
 //
-// © H2O.ai 2018
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
 //------------------------------------------------------------------------------
 #include <errno.h>      // errno
 #include <fcntl.h>      // open, posix_fallocate[?]
@@ -143,10 +157,16 @@ void File::resize(size_t newsize) {
 
 void File::assert_is_not_dir() const {
   load_stats();
-  if (S_ISDIR(statbuf.st_mode)) {
+  #if DT_OS_WINDOWS
+    bool isdir = (statbuf.st_mode & S_IFMT) == S_IFDIR;
+  #else
+    bool isdir = S_ISDIR(statbuf.st_mode);
+  #endif
+  if (isdir) {
     throw IOError() << "File " << name << " is a directory";
   }
 }
+
 
 void File::load_stats() const {
   if (statbuf.st_size >= 0) return;
