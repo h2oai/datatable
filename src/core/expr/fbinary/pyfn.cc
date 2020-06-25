2e90a7f1305a722db68e70594c63900a6303e3a2
Pasha Stetsenko <pasha@h2o.ai>
2020-06-25 11:45:45 -0700

    Fix linking issues on macOS Catalina 10.15.5 (#2490)
    * Templated _sfr<> constants needed to be declared static.
    * Fix some more compiler warnings

--- src/core/expr/fbinary/pyfn.cc
+++ src/core/expr/fbinary/pyfn.cc
@@ -83,7 +83,7 @@ void py::DatatableModule::init_fbinary()
 {
   #define FBINARY(ARGS, OP) \
     ADD_FN(&dt::expr::fbinary_pyfn, dt::expr::ARGS); \
-    register_args(dt::expr::ARGS, dt::expr::OP);
+    register_args(dt::expr::ARGS, dt::expr::OP)
 
   FBINARY(args_atan2,      Op::ARCTAN2);
   FBINARY(args_hypot,      Op::HYPOT);
