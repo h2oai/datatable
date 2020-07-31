90fe50a598d486c1bcf7dcd7f6d45fb70d2571f3
Pasha Stetsenko <pasha@h2o.ai>
2020-07-30 18:38:47 -0700

    Add missing headers

--- src/core/utils/tests.cc
+++ src/core/utils/tests.cc
@@ -19,7 +19,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 //------------------------------------------------------------------------------
-#include <iostream>
+#include <cstring>             // std::strlen, std::strncmp
 #include <memory>              // std::unique_ptr
 #include <string>              // std::string
 #include <unordered_map>       // std::unordered_map
