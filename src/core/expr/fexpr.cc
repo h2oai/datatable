f3f6cba14d37dbd9e68fd03f8b3e31285425d216
Pasha Stetsenko <pasha@h2o.ai>
2020-08-14 16:16:43 -0700

    Add operator __mul__

--- src/core/expr/fexpr.cc
+++ src/core/expr/fexpr.cc
@@ -154,7 +154,7 @@ static oobj make_binexpr(dt::expr::Op op, robj lhs, robj rhs) {
 
 // oobj PyFExpr::nb__add__(robj lhs, robj rhs)      { return make_binexpr(dt::expr::Op::PLUS,     lhs, rhs); }
 // oobj PyFExpr::nb__sub__(robj lhs, robj rhs)      { return make_binexpr(dt::expr::Op::MINUS,    lhs, rhs); }
-oobj PyFExpr::nb__mul__(robj lhs, robj rhs)      { return make_binexpr(dt::expr::Op::MULTIPLY, lhs, rhs); }
+// oobj PyFExpr::nb__mul__(robj lhs, robj rhs)      { return make_binexpr(dt::expr::Op::MULTIPLY, lhs, rhs); }
 // oobj PyFExpr::nb__truediv__(robj lhs, robj rhs)  { return make_binexpr(dt::expr::Op::DIVIDE,   lhs, rhs); }
 oobj PyFExpr::nb__floordiv__(robj lhs, robj rhs) { return make_binexpr(dt::expr::Op::INTDIV,   lhs, rhs); }
 oobj PyFExpr::nb__mod__(robj lhs, robj rhs)      { return make_binexpr(dt::expr::Op::MODULO,   lhs, rhs); }
