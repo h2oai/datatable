e71900f15736b5e702375d6085171a826e463dde
Pasha Stetsenko <pasha@h2o.ai>
2020-08-14 15:36:55 -0700

    Started working on FExpr documentation

---
+++ docs/api/fexpr/__add__.rst
@@ -0,0 +1,5 @@
+.. py:currentmodule:: datatable
+
+.. xmethod:: datatable.FExpr.__add__
+    :src: src/core/expr/fbinary/fexpr__add__.cc evaluate1
+    :doc: src/core/expr/fbinary/fexpr__add__.cc doc__add__
--- src/core/expr/fbinary/fexpr__add__.cc
+++ src/core/expr/fbinary/fexpr__add__.cc
@@ -83,6 +83,39 @@ class FExpr_BinaryPlus : public FExpr_BinaryOp {
 
 
 
+#if 0
+static const char* doc__add__ =
+R"(__add__(x, y)
+--
+
+Add two FExprs together, which corresponds to python operator `+`.
+
+If `x` or `y` are multi-column expressions, then they must have the
+same number of columns, and the `+` operator will be applied to each
+corresponding pair of columns. If either `x` or `y` are single-column
+while the other is multi-column, then the single-column expression
+will be repeated to the same number of columns as its opponent.
+
+The result of adding two columns with different stypes will have the
+following stype:
+
+  - `max(x.stype, y.stype, int32)` if both columns are numeric (i.e.
+    bool, int or float);
+
+  - `str32`/`str64` if at least one of the columns is a string. In
+    this case the `+` operator implements string concatenation, same
+    as in Python.
+
+Parameters
+----------
+x, y: FExpr
+    The arguments must be either `FExpr`s, or expressions that can be
+    converted into `FExpr`s.
+
+return: FExpr
+    An expression that evaluates `x + y`.
+)";
+#endif
 
 py::oobj PyFExpr::nb__add__(py::robj lhs, py::robj rhs) {
   return PyFExpr::make(
