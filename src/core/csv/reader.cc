ea3357a5f8386f13d4a17411fac001e1b74cdee4
Pasha Stetsenko <pasha@h2o.ai>
2020-10-30 12:02:39 -0700

    remove const_cast

--- src/core/csv/reader.cc
+++ src/core/csv/reader.cc
@@ -922,7 +922,7 @@ void GenericReader::decode_utf16() {
   // `buf` is a borrowed ref, belongs to PyObject* `t`
   const char* buf = PyUnicode_AsUTF8AndSize(t, &ssize);
   input_mbuf = Buffer::unsafe(
-                  const_cast<void*>(static_cast<const void*>(buf)),
+                  static_cast<const void*>(buf),
                   static_cast<size_t>(ssize) + 1);
   sof = static_cast<char*>(input_mbuf.wptr());
   eof = sof + ssize + 1;
