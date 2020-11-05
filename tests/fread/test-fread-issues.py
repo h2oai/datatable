0d9376c32483530b61106e30b0858b789af870c5
Pradeep krishnamurthy <pradkrish84@gmail.com>
2020-11-05 11:44:53 +0100

    add tests

--- tests/fread/test-fread-issues.py
+++ tests/fread/test-fread-issues.py
@@ -542,6 +542,11 @@ def test_issue2523():
     with pytest.raises(IOError):
         dt.fread("{\n  \"cells\": [\n    {\n\"import numpy \\n\",\n")
 
+# Note: Realised after adding this test that it's similar to test_issue1036
+def test_issue2680():
+    src = '1\tWild Hogs (2007)\tAdevnture\n' * 500 + '2\t"Great Performances" Cats (1998)\tMusical\n' * 500
+    DT = dt.fread(src, fill=True)
+    assert DT.to_tuples()[900] == (2, '"Great Performances" Cats (1998)', 'Musical')
 
 def test_issue2681():
     src = 'A,B,C\n' + 'rr,dd",g\n' + '1,2,3\n'*99 + 'abc,def\r\n'
