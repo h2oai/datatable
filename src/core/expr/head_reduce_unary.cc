ac165156b7e48172761bf49377f294ebf84c2326
samukweku <samueloranyeli@gmail.com>
2020-11-19 19:12:13 +1100

    examples for last()

--- src/core/expr/head_reduce_unary.cc
+++ src/core/expr/head_reduce_unary.cc
@@ -153,6 +153,101 @@ See Also
 --------
 
 - :func:`first()` -- function that returns the first row.
+Examples
+--------
+
+:func:`last()` returns the last column in a frame::
+
+    from datatable import dt, f, by, sort, last
+
+    df = dt.Frame({"A": [1, 1, 2, 1, 2],
+                   "B": [None, 2, 3, 4, None]})
+
+    df
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 5, 2
+
+    0,1,NA
+    1,1,2
+    2,2,3
+    3,1,4
+    4,2,NA
+
+::
+
+    dt.last(df)
+
+.. dtframe::
+    :names: B
+    :types: int8
+    :shape: 5, 1
+
+    0,NA
+    1,2
+    2,3
+    3,4
+    4,NA
+
+Within a frame, it returns the last row::
+
+    df[:, last(f[:])]
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 1, 2
+
+    0,2,NA
+
+The above code can be replicated by passing -1 to the ``i`` section instead::
+
+    df[-1, :]
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 1, 2
+
+    0,2,NA
+
+Like :func:`first()`, :func:`last()` can be handy if you wish to get the last non null value in a column::
+
+   df[f.B != None, dt.last(f.B)]
+
+.. dtframe::
+    :names: B
+    :types: int8
+    :shape: 1,1
+
+    0,4
+
+:func:`last()` returns the last row per group in a :func:`by()` operation::
+
+    df[:, last(f[:]), by("A")]
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 2, 2
+
+    0,1,4
+    1,2,NA
+
+
+To get the last non-null value per row in a :func:`by()` operation, you can use the :func:`sort()` function, and set the ``na_position`` argument as ``first`` (this will move the ``NAs`` to the top of the column)::
+
+    df[:, last(f[:]), by("A"), sort("B", na_position="first")]
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 2, 2
+
+    0,1,4
+    1,2,3
 
 )";
 #endif
