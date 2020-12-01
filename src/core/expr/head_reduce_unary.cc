347a0fa837a0487e622d1deb488f029e8ddd254b
Samuel Oranyeli <samueloranyeli@gmail.com>
2020-12-02 07:47:28 +1100

    [DOC] first() examples (#2731)

--- src/core/expr/head_reduce_unary.cc
+++ src/core/expr/head_reduce_unary.cc
@@ -131,6 +131,97 @@ See Also
 
 - :func:`last()` -- function that returns the last row.
 
+Examples
+--------
+:func:`first()` returns the first column in a frame::
+
+    from datatable import dt, f, by, sort, first
+    df = dt.Frame({"A": [1, 1, 2, 1, 2], 
+                   "B": [None, 2, 3, 4, 5]})
+    df 
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 5, 2
+
+    0,1,NA
+    1,1,2
+    2,2,3
+    3,1,4
+    4,2,5
+
+::
+
+    dt.first(df)
+
+.. dtframe::
+    :names: A
+    :types: int8
+    :shape: 5, 1
+
+    0,1
+    1,1
+    2,2
+    3,1
+    4,2
+
+Within a frame, it returns the first row::
+
+    df[:, first(f[:])]
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 1, 2
+
+    0,1,NA
+
+Of course, you can replicate this by passing 0 to the ``i`` section instead::
+
+    df[0, :]
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 1, 2
+
+    0,1,NA
+
+:func:`first()` comes in handy if you wish to get the first non null value in a column::
+
+    df[f.B != None, first(f.B)]
+
+.. dtframe::
+    :names: B
+    :types: int8
+    :shape: 1,1
+
+    0,2
+
+:func:`first()` returns the first row per group in a :func:`by()` operation::
+
+    df[:, first(f[:]), by("A")]
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 2, 2
+
+    0,1,NA
+    1,2,3
+
+To get the first non-null value per row in a :func:`by()` operation, you can use the :func:`sort()` function, and set the ``na_position`` argument as ``last``::
+
+    df[:, first(f[:]), by("A"), sort("B", na_position="last")]
+
+.. dtframe::
+    :names: A,B
+    :types: int8, int8
+    :shape: 2, 2
+
+    0,1,2
+    1,2,3
 )";
 
 
@@ -153,6 +244,7 @@ See Also
 --------
 
 - :func:`first()` -- function that returns the first row.
+
 Examples
 --------
 
