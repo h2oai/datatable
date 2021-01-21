986eeae5aa3467e73d5966c18554172017f295f1
samukweku <samueloranyeli@gmail.com>
2021-01-22 08:26:22 +1100

    [DOC] Examples for dt.rbind()

--- src/core/frame/rbind.cc
+++ src/core/frame/rbind.cc
@@ -267,6 +267,71 @@ by_names: bool
 return: Frame
 
 
+Examples
+--------
+.. code-block:: python
+
+    >>> from datatable import dt
+    >>>
+    >>> DT1 = dt.Frame({"Weight": [5, 4, 6], "Height": [170, 172, 180]})
+    >>> DT1
+       | Weight  Height
+       |  int32   int32
+    -- + ------  ------
+     0 |      5     170
+     1 |      4     172
+     2 |      6     180
+    [3 rows x 2 columns]
+
+    >>> DT2 = df2 = dt.Frame({"Weight": [4, 4, 5], "Height": [180, 181, 169]})
+    >>> DT2
+       | Weight  Height
+       |  int32   int32
+    -- + ------  ------
+     0 |      4     180
+     1 |      4     181
+     2 |      5     169
+    [3 rows x 2 columns]
+
+    >>> dt.rbind([DT1, DT2])
+       | Weight  Height
+       |  int32   int32
+    -- + ------  ------
+     0 |      5     170
+     1 |      4     172
+     2 |      6     180
+     3 |      4     180
+     4 |      4     181
+     5 |      5     169
+    [6 rows x 2 columns]
+
+
+If the number of columns are not equal, you can force the row binding by setting
+the `force` parameter to `True`::
+
+    >>> DT2["Age"] = dt.Frame([25, 50, 67])
+    >>> DT2
+       | Weight  Height    Age
+       |  int32   int32  int32
+    -- + ------  ------  -----
+     0 |      4     180     25
+     1 |      4     181     50
+     2 |      5     169     67
+    [3 rows x 3 columns]
+
+    >>> dt.rbind([DT1, DT2], force=True)
+       | Weight  Height    Age
+       |  int32   int32  int32
+    -- + ------  ------  -----
+     0 |      5     170     NA
+     1 |      4     172     NA
+     2 |      6     180     NA
+     3 |      4     180     25
+     4 |      4     181     50
+     5 |      5     169     67
+    [6 rows x 3 columns]
+
+
 See also
 --------
 - :func:`cbind()` -- function for col-binding several frames.
