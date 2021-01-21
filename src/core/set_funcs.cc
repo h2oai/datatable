012a730f55118c40a50eaf60bad0ebeca51f4a00
samukweku <samueloranyeli@gmail.com>
2021-01-22 08:00:05 +1100

    [DOC] Examples for intersect()

--- src/core/set_funcs.cc
+++ src/core/set_funcs.cc
@@ -460,6 +460,51 @@ except: ValueError | NotImplementedError
         * - :exc:`dt.exceptions.NotImplementedError`
           - raised when one of the columns has stype `obj64`.
 
+
+Examples
+--------
+.. code-block:: python
+
+    >>> from datatable import dt, f
+    >>>
+    >>> s1 = dt.Frame([4, 5, 6, 20, 42])
+    >>> s2 = dt.Frame([1, 2, 3, 5, 42])
+    >>>
+    >>> s1
+       |    C0
+       | int32
+    -- + -----
+     0 |     4
+     1 |     5
+     2 |     6
+     3 |    20
+     4 |    42
+    [5 rows x 1 column]
+
+    >>> s2
+       |    C0
+       | int32
+    -- + -----
+     0 |     1
+     1 |     2
+     2 |     3
+     3 |     5
+     4 |    42
+    [5 rows x 1 column]
+
+
+Intersection of the two frames::
+
+    >>> dt.intersect([s1, s2])
+       |    C0
+       | int32
+    -- + -----
+     0 |     5
+     1 |    42
+    [2 rows x 1 column]
+
+
+
 See Also
 --------
 - :func:`setdiff()` -- calculate the set difference between the frames.
