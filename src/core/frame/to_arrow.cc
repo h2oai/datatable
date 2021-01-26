41b832d3a1f96769bcff892267cdcb94f231ff79
Pasha Stetsenko <pasha@h2o.ai>
2021-01-25 19:21:15 -0800

    Add documentation for .to_arrow()

--- src/core/frame/to_arrow.cc
+++ src/core/frame/to_arrow.cc
@@ -36,11 +36,19 @@ static const char* doc_to_arrow =
 R"(to_arrow(self)
 --
 
-Convert this frame into an arrow ``Table``.
+Convert this frame into a ``pyarrow.Table`` object. The ``pyarrow``
+module must be installed.
+
+The conversion is multi-threaded and done in C++, but it does
+involve creating a copy of the data, except for the cases when the
+data was originally imported from Arrow. This is caused by differences
+in the data storage formats of datatable and Arrow.
 
 Parameters
 ----------
 return: pyarrow.Table
+    A ``Table`` object is always returned, even if the source is a
+    single-column datatable Frame.
 
 except: ImportError
     If the `pyarrow` module is not installed.
