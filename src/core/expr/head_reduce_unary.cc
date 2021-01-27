f01493cbacc8dfdc64f6eabf043459afef0a28ea
Samuel Oranyeli <samueloranyeli@gmail.com>
2021-01-28 06:14:53 +1100

    Examples for sd() (#2836)

--- src/core/expr/head_reduce_unary.cc
+++ src/core/expr/head_reduce_unary.cc
@@ -746,6 +746,46 @@ except: TypeError
     The exception is raised when one of the columns from `cols`
     has a non-numeric type.
 
+
+Examples
+--------
+
+.. code-block:: python
+
+    >>> from datatable import dt, f
+    >>>
+    >>> DT = dt.Frame(A = [0, 1, 2, 3], B = [0, 2, 4, 6])
+    >>> DT
+       |     A      B
+       | int32  int32
+    -- + -----  -----
+     0 |     0      0
+     1 |     1      2
+     2 |     2      4
+     3 |     3      6
+    [4 rows x 2 columns]
+
+
+Get the standard deviation of column A::
+
+    >>> DT[:, dt.sd(f.A)]
+       |       A
+       | float64
+    -- + -------
+     0 | 1.29099
+    [1 row x 1 column]
+
+
+Get the standard deviation of columns A and B::
+
+    >>> DT[:, dt.sd([f.A, f.B])]
+       |       A        B
+       | float64  float64
+    -- + -------  -------
+     0 | 1.29099  2.58199
+    [1 row x 2 columns]
+
+
 See Also
 --------
 
