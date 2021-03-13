3a5b5df1aab68efd838abc017ee9244a2f14906d
Pasha Stetsenko <pasha@h2o.ai>
2021-03-12 18:54:28 -0800

    Cast date32 into string

--- src/core/column/cast_date32.cc
+++ src/core/column/cast_date32.cc
@@ -21,6 +21,7 @@
 //------------------------------------------------------------------------------
 #include <cmath>             // std::ismath
 #include <cstdlib>           // std::strtod
+#include "csv/toa.h"
 #include "column/cast.h"
 #include "python/string.h"
 #include "read/constants.h"  // dt::read::pow10lookup
@@ -76,6 +77,23 @@ bool CastDate32_ColumnImpl::get_element(size_t i, double* out) const {
   return _get<double>(i, out);
 }
 
+
+bool CastDate32_ColumnImpl::get_element(size_t i, CString* out) const {
+  int32_t x;
+  bool isvalid = arg_.get_element(i, &x);
+  if (isvalid) {
+    out->prepare_buffer(14);
+    char* ch0 = ch;
+    date32_toa(&ch, x);
+    out->set_size(static_cast<size_t>(ch - ch0));
+  }
+  return isvalid;
+}
+
+
+// bool get_element(size_t, py::oobj*) const {
+//
+// }
 
 
 
