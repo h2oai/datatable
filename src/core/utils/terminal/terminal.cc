9e5be519068f1ca13ebb7956ee7028a6fe99650f
Pasha Stetsenko <pasha@h2o.ai>
2021-05-10 12:44:10 -0700

    Treat google.colab shell as a Jupyter notebook

--- src/core/utils/terminal/terminal.cc
+++ src/core/utils/terminal/terminal.cc
@@ -150,7 +150,8 @@ void Terminal::_check_ipython() {
   if (ipython) {
     auto ipy = ipython.invoke("get_ipython");
     std::string ipy_type = ipy.typestr();
-    if (ipy_type.find("ZMQInteractiveShell") != std::string::npos) {
+    if (ipy_type.find("ZMQInteractiveShell") != std::string::npos ||
+        ipy_type.find("google.colab") != std::string::npos) {
       display_allow_unicode = true;
       is_jupyter_ = true;
     }
