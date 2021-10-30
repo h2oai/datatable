h2oai/datatable

commit 955baffe49c775727712e4ad0ac76852bc1be2b5

Fix a crash happening in `.to_csv()` due to multithreading (#3192)
- fix crash that was caused by a mismatch in calculation of "nrows per chunk" in `write_manager::determine_chunking_strategy()` and `write_manager::write_rows()`;
- minor improvements to test.

Closes https://github.com/h2oai/datatable/issues/3176

src/core/write/write_manager.cc
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// Copyright 2018-2020 H2O.ai
+// Copyright 2018-2021 H2O.ai
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -29,7 +29,7 @@
 
 namespace dt {
 namespace write {
-  
+
 
 //------------------------------------------------------------------------------
 // Construction
@@ -232,7 +232,7 @@ void write_manager::determine_chunking_strategy()
 
   int attempts = 5;
   while (attempts--) {
-    double rows_per_chunk = static_cast<double>(nrows + 1) / static_cast<double>(nchunks);
+    double rows_per_chunk = static_cast<double>(nrows) / static_cast<double>(nchunks);
     auto bytes_per_chunk = static_cast<size_t>(bytes_per_row * rows_per_chunk);
     if (rows_per_chunk < 1.0) {
       // If each row's size is too large, then parse 1 row at a time.

tests/frame/test-tocsv.py
@@ -236,7 +236,7 @@ def test_save_hexdouble_special():
 def test_save_hexdouble_random(seed):
     random.seed(seed)
     src = [(1.5 * random.random() - 0.5) * 10**random.randint(-325, 308)
-           for _ in range(1000)]
+           for _ in range(random.randint(1, 1000))]
     d = dt.Frame(src)
     hexxed = d.to_csv(hex=True).split("\n")[1:-1]
     assert hexxed == [pyhex(v) for v in src]
@@ -437,6 +437,15 @@ def test_issue2382():
                    '\n'.join(','.join(['"1"'] * 200) for j in range(20)) + "\n")
 
 
+def test_issue3176():
+    import multiprocessing as mp
+    dt.options.nthreads = min(8, mp.cpu_count())
+    nrows = dt.options.nthreads * 2 - 1
+    ncols = 41
+    src = [0.0] * nrows
+    DT0 = dt.Frame([src] * ncols)
+    DT1 = dt.Frame(DT0.to_csv())
+    assert_equals(DT0, DT1)
 
 
 #-------------------------------------------------------------------------------

commit 91e11153f6f293c5cecc0b4933b8bc70dc93931c

Fix some issues caused by #3192 (#3193)
`write_manager::determine_chunking_strategy()` is used in several other places and the recent changes in #3192 could cause some issues there. Try to fix #3176 in `write_manager::write_rows()` only.

src/core/write/write_manager.cc
@@ -139,7 +139,7 @@ void write_manager::write_rows()
     public:
       OTask(size_t nrows, size_t nch, size_t rowsize,
             write_manager* wm, WritableBuffer* wbuf, bool compress)
-        : ctx_(rowsize, nrows / nch, compress),
+        : ctx_(rowsize, std::max<size_t>(nrows / nch, 1), compress),
           wb_(wbuf),
           wmanager_(wm),
           nrows_(nrows),
@@ -232,7 +232,7 @@ void write_manager::determine_chunking_strategy()
 
   int attempts = 5;
   while (attempts--) {
-    double rows_per_chunk = static_cast<double>(nrows) / static_cast<double>(nchunks);
+    double rows_per_chunk = static_cast<double>(nrows + 1) / static_cast<double>(nchunks);
     auto bytes_per_chunk = static_cast<size_t>(bytes_per_row * rows_per_chunk);
     if (rows_per_chunk < 1.0) {
       // If each row's size is too large, then parse 1 row at a time.