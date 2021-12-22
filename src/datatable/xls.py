f5d6c9ce0f208903ff71e84866d0de1699ba9b24
Oleksiy Kononenko <oleksiy.s.kononenko@gmail.com>
2021-12-21 23:03:36 -0800

    Allow both slash and backslash on Windows for the xls subpath

--- src/datatable/xls.py
+++ src/datatable/xls.py
@@ -39,8 +39,9 @@ def read_xls_workbook(filename, subpath):
         if subpath in wb.sheet_names():
             sheetname = subpath
         else:
-            if "/" in subpath:
-                sheetname, xlsrange = subpath.rsplit('/', 1)
+            sep = "/" if "/" in subpath else "\\" if "\\" in subpath else None
+            if sep is not None:
+                sheetname, xlsrange = subpath.rsplit(sep, 1)
                 range2d = _excel_coords_to_range2d(xlsrange)
             if not(sheetname in wb.sheet_names() and range2d is not None):
                 raise ValueError("Sheet `%s` is not found in the XLS file"
