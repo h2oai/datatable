943d8272da58eca4bf693597933ff6e7c4d33986
sammychoco <samuel.oranyeli@slalom.com>
2022-07-26 21:28:52 +1000

    baby steps

--- src/core/expr/fexpr_fillna.cc
+++ src/core/expr/fexpr_fillna.cc
@@ -28,6 +28,7 @@
 #include "expr/workframe.h"
 #include "python/xargs.h"
 #include "stype.h"
+#include <iostream>
 
 
 namespace dt {
@@ -36,31 +37,20 @@ namespace dt {
     class FExpr_FillNA : public FExpr_Func {
       private:
         ptrExpr arg_;
-        std::string direction_;
+        bool reverse_;
 
       public:
-        FExpr_FillNA(ptrExpr &&arg, py::oobj direction)
-            : arg_(std::move(arg))
-             {if (direction.is_string()) {
-              direction_ = direction.to_string();
-             }
-             else {
-                throw TypeError() << "Parameter `direction` in fillna() should be "
-                    "a string, instead got " << direction.typeobj();
-             }
-             bool bool_direction = (direction_=="down") || (direction_=="up");
-             if (!bool_direction) {
-              throw ValueError() << "The value for the parameter `direction` in fillna() "
-                  "should be either `up` or `down`.";
-             }
-             }
+        FExpr_FillNA(ptrExpr &&arg, bool reverse)
+            : arg_(std::move(arg)),
+              reverse_(reverse)
+             {}
 
         std::string repr() const override {
           std::string out = "fillna";
           out += '(';
           out += arg_->repr();
-          out += ", direction=";
-          out += direction_;
+          out += ", reverse=";
+          out += reverse_? "True" : "False";
           out += ')';
           return out;
         }
@@ -68,68 +58,76 @@ namespace dt {
 
         Workframe evaluate_n(EvalContext &ctx) const override {
           Workframe wf = arg_->evaluate_n(ctx);
-          Groupby gby = Groupby::single_group(wf.nrows());
+          // Groupby gby = Groupby::single_group(wf.nrows());
 
-          if (ctx.has_groupby()) {
-            wf.increase_grouping_mode(Grouping::GtoALL);
-            gby = ctx.get_groupby();
-          }
+          // if (ctx.has_groupby()) {
+          //   wf.increase_grouping_mode(Grouping::GtoALL);
+          //   gby = ctx.get_groupby();
+          // }
 
-          bool bool_direction = direction_=="down";
-          for (size_t i = 0; i < wf.ncols(); ++i) {
-            Column coli = evaluate1(wf.retrieve_column(i), bool_direction, gby);
-            wf.replace_column(i, std::move(coli));
-          }
+          // bool bool_direction = direction_=="down";
+          // bool is_valid;
+          // int64_t res;
+          // for (size_t i = 0; i < wf.ncols(); ++i) {
+          //   Column coli = wf.retrieve_column(i);
+          //   is_valid = coli.get_element(4, &res);
+          //   std::cout<<is_valid<<std::endl;
+          //   std::cout<<res<<std::endl;
+          //   //Column coli = evaluate1(wf.retrieve_column(i), bool_direction, gby);
+          //   //wf.replace_column(i, std::move(coli));
+          // }
           return wf;
         }
 
 
-      Column evaluate1(Column &&col, bool bool_direction, const Groupby &gby) const {
-        SType stype = col.stype();
-        switch (stype) {
-          case SType::VOID:    return Column(new ConstNa_ColumnImpl(col.nrows()));
-          case SType::BOOL:
-          case SType::INT8:    return make<int8_t>(std::move(col), bool_direction, gby);
-          case SType::INT16:   return make<int16_t>(std::move(col), bool_direction, gby);
-          case SType::INT32:   return make<int32_t>(std::move(col), bool_direction, gby);
-          case SType::INT64:   return make<int64_t>(std::move(col), bool_direction, gby);
-          case SType::FLOAT32: return make<float>(std::move(col), bool_direction, gby);
-          case SType::FLOAT64: return make<double>(std::move(col), bool_direction, gby);
-          //case SType::STR32:   
-          //case SType::STR64: return make<CString>(std::move(col), bool_direction, gby);  
-          //case SType::DATE32:
-          //case SType::DATE64:
-          //case SType::TIME64:
-          default: throw TypeError()
-            << "Invalid column of type `" << stype << "` in " << repr();
-        }
-      }
+      // Column evaluate1(Column &&col, bool bool_direction, const Groupby &gby) const {
+      //   SType stype = col.stype();
+      //   switch (stype) {
+      //     case SType::VOID:    return Column(new ConstNa_ColumnImpl(col.nrows()));
+      //     case SType::BOOL:
+      //     case SType::INT8:    return make<int8_t>(std::move(col), bool_direction, gby);
+      //     case SType::INT16:   return make<int16_t>(std::move(col), bool_direction, gby);
+      //     case SType::INT32:   return make<int32_t>(std::move(col), bool_direction, gby);
+      //     case SType::INT64:   return make<int64_t>(std::move(col), bool_direction, gby);
+      //     case SType::FLOAT32: return make<float>(std::move(col), bool_direction, gby);
+      //     case SType::FLOAT64: return make<double>(std::move(col), bool_direction, gby);
+      //     //case SType::STR32:   
+      //     //case SType::STR64: return make<CString>(std::move(col), bool_direction, gby);  
+      //     //case SType::DATE32:
+      //     //case SType::DATE64:
+      //     //case SType::TIME64:
+      //     default: throw TypeError()
+      //       << "Invalid column of type `" << stype << "` in " << repr();
+      //   }
+      // }
 
 
-        template <typename T>
-        Column make(Column &&col, bool bool_direction, const Groupby &gby) const {
-           return Column(new Latent_ColumnImpl(
-             new FillNA_ColumnImpl<T>(std::move(col), bool_direction, gby)
-           ));
-        }
+      //   template <typename T>
+      //   Column make(Column &&col, bool bool_direction, const Groupby &gby) const {
+      //      return Column(new Latent_ColumnImpl(
+      //        new FillNA_ColumnImpl<T>(std::move(col), bool_direction, gby)
+      //      ));
+      //   }
     };
 
 
 
     static py::oobj pyfn_fillna(const py::XArgs &args) {
-      auto fillna = args[0].to_oobj();
-      auto direction = args[1].to_oobj();
-      return PyFExpr::make(new FExpr_FillNA(as_fexpr(fillna), direction));
+      auto arg0 = args[0].to_oobj();
+      auto reverse = args[1].to<bool>(false);
+
+
+      return PyFExpr::make(new FExpr_FillNA(as_fexpr(arg0), reverse));
     }
 
 
     DECLARE_PYFN(&pyfn_fillna)
         ->name("fillna")
         //->docs(doc_dt_fillna)
-        ->arg_names({"column", "direction"})
-        ->n_required_args(2)
+        ->arg_names({"column", "reverse"})
+        ->n_required_args(1)
         ->n_positional_args(1)
-        ->n_positional_or_keyword_args(1);
+        ->n_keyword_args(1);
 
   }  // namespace dt::expr
 }    // namespace dt
