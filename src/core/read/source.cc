e2e35bcdd0a7f5269a8a8bdd84ca0787a1cd377b
Oleksiy Kononenko <oleksiy.s.kononenko@gmail.com>
2022-08-02 00:37:49 -0700

    Cosmetics

--- src/core/read/source.cc
+++ src/core/read/source.cc
@@ -216,7 +216,7 @@ Source_Url::Source_Url(const std::string& url) : Source(url) {
 py::oobj Source_Url::read(GenericReader& reader) {
   reader.source_name = &name_;
   TemporaryFile tmpfile;
-{
+  {
     dt::progress::work job(1);
     job.set_message("Downloading " + url_);
     // Characters [0-9a-zA-Z._~-] are always considered "safe".
