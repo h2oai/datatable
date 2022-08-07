d9945132e5e434fcc70f1e0d27859632909be19f
Oleksiy Kononenko <oleksiy.s.kononenko@gmail.com>
2022-08-07 14:49:36 -0700

    Adjust years

--- src/core/tests/test_parallel_for.cc
+++ src/core/tests/test_parallel_for.cc
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// Copyright 2019-2020 H2O.ai
+// Copyright 2019-2022 H2O.ai
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
