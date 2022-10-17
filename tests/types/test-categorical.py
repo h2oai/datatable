9a9062e8cdae69dea940f2169dcb1903147e204e
Oleksiy Kononenko <oleksiy.s.kononenko@gmail.com>
2022-10-17 14:12:30 -0700

    Add newlines

--- tests/types/test-categorical.py
+++ tests/types/test-categorical.py
@@ -635,3 +635,5 @@ def test_codes_multicolumn(cat_type, code_type):
     DT_ref = dt.Frame([[0, 1, 2, 0, 1, 3, 1, 2] * N,
                        [0, 2, 3, 0, 2, 1, 2, 4] * N], type=code_type)
     assert_equals(DT_codes, DT_ref)
+
+
