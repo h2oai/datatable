26692fd3f322cee29693a37564c8631287912732
Oleksiy Kononenko <oleksiy.s.kononenko@gmail.com>
2022-11-29 12:26:08 -0800

    Add py311 for x64 on Jenkins

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 //------------------------------------------------------------------------------
-// Copyright 2018-2020 H2O.ai
+// Copyright 2018-2022 H2O.ai
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -256,12 +256,14 @@ ansiColor('xterm') {
                                             /opt/python/cp38-cp38/bin/python3.8 ci/ext.py wheel --audit && \
                                             /opt/python/cp39-cp39/bin/python3.9 ci/ext.py wheel --audit && \
                                             /opt/python/cp310-cp310/bin/python3.10 ci/ext.py wheel --audit && \
+                                            /opt/python/cp311-cp311/bin/python3.11 ci/ext.py wheel --audit && \
                                             echo '===== Py3.8 Debug =====' && unzip -p dist/*debug*.whl datatable/_build_info.py && \
                                             mv dist/*debug*.whl . && \
                                             echo '===== Py3.7 =====' && unzip -p dist/*cp37*.whl datatable/_build_info.py && \
                                             echo '===== Py3.8 =====' && unzip -p dist/*cp38*.whl datatable/_build_info.py && \
                                             echo '===== Py3.9 =====' && unzip -p dist/*cp39*.whl datatable/_build_info.py && \
                                             echo '===== Py3.10 =====' && unzip -p dist/*cp310*.whl datatable/_build_info.py && \
+                                            echo '===== Py3.11 =====' && unzip -p dist/*cp311*.whl datatable/_build_info.py && \
                                             mv *debug*.whl dist/ && \
                                             ls -la dist"
                                 """
@@ -430,6 +432,20 @@ ansiColor('xterm') {
                                     unstash 'datatable-sources'
                                     unstash 'x86_64-manylinux-wheels'
                                     test_in_docker("x86_64-manylinux-py310", "310",
+                                                   DOCKER_IMAGE_X86_64_MANYLINUX)
+                                }
+                            }
+                        }
+                    }) <<
+                    namedStage('Test x86_64-manylinux-py311', doPy38Tests, { stageName, stageDir ->
+                        node(NODE_LINUX) {
+                            buildSummary.stageWithSummary(stageName, stageDir) {
+                                cleanWs()
+                                dumpInfo()
+                                dir(stageDir) {
+                                    unstash 'datatable-sources'
+                                    unstash 'x86_64-manylinux-wheels'
+                                    test_in_docker("x86_64-manylinux-py311", "311",
                                                    DOCKER_IMAGE_X86_64_MANYLINUX)
                                 }
                             }
@@ -792,6 +808,7 @@ def get_python_for_docker(String pyver, String image) {
         if (pyver == "38") return "/opt/python/cp38-cp38/bin/python3.8"
         if (pyver == "39") return "/opt/python/cp39-cp39/bin/python3.9"
         if (pyver == "310") return "/opt/python/cp310-cp310/bin/python3.10"
+        if (pyver == "311") return "/opt/python/cp311-cp311/bin/python3.11"
     }
     throw new Exception("Unknown python ${pyver} for docker ${image}")
 }
--- src/core/python/obj.cc
+++ src/core/python/obj.cc
@@ -1292,19 +1292,19 @@ robj rnone()    { return robj(Py_None); }
 
 robj rstdin() {
   return robj(
-    PySys_GetObject("stdin")         // borrowed ref
+    PySys_GetObject("stdin")  // borrowed ref
   );
 }
 
 robj rstdout() {
   return robj(
-    PySys_GetObject("stdout")         // borrowed ref
+    PySys_GetObject("stdout")  // borrowed ref
   );
 }
 
 robj rstderr() {
   return robj(
-    PySys_GetObject("stderr")         // borrowed ref
+    PySys_GetObject("stderr")  // borrowed ref
   );
 }
 
@@ -1314,7 +1314,7 @@ void write_to_stdout(const std::string& str) {
   oobj writer;
   if (py_stdout && py_stdout != Py_None) {
     writer = oobj::from_new_reference(
-      PyObject_GetAttrString(py_stdout, "write")   // new ref
+      PyObject_GetAttrString(py_stdout, "write")  // new ref
     );
     if (!writer) PyErr_Clear();
   }
