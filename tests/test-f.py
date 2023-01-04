691f0187bf5a4cb07c5ce9e1370957b68d524cfb
samukweku <samueloranyeli@gmail.com>
2023-01-04 11:09:15 +1100

    fix test failures for test-f.py

--- tests/test-f.py
+++ tests/test-f.py
@@ -501,11 +501,11 @@ def test_codes():
 
 
 def test_nth():
-    assert str(dt.nth(f.A, nth=0)) == str(f.A.nth(nth=0, skipna=False))
-    assert str(dt.nth(f.A, nth=1, skipna=True)) == str(f.A.nth(nth=1, skipna=True))
+    assert str(dt.nth(f.A, n=0)) == str(f.A.nth(n=0, skipna=False))
+    assert str(dt.nth(f.A, n=1, skipna=True)) == str(f.A.nth(n=1, skipna=True))
     assert str(dt.nth(f[:], -1, skipna=False)) == str(f[:].nth(-1, False))
     DT = dt.Frame(A = [9, 8, 2, 3, None, None, 3, 0, 5, 5, 8, None, 1])
-    assert_equals(DT[:, f.A.nth(nth=1, skipna=False)], DT[:, dt.nth(f.A, 1, False)])
-    assert_equals(DT[:, f.A.nth(nth=0, skipna=True)], DT[:, dt.nth(f.A, 0, True)])
-    assert_equals(DT[:, f.A.nth(nth=0, skipna=True)], DT[:, dt.nth(f.A, 0, skipna=True)])
+    assert_equals(DT[:, f.A.nth(n=1, skipna=False)], DT[:, dt.nth(f.A, 1, False)])
+    assert_equals(DT[:, f.A.nth(n=0, skipna=True)], DT[:, dt.nth(f.A, 0, True)])
+    assert_equals(DT[:, f.A.nth(n=0, skipna=True)], DT[:, dt.nth(f.A, 0, skipna=True)])
     
