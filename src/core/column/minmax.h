9c667cfe1512435e9529df123421f779ab89e04b
Oleksiy Kononenko <oleksiy.s.kononenko@gmail.com>
2023-03-02 15:24:37 -0800

    Cosmetics

--- src/core/column/minmax.h
+++ src/core/column/minmax.h
@@ -37,38 +37,36 @@ class MinMax_ColumnImpl : public ReduceUnary_ColumnImpl<T, IS_GROUPED> {
         bool isvalid = this->col_.get_element(i, &value);
         *out = value;
         return isvalid;
-      } else {
-        // res` will be updated on the first valid element,
-        // due to `res_isna` initially being set to `true`.
-        // So the default value here
-        // only silences the compiler warning and makes the update
-        // to happen a little bit faster, but it has no effect
-        // on the final result.
-        T res = MIN ? std::numeric_limits<T>::max()
-                    : std::numeric_limits<T>::min();
-        bool res_isna = true;
-        size_t i0, i1;
-        this->gby_.get_group(i, &i0, &i1);
+      }
 
-        for (size_t gi = i0; gi < i1; ++gi) {
-          T value;
-          bool isvalid = this->col_.get_element(gi, &value);
-          if (MIN) {
-            if (isvalid && (value < res || res_isna)) {
-              res = value;
-              res_isna = false;
-            }
-          } else {
-            if (isvalid && (value > res || res_isna)) {
-              res = value;
-              res_isna = false;
-            }
+      // res` will be updated on the first valid element, due to `res_isna`
+      // initially being set to `true`. So the default value here
+      // only silences the compiler warning and makes the update
+      // to happen a little bit faster, but it has no effect on the final result.
+      T res = MIN ? std::numeric_limits<T>::max()
+                  : std::numeric_limits<T>::min();
+      bool res_isna = true;
+      size_t i0, i1;
+      this->gby_.get_group(i, &i0, &i1);
+
+      for (size_t gi = i0; gi < i1; ++gi) {
+        T value;
+        bool isvalid = this->col_.get_element(gi, &value);
+        if (MIN) {
+          if (isvalid && (value < res || res_isna)) {
+            res = value;
+            res_isna = false;
+          }
+        } else {
+          if (isvalid && (value > res || res_isna)) {
+            res = value;
+            res_isna = false;
           }
         }
-
-        *out = static_cast<T>(res);
-        return !res_isna;
       }
+
+      *out = static_cast<T>(res);
+      return !res_isna;
     }
 };
 
