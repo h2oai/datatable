9547946f028d5dd2d16c5fdb7b4aa9459d333fe3
Samuel Oranyeli <samueloranyeli@gmail.com>
2023-03-07 04:46:45 +1100

    Refactor `min()` and `max()` reducers to use `FExpr` (#3401)
    WIP for #2562

---
+++ src/core/column/minmax.h
@@ -0,0 +1,76 @@
+//------------------------------------------------------------------------------
+// Copyright 2023 H2O.ai
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//------------------------------------------------------------------------------
+#ifndef dt_COLUMN_MINMAX_h
+#define dt_COLUMN_MINMAX_h
+#include "column/reduce_unary.h"
+#include "stype.h"
+namespace dt {
+
+
+template <typename T, bool MIN, bool IS_GROUPED>
+class MinMax_ColumnImpl : public ReduceUnary_ColumnImpl<T, IS_GROUPED> {
+  public:
+    using ReduceUnary_ColumnImpl<T, IS_GROUPED>::ReduceUnary_ColumnImpl;
+
+    bool get_element(size_t i, T* out) const override {      
+      if (IS_GROUPED) {
+        T value;
+        bool isvalid = this->col_.get_element(i, &value);
+        *out = value;
+        return isvalid;
+      }
+
+      // res` will be updated on the first valid element, due to `res_isna`
+      // initially being set to `true`. So the default value here
+      // only silences the compiler warning and makes the update
+      // to happen a little bit faster, but it has no effect on the final result.
+      T res = MIN ? std::numeric_limits<T>::max()
+                  : std::numeric_limits<T>::min();
+      bool res_isna = true;
+      size_t i0, i1;
+      this->gby_.get_group(i, &i0, &i1);
+
+      for (size_t gi = i0; gi < i1; ++gi) {
+        T value;
+        bool isvalid = this->col_.get_element(gi, &value);
+        if (MIN) {
+          if (isvalid && (value < res || res_isna)) {
+            res = value;
+            res_isna = false;
+          }
+        } else {
+          if (isvalid && (value > res || res_isna)) {
+            res = value;
+            res_isna = false;
+          }
+        }
+      }
+
+      *out = static_cast<T>(res);
+      return !res_isna;
+    }
+};
+
+
+}  // namespace dt
+
+#endif
--- tests/test-reduce.py
+++ tests/test-reduce.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
-# Copyright 2018-2022 H2O.ai
+# Copyright 2018-2023 H2O.ai
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -292,6 +292,14 @@ def test_minmax_integer(mm, st):
 
 
 @pytest.mark.parametrize("mm", [dt.min, dt.max])
+@pytest.mark.parametrize("st", dt.ltype.int.stypes)
+def test_minmax_integer_grouped(mm, st):
+    src = [3, 2, 2, 2, 2, 3, -100, 15, -100]
+    DT = dt.Frame(A=src, stype=st)
+    assert DT[:, mm(f.A), by(f.A)].to_list() == [[-100, 2, 3, 15]]*2
+
+
+@pytest.mark.parametrize("mm", [dt.min, dt.max])
 def test_minmax_real(mm):
     src = [5.6, 12.99, 1e+12, -3.4e-22, math.nan, 0.0]
     DT = dt.Frame(A=src)
@@ -304,6 +312,13 @@ def test_minmax_infs(mm):
     answer = -math.inf if mm == dt.min else +math.inf
     DT = dt.Frame(A=src)
     assert DT[:, mm(f.A)].to_list() == [[answer]]
+
+
+@pytest.mark.parametrize("mm", [dt.min, dt.max])
+@pytest.mark.parametrize("src", [[math.inf], [-math.inf]])
+def test_minmax_infs_only(mm, src):
+    DT = dt.Frame(A=src)
+    assert DT[:, mm(f.A)].to_list() == [src]
 
 
 @pytest.mark.parametrize("mm", [dt.min, dt.max])
@@ -325,6 +340,43 @@ def test_minmax_comprehension(mm, res):
     # See issue #3409
     s = mm([i for i in range(10)])
     assert s == res
+
+
+@pytest.mark.parametrize("mm, res", [(dt.min, 0), (dt.max, 9)])
+def test_minmax_generator_comprehension(mm, res):
+    # See issue #3409
+    s = mm(i for i in range(10))
+    assert s == res
+
+
+def test_min_multicolumn():
+    # See issue #3406
+    DT = dt.Frame({'C0':range(5), 'C1':range(5,10)})
+    DT_min_list = DT[:, dt.min([f.C0, f.C1])]
+    DT_min_tuple = DT[:, dt.min((f.C0, f.C1))]
+    DT_min_dict = DT[:, dt.min({"A":f.C0, "B":f.C1})]
+    DT_ref = dt.Frame([[0]/dt.int32, [5]/dt.int32])
+    assert_equals(DT_min_list, DT_ref)
+    assert_equals(DT_min_tuple, DT_ref)
+    assert_equals(DT_min_dict, DT_ref[:, {"A":f.C0, "B":f.C1}])
+
+def test_max_multicolumn():
+    # See issue #3406
+    DT = dt.Frame({'C0':range(5), 'C1':range(5,10)})
+    DT_max_list = DT[:, dt.max([f.C0, f.C1])]
+    DT_max_tuple = DT[:, dt.max((f.C0, f.C1))]
+    DT_max_dict = DT[:, dt.max({"A":f.C0, "B":f.C1})]
+    DT_ref = dt.Frame([[4]/dt.int32, [9]/dt.int32])
+    assert_equals(DT_max_list, DT_ref)
+    assert_equals(DT_max_tuple, DT_ref)
+    assert_equals(DT_max_dict, DT_ref[:, {"A":f.C0, "B":f.C1}])
+
+
+@pytest.mark.parametrize("mm, res", [(dt.min, 0), (dt.max, 4)])
+def test_minmax_frame(mm, res):
+    # See issue #3406
+    DT = dt.Frame(range(5))
+    assert mm(DT)[0,0] == res
 
 
 
