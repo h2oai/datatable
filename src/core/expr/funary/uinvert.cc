ac90eb6c9a9b8244eb2a0185ed496c596a687684
samukweku <samueloranyeli@gmail.com>
2023-03-29 22:48:22 +1100

    restrict inverse to bool and int columns

--- src/core/expr/funary/uinvert.cc
+++ src/core/expr/funary/uinvert.cc
@@ -56,44 +56,32 @@ class FExpr_UInvert : public FExpr_FuncUnary {
       Column col_out;
 
       switch (stype) {
-        case SType::VOID: return Column(new ConstNa_ColumnImpl(
-                                   col.nrows(), SType::VOID
-                                 ));
+        case SType::VOID: 
+          return Column(new ConstNa_ColumnImpl(col.nrows(), SType::VOID));
         case SType::BOOL: 
-          col_out =  Column(new FuncUnary1_ColumnImpl<int8_t, int8_t>(
-                      std::move(col), op_invert_bool, col.nrows(), SType::BOOL
-                     ));
-          break;
+          return Column(new FuncUnary1_ColumnImpl<int8_t, int8_t>(
+                   std::move(col), op_invert_bool, col.nrows(), SType::BOOL
+                 ));
         case SType::INT8: 
-          col_out = Column(new FuncUnary1_ColumnImpl<int8_t, int8_t>(
-                      std::move(col), op_invert<int8_t>, col.nrows(), SType::INT8
-                    ));
-          break;
+          return Column(new FuncUnary1_ColumnImpl<int8_t, int8_t>(
+                   std::move(col), op_invert<int8_t>, col.nrows(), SType::INT8
+                 ));
         case SType::INT16: 
-          col_out = Column(new FuncUnary1_ColumnImpl<int16_t, int16_t>(
-                      std::move(col), op_invert<int16_t>, col.nrows(), SType::INT16
-                    ));
-          break;        
+          return Column(new FuncUnary1_ColumnImpl<int16_t, int16_t>(
+                   std::move(col), op_invert<int16_t>, col.nrows(), SType::INT16
+                 ));
         case SType::INT32:
-        case SType::DATE32:
-          col_out = Column(new FuncUnary1_ColumnImpl<int32_t, int32_t>(
-                      std::move(col), op_invert<int32_t>, col.nrows(), SType::INT32
-                    ));
-          break;        
+          return Column(new FuncUnary1_ColumnImpl<int32_t, int32_t>(
+                   std::move(col), op_invert<int32_t>, col.nrows(), SType::INT32
+                 ));
         case SType::INT64:
-        case SType::TIME64:
-          col_out = Column(new FuncUnary1_ColumnImpl<int64_t, int64_t>(
-                      std::move(col), op_invert<int64_t>, col.nrows(), SType::INT64
-                    ));
-          break;; 
+          return Column(new FuncUnary1_ColumnImpl<int64_t, int64_t>(
+                   std::move(col), op_invert<int64_t>, col.nrows(), SType::INT64
+                 ));
         default:
           throw TypeError() << "Cannot apply unary `operator ~` to a column with "
                               "stype `" << stype << "`";
       }
-      if (stype == SType::DATE32 || stype == SType::TIME64) {
-          col_out.cast_inplace(stype);
-      }
-      return col_out;
     }
 };
 
