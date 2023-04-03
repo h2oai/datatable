8c284178bf1ac9bbcf876ecc6e2487ebe5c1686a
samukweku <samueloranyeli@gmail.com>
2023-04-03 23:11:33 +1000

    update tests

--- tests/ijby/test-func-unary.py
+++ tests/ijby/test-func-unary.py
@@ -244,9 +244,7 @@ def test_dt_hyperbolic1(math_func, dt_func):
     assert RES.to_list() == [[hyperbolic_func(math_func, x) for x in src] for src in srcs]
 
 def test_dt_hyperbolic_acosh():
-    # floating point might have different precision
-    # from python, hence the use of integers
-    srcs = [[7, 56, 3, 1]] 
+    srcs = [[7, 3, 1]] 
     DT = dt.Frame(srcs)
     RES = DT[:, dt.math.arcosh(f[:])]
     frame_integrity_check(RES)
