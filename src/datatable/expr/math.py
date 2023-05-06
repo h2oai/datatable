aaec02e8bab408724211e82c8a9b36ee7b5fe6a1
samukweku <samueloranyeli@gmail.com>
2023-05-07 08:23:35 +1000

    copyright date updates

--- src/datatable/expr/math.py
+++ src/datatable/expr/math.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -------------------------------------------------------------------------------
-# Copyright 2018 H2O.ai
+# Copyright 2018-2023 H2O.ai
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
